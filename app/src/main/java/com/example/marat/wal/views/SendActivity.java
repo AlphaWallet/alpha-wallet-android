b8529337923832c0889af45489fc76f2713b6489
Marat Subkhankulov <m.subkhankulov@gmail.com>
2017-10-09 23:11:28 -0700

    Adds qr scanner

--- app/src/main/java/com/example/marat/wal/views/SendActivity.java
+++ app/src/main/java/com/example/marat/wal/views/SendActivity.java
@@ -1,16 +1,24 @@
 package com.example.marat.wal.views;
 
+import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.marat.wal.R;
 import com.example.marat.wal.controller.Controller;
 import com.example.marat.wal.model.VMAccount;
+import com.example.marat.wal.views.barcode.BarcodeCaptureActivity;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +31,11 @@ public class SendActivity extends AppCompatActivity {
     private EditText mTo;
     private EditText mAmount;
     private EditText mPassword;
+    private static final String LOG_TAG = SendActivity.class.getSimpleName();
+    private static final int BARCODE_READER_REQUEST_CODE = 1;
+
+    private TextView mResultTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,5 +68,30 @@ public class SendActivity extends AppCompatActivity {
                 mController.clickSend(SendActivity.this, mFromSpinner.getSelectedItem().toString(), mTo.getText().toString(), mAmount.getText().toString(), mPassword.getText().toString());
             }
         });
+
+        mResultTextView = (TextView) findViewById(R.id.result_textview);
+
+        ImageButton scanBarcodeButton = (ImageButton) findViewById(R.id.scan_barcode_button);
+        scanBarcodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
+                startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BARCODE_READER_REQUEST_CODE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    Point[] p = barcode.cornerPoints;
+                    mTo.setText(barcode.displayValue);
+                } else mResultTextView.setText(R.string.no_barcode_captured);
+            } else Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
+                    CommonStatusCodes.getStatusCodeString(resultCode)));
+        } else super.onActivityResult(requestCode, resultCode, data);
     }
 }

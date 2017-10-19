5ffa240689b2ab86b95b76eefd3ec134f7f06e75
Marat Subkhankulov <m.subkhankulov@gmail.com>
2017-10-19 10:47:10 -0700

    Adds preferences view

--- app/src/main/java/com/wallet/crypto/trust/views/IntroActivity.java
+++ app/src/main/java/com/wallet/crypto/trust/views/IntroActivity.java
@@ -5,11 +5,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 
+import com.github.paolorotolo.appintro.AppIntro2;
 import com.wallet.crypto.trust.R;
 import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 
-public class IntroActivity extends AppIntro {
+public class IntroActivity extends AppIntro2 {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +24,7 @@ public class IntroActivity extends AppIntro {
         // OPTIONAL METHODS
         // Override bar/separator color.
         setBarColor(Color.parseColor("#3F51B5"));
-        setSeparatorColor(Color.parseColor("#2196F3"));
+        //setSeparatorColor(Color.parseColor("#2196F3"));
 
         // Hide Skip/Done generateButton.
         //showSkipButton(true);

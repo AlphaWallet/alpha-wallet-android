6c6aa6725899c44c7a50f9efc944cd12cde6745e
Philipp Rieger <Phoebus91@web.de>
2017-10-23 07:43:00 +0200

    Added JNI Support and PasswordManager. (#1)
    * Added JNI Support and PasswordManager.
    
    * Changed Shared Prefs to get it from the PreferenceManager.
    
    * Modified methods so that passwords for multiple addresses can be stored.

---
+++ app/src/main/cpp/native-lib.c
@@ -0,0 +1,21 @@
+
+#include <string.h>
+#include <jni.h>
+
+
+
+JNIEXPORT jstring JNICALL
+Java_com_wallet_crypto_trust_controller_PasswordManager_getKeyStringFromNative( JNIEnv* env, jobject thiz )
+{
+    // TODO: fill in your key - must be 32 bytes
+    const jstring key = "35TheTru5tWa11ets3cr3tK3y377123!";
+    return (*env)->NewStringUTF(env, key);
+}
+
+JNIEXPORT jbyteArray JNICALL
+Java_com_wallet_crypto_trust_controller_PasswordManager_getIvStringFromNative( JNIEnv* env, jobject thiz )
+{
+    // TODO: fill in your iv - must be 16 bytes
+    const jstring iv = "8201va0184a0md8i";
+    return (*env)->NewStringUTF(env, iv);
+}

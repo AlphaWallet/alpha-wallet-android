1b79684da0503ac9c655a6685a38a7b6766db26f
Marat Subkhankulov <m.subkhankulov@gmail.com>
2017-11-16 07:58:55 -0800

    Disable dex to avoid retrofit crash

---
+++ app/src/main/java/com/wallet/crypto/trustapp/views/TokenListActivity.java
@@ -0,0 +1,31 @@
+package com.wallet.crypto.trustapp.views;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.wallet.crypto.trustapp.R;
+
+public class TokenListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_token_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}

3f945af3acc81a3b91008970042ef0a72e72e5a3
Marat Subkhankulov <msubkhankulov@nimblestorage.com>
2017-12-17 00:38:31 -0800

    Don't start instantiate Crashlytics in DEBUG build

--- app/src/main/java/com/wallet/crypto/trustapp/views/SplashActivity.java
+++ app/src/main/java/com/wallet/crypto/trustapp/views/SplashActivity.java
@@ -3,7 +3,11 @@ package com.wallet.crypto.trustapp.views;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
 import com.crashlytics.android.Crashlytics;
+import com.wallet.crypto.trustapp.BuildConfig;
+
 import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
@@ -13,7 +17,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
 
         // Start home activity
 

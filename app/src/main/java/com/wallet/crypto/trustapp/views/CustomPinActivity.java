99e3626acfbe7454031559f32de9088e2576d2c1
Philipp Rieger <phoebus91@web.de>
2017-12-26 17:38:23 +0100

    Add CustomPinActivity.

---
+++ app/src/main/java/com/wallet/crypto/trustapp/views/CustomPinActivity.java
@@ -0,0 +1,22 @@
+package com.wallet.crypto.trustapp.views;
+
+import com.github.omadahealth.lollipin.lib.managers.AppLockActivity;
+
+public class CustomPinActivity extends AppLockActivity {
+
+    @Override
+    public void showForgotDialog() {
+
+    }
+
+    @Override
+    public void onPinFailure(int attempts) {
+
+    }
+
+    @Override
+    public void onPinSuccess(int attempts) {
+
+    }
+}
+

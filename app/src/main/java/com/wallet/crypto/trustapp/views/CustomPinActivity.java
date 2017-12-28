c2a72c95a2a544c08a6f7384726324ae4ae7d977
Philipp Rieger <phoebus91@web.de>
2017-12-28 18:52:50 +0100

    Added info dialog to forgot button.

--- app/src/main/java/com/wallet/crypto/trustapp/views/CustomPinActivity.java
+++ app/src/main/java/com/wallet/crypto/trustapp/views/CustomPinActivity.java
@@ -1,12 +1,26 @@
 package com.wallet.crypto.trustapp.views;
 
+import android.content.DialogInterface;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+
 import com.github.omadahealth.lollipin.lib.managers.AppLockActivity;
+import com.wallet.crypto.trustapp.R;
 
 public class CustomPinActivity extends AppLockActivity {
 
     @Override
     public void showForgotDialog() {
-
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+        builder.setTitle("Warning")
+                .setMessage("Please get in touch with the devs.")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 
     @Override
@@ -23,8 +37,5 @@ public class CustomPinActivity extends AppLockActivity {
     public int getPinLength() {
         return super.getPinLength();
     }
-
-
-
 }
 

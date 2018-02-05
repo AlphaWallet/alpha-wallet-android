761c79ffdd21d9ba845a4dc465e4f8615e3d8f27
James Brown <soylentblue@hotmail.co.uk>
2018-02-05 19:42:33 +0800

    Refactor message signing code

--- app/src/main/java/com/wallet/crypto/alphawallet/interact/CreateTransactionInteract.java
+++ app/src/main/java/com/wallet/crypto/alphawallet/interact/CreateTransactionInteract.java
@@ -21,17 +21,10 @@ public class CreateTransactionInteract {
         this.passwordStore = passwordStore;
     }
 
-    //TODO: refactor with full reactive chaining (ie remove the blocking get)
     public Single<SignaturePair> sign(Wallet wallet, MessagePair messagePair) {
-        return Single.fromCallable(() -> {
-            Single<byte[]> sig = passwordStore.getPassword(wallet)
-                    .flatMap(password ->
-                            transactionRepository.getSignature(wallet, messagePair.message, password)
-                                    .observeOn(AndroidSchedulers.mainThread()));
-
-            SignaturePair sigPair = new SignaturePair(messagePair.selection, sig.blockingGet());
-            return sigPair;
-        });
+        return passwordStore.getPassword(wallet)
+                .flatMap(password -> transactionRepository.getSignature(wallet, messagePair.message, password))
+                .map(sig -> new SignaturePair(messagePair.selection, sig));
     }
 
     public Single<String> create(Wallet from, String to, BigInteger subunitAmount, BigInteger gasPrice, BigInteger gasLimit, byte[] data) {

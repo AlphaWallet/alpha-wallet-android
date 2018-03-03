d636156c4b6f7d0a035ee1d6036f10c22292e31a
colourful-land <a@colourful.land>
2018-03-03 11:05:22 +0800

    comment on the code after I listened to James explaining the logic behind the code. Also renaming variable and removing unused code from the previous implementation, and some try{}catch{} with no apparent uses

--- app/src/main/java/com/wallet/crypto/alphawallet/entity/SignaturePair.java
+++ app/src/main/java/com/wallet/crypto/alphawallet/entity/SignaturePair.java
@@ -6,8 +6,16 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Created by James on 30/01/2018.
- */
+ * Created by James on 30/01/2018. Signature Pair refers to the
+ * following data structure [selection of tokens][signature]. The word
+ * "Signature Pair" has nothing to do with the pair of numbers that
+ * makes up signature (𝑟, 𝑠), nor does it has anything to do with
+ * Elliptic-Curve pairing. The "Pair" word is here refer to a Java
+ * concept where you combine data to pass to the observer. And the
+ * Pairing is actually not done in this class, because other data not
+ * in the pair (like timestamp) are to be bound together outside of
+ * the class.
+y */
 
 public class SignaturePair {
     public byte[] signature;
@@ -16,13 +24,20 @@ public class SignaturePair {
     public String signatureStr;
     public String selectionStr;
 
-    public SignaturePair(String selection, byte[] sig, String digest)
+    /**
+     * You might expect the code to combine the message and signature
+     * to be in this class - it is not.
+     * @param selection what is returned from generate
+     * @param sig       65 bytes of signature
+     * @param message   the message being signed, including the 'selection'.
+     */
+    public SignaturePair(String selection, byte[] sig, String message)
     {
         selectionStr = selection;
         signature = sig;
         BigInteger bi = new BigInteger(signature);
         signatureStr  = bi.toString(10);
-        message = digest;
+        this.message = message;
 
         //check if the produced number was negative
         if (signatureStr.charAt(0) == '-')
@@ -32,80 +47,77 @@ public class SignaturePair {
         }
     }
 
-    /* James made this to generate a compact string representation of the indices of an ERC875 asset.*/
+    /**
+     * Generate a compact string representation of the indices of an
+     * ERC875 asset.  Notice that this function is not used in this
+     * class. It is used to return the selectionStr to be used as a
+     * parameter of the constructor */
     public static String generateSelection(List<Integer> indexList)
     {
-        String selection = null;
-
-        try {
-            if (indexList != null && indexList.size() > 0) {
-                Collections.sort(indexList); // Indices needs to be sorted to find its bitfield representation
-                final int NIBBLE = 4;
-                Integer lowestValue = indexList.get(0);
-                int zeroCount = lowestValue / NIBBLE;
-                // now reduce the index to base of this value
-                int correctionFactor = zeroCount * NIBBLE;
-                Integer highestValue = indexList.get(indexList.size() - 1); //TODO: Check for highest value out of range of bitfield
-
-                /* the method here is easier to express with matrix programming like this:
-                indexList = indexList - correctionFactor # reduce every element of the list by an int
-                selection = sum(2^indexList)             # raise every element and add the result back */
-                BigInteger bitFieldLookup = BigInteger.ZERO;
-                for (Integer i : indexList) {
-                    BigInteger adder = BigInteger.valueOf(2).pow(i - correctionFactor);
-                    bitFieldLookup = bitFieldLookup.add(adder);
-                }
-
-                String truncatedValueDecimal = bitFieldLookup.toString(10); //decimal of reduced bitfield
-
-                // to create string
-                StringBuilder sb = new StringBuilder();
-                sb.append(String.format("%1$02d", truncatedValueDecimal.length()));
-                sb.append(String.format("%1$02d", zeroCount));
-                sb.append(String.valueOf(truncatedValueDecimal));
-
-                selection = sb.toString();
-            }
-        }
-        catch (Exception e)
-        {
-            selection = null;
+        if (indexList == null || indexList.size() == 0) {
+            return null;
         }
 
-        return(selection);
+        Collections.sort(indexList); // just to find the lowest value
+        // since sorting is not needed to make the bitFieldLookup
+        Integer lowestValue = indexList.get(0);
+        final int NIBBLE = 4;
+        int zeroCount = lowestValue / NIBBLE;
+        // now reduce the index to base of this value
+        int correctionFactor = zeroCount * NIBBLE;
+        // TODO: Check for highest value out of range of bitfield. Like this:
+        // Integer highestValue = indexList.get(indexList.size() - 1);
+
+        /* the method here is easier to express with matrix programming like this:
+        indexList = indexList - correctionFactor # reduce every element of the list by an int
+        selection = sum(2^indexList)             # raise every element and add the result back */
+        BigInteger bitFieldLookup = BigInteger.ZERO;
+        for (Integer i : indexList) {
+            BigInteger adder = BigInteger.valueOf(2).pow(i - correctionFactor);
+            bitFieldLookup = bitFieldLookup.add(adder);
+        }
+
+        String truncatedValueDecimal = bitFieldLookup.toString(10); //decimal of reduced bitfield
+
+        // to create string
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%1$02d", truncatedValueDecimal.length()));
+        sb.append(String.format("%1$02d", zeroCount));
+        sb.append(String.valueOf(truncatedValueDecimal));
+
+        return sb.toString();
     }
 
+    /**
+     * The reverse of generateSelection - used in scanning the QR code.
+     */
     public static List<Integer> buildIndexList(String selection) {
         List<Integer> intList = new ArrayList<>();
-        try {
-            final int NIBBLE = 4;
-            //one: convert to bigint
-            String lengthStr = selection.substring(0, 2);
-            int selectionLength = Integer.parseInt(lengthStr);
-            String trailingZerosStr = selection.substring(2, 4);
-            int trailingZeros = Integer.parseInt(trailingZerosStr);
-            int correctionFactor = trailingZeros*NIBBLE;
+        final int NIBBLE = 4;
+        //one: convert to bigint
+        String lengthStr = selection.substring(0, 2);
+        int selectionLength = Integer.parseInt(lengthStr);
+        String trailingZerosStr = selection.substring(2, 4);
+        int trailingZeros = Integer.parseInt(trailingZerosStr);
+        int correctionFactor = trailingZeros*NIBBLE;
 
-            String selectionStr = selection.substring(4, 4 + selectionLength);
-            BigInteger bitField = new BigInteger(selectionStr, 10);
+        String selectionStr = selection.substring(4, 4 + selectionLength);
+        BigInteger bitField = new BigInteger(selectionStr, 10);
 
-            int radix = bitField.getLowestSetBit();
-            while (!bitField.equals(BigInteger.ZERO)) {
-                if (bitField.testBit(radix)) {
-                    intList.add(radix + correctionFactor); //because we need to encode index zero as the first bit
-                    bitField = bitField.clearBit(radix);
-                }
-                radix++;
+        int radix = bitField.getLowestSetBit();
+        while (!bitField.equals(BigInteger.ZERO)) {
+            if (bitField.testBit(radix)) {
+                intList.add(radix + correctionFactor); //because we need to encode index zero as the first bit
+                bitField = bitField.clearBit(radix);
             }
+            radix++;
         }
-        catch (Exception e)
-        {
-
-        }
-
         return intList;
     }
 
+    /**
+     * twosComplement is an implementation of
+     * https://en.wikipedia.org/wiki/Two's_complement */
     public static BigInteger twosComplement(BigInteger original)
     {
         // for negative BigInteger, top byte is negative
@@ -126,8 +138,6 @@ public class SignaturePair {
 
         String lengthStr = qrMessage.substring(0, 2);
         int selectionLength = Integer.parseInt(lengthStr);
-        String trailingZerosStr = qrMessage.substring(2, 4);
-        int trailingZeros = Integer.parseInt(trailingZerosStr);
         selectionStr = qrMessage.substring(0, 4 + selectionLength);
         signatureStr = qrMessage.substring(4 + selectionLength);
         message = selectionStr + "," + timeMessage;
@@ -142,17 +152,5 @@ public class SignaturePair {
             System.arraycopy(signature, 1, sigCopy, 0, 65);
             signature = sigCopy;
         }
-    }
-
-    private int getTrailingZeros(String s)
-    {
-        int i = (s.length() - 1);
-        int trailingZeros = 0;
-        while (s.charAt(i--) == '0')
-        {
-            trailingZeros++;
-        }
-
-        return trailingZeros;
     }
 }

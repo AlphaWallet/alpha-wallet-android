8da6310fd3b06c205d4a90f358a7193da23c19de
James Brown <soylentblue@hotmail.co.uk>
2018-03-09 23:19:51 +0800

    Update universal link test

--- app/src/test/java/com/wallet/crypto/alphawallet/UniversalLinkTest.java
+++ app/src/test/java/com/wallet/crypto/alphawallet/UniversalLinkTest.java
@@ -24,21 +24,21 @@ import java.util.Date;
  * byte: v
  * bytes32: r
  * bytes32: s
- * 
+ *
  */
 
 
 public class UniversalLinkTest {
-    String link = "https://app.awallet.io/?AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAsaK8LsUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAWqKEQbyaECakvG8LqLvkhtHQnaVzKznkAD4=";
-    int[] indices = new int[] { 0x3e };
+    String link = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALyaECakvG8LqLvkhtHQnaVzKznkAKQApQAAUIUqPzGusqG4jDWy4WUbXPs3vTtZgZnJungxzz7TmzwMSSi/VfTEW0dZduwWdwYqvVR4c2dD9TppFl4JPHQ5mJwxHA==";
+    int[] indices = new int[] { 0xa4, 0xa5 };
     boolean verifySignature(byte[] message, byte[] signature) {
         return false;
     }
     @Test
     public void UniversalLinkShouldBeParsedCorrectly() {
         SalesOrder order = SalesOrder.parseUniversalLink(link);
-        assertEquals(new BigInteger("0x2c68af0bb140000", 16), order.priceWei );
-        assertEquals(0x5aa28441, order.expiry);
+        assertEquals(new BigInteger("0", 16), order.priceWei );
+        assertEquals(0x0, order.expiry);
         assertEquals("0xbc9a1026a4bc6f0ba8bbe486d1d09da5732b39e4", order.contractAddress);
         assertArrayEquals(indices, order.tickets);
         assertTrue(verifySignature(order.message, order.signature));

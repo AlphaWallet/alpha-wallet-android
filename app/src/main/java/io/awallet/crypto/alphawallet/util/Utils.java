1a2e5c76f9fb813f045b98e6aa1927fc59e5275f
justindg <jrbdeguzman@gmail.com>
2018-04-19 11:51:57 +0800

    Updated InputView with some features

---
+++ app/src/main/java/io/awallet/crypto/alphawallet/util/Utils.java
@@ -0,0 +1,16 @@
+package io.awallet.crypto.alphawallet.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+public class Utils {
+    public static int dp2px(Context context, int dp) {
+        Resources r = context.getResources();
+        return (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                dp,
+                r.getDisplayMetrics()
+        );
+    }
+}

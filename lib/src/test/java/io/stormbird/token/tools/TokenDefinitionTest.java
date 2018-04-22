c7906487fac284a4f2578f94c6045858151ae279
colourful-land <a@colourful.land>
2018-04-22 18:00:10 +0800

    adding test code to the new Token Definition

---
+++ lib/src/test/java/io/stormbird/token/tools/TokenDefinitionTest.java
@@ -0,0 +1,40 @@
+package io.awallet.crypto.alphawallet;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
+import io.stormbird.token.tools.TokenDefinition;
+
+public class TokenDefinitionTest {
+    BigInteger[] ticketIDs = {
+	    BigInteger.valueOf(0x010CCB53), BigInteger.valueOf(0x010CCB54),
+	    BigInteger.valueOf(0x02020075), BigInteger.valueOf(0x02020076)
+    };
+    File file = new File("../app/src/main/assets/ticket.xml");
+
+    @Test
+    public void TokenDefinitionShouldParse() throws IOException, SAXException {
+        assertTrue(file.exists());
+        TokenDefinition ticketAsset = new TokenDefinition(new FileInputStream(file), "en");
+        assertFalse(ticketAsset.fields.isEmpty());
+
+        //NonFungibleToken ticket = new NonFungibleToken(ticketIDs[0], ticketAsset);
+        //assertEquals("Number", ticket.getAttribute("number").name);
+        //assertEquals(BigInteger.valueOf(0xCB53), ticket.getAttribute("number").value);
+        /* Epoch, the following test only works from Singapore */
+        /* Travis isn't in Singapore ... */
+        //assertEquals("Thu Jan 01 07:30:00 SGT 1970", ticket.getAttribute("time").text);
+        //assertEquals(BigInteger.ZERO, ticket.getAttribute("time").value);
+    }
+
+}

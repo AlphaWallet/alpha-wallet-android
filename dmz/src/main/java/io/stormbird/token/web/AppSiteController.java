404e42f7febaf41601dee394d0883845a73b2451
James Brown <soylentblue@hotmail.co.uk>
2018-04-30 12:52:43 +0800

    Mods to DMZ server to start decoding ticket

--- dmz/src/main/java/io/stormbird/token/web/AppSiteController.java
+++ dmz/src/main/java/io/stormbird/token/web/AppSiteController.java
@@ -2,22 +2,62 @@ package io.stormbird.token.web;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import io.stormbird.token.entity.MagicLinkData;
+import io.stormbird.token.entity.SalesOrderMalformed;
+import io.stormbird.token.tools.ParseMagicLink;
+import io.stormbird.token.web.Service.CryptoFunctions;
+
 
 @Controller
 @SpringBootApplication
+@RequestMapping("/")
 public class AppSiteController {
 
-   @GetMapping("/")
-    public String index(@RequestParam(name="base64", required=false, defaultValue="BCD4") String base64, Model model) {
-        model.addAttribute("base64", base64);
+    private static ParseMagicLink parser = new ParseMagicLink();
+    private static CryptoFunctions cryptoFunctions = new CryptoFunctions();
+
+    @GetMapping(value = "/{magicLink}")
+    public String getBird(@PathVariable("magicLink") String magicLink, Model model)
+    {
+        model.addAttribute("base64", magicLink);
+        try
+        {
+            MagicLinkData data = parser.parseUniversalLink(magicLink);
+            model.addAttribute("base64", data.contractAddress);
+        }
+        catch (SalesOrderMalformed e)
+        {
+            e.printStackTrace();
+        }
         return "index";
     }
 
+    @RequestMapping("/")
+    public String home(HttpServletRequest request){
+        return "index";
+    }
+
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(AppSiteController.class, args);
+		parser.setCryptoInterface(cryptoFunctions);
 	}
+
+
+	//1. get ticket from URL  <---
+    //2. interpret URL extension
+    //  - Move SalesOrder code to library
+    //  - get values out of the order
+    //  - Update tests
+    //3. Add web3j
+    //4. Init web3j and get balance
+    //5. Display results
 }

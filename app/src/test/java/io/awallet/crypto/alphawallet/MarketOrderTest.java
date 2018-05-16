d852e7ba67eedec053d5b61de89634e170fc858b
James Brown <soylentblue@hotmail.co.uk>
2018-05-16 19:49:25 +0800

    Fix for tests

--- app/src/test/java/io/awallet/crypto/alphawallet/MarketOrderTest.java
+++ app/src/test/java/io/awallet/crypto/alphawallet/MarketOrderTest.java
@@ -119,7 +119,7 @@ public class MarketOrderTest
             }
 
             @Override
-            public Observable<Transaction[]> fetchNetworkTransaction(Wallet wallet, Transaction lastBlock)
+            public Observable<Transaction[]> fetchNetworkTransaction(Wallet wallet, long lastBlock)
             {
                 return null;
             }

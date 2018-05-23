a8d60cf94ece0b741e1912c669d1668fc095ca62
colourful-land <a@colourful.land>
2018-05-23 09:30:18 +0800

    using getLanguage instead of getDisplayLanguage. locale now tested for XML reading too.

---
+++ app/src/main/java/io/stormbird/wallet/repository/AssetDefinition.java
@@ -0,0 +1,28 @@
+package io.stormbird.wallet.repository;
+
+import android.content.res.Resources;
+
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+
+import io.stormbird.token.tools.TokenDefinition;
+
+/**
+ * Created by weiwu on 22/4/18.
+ * This is a wrapper class that is intended to be discarded eventually
+ */
+
+public class AssetDefinition extends TokenDefinition {
+
+    public AssetDefinition(InputStream xmlAsset, Locale locale) throws IOException, SAXException {
+        super(xmlAsset, locale);
+    }
+
+    public AssetDefinition(String filename, Resources res) throws IOException, SAXException {
+        super(res.getAssets().open(filename), res.getConfiguration().locale);
+    }
+
+}

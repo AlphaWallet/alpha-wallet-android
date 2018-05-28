d71df64c4f3becf39fe7ca3466b20d9ef2b5ec38
justindg <jrbdeguzman@gmail.com>
2018-05-28 11:20:38 +0800

    Added measures to prevent crashing in Android O

--- app/src/main/java/io/stormbird/wallet/util/TabUtils.java
+++ app/src/main/java/io/stormbird/wallet/util/TabUtils.java
@@ -2,10 +2,12 @@ package io.stormbird.wallet.util;
 
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.res.ResourcesCompat;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,18 +20,27 @@ import java.lang.reflect.Field;
 
 public class TabUtils {
     public static void changeTabsFont(Context context, TabLayout tabLayout) {
-        Typeface typeface = ResourcesCompat.getFont(context, R.font.font_regular);
-        ViewGroup vg = (ViewGroup) tabLayout.getChildAt(0);
-        int tabsCount = vg.getChildCount();
-        for (int j = 0; j < tabsCount; j++) {
-            ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
-            int tabChildsCount = vgTab.getChildCount();
-            for (int i = 0; i < tabChildsCount; i++) {
-                View tabViewChild = vgTab.getChildAt(i);
-                if (tabViewChild instanceof TextView) {
-                    ((TextView) tabViewChild).setTypeface(typeface);
+        try {
+            Typeface typeface = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                typeface = context.getResources().getFont(R.font.font_regular);
+            } else {
+                typeface = ResourcesCompat.getFont(context, R.font.font_regular);
+            }
+            ViewGroup vg = (ViewGroup) tabLayout.getChildAt(0);
+            int tabsCount = vg.getChildCount();
+            for (int j = 0; j < tabsCount; j++) {
+                ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
+                int tabChildsCount = vgTab.getChildCount();
+                for (int i = 0; i < tabChildsCount; i++) {
+                    View tabViewChild = vgTab.getChildAt(i);
+                    if (tabViewChild instanceof TextView) {
+                        ((TextView) tabViewChild).setTypeface(typeface);
+                    }
                 }
             }
+        } catch (Resources.NotFoundException nfe) {
+            Log.e(TabUtils.class.getSimpleName(), nfe.getMessage(), nfe);
         }
     }
 

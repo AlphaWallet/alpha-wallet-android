f3ea94b049163e9ec1cefeefb4a60231692aeac9
James Brown <soylentblue@hotmail.co.uk>
2018-07-13 13:46:51 +0800

    Tidy imports

--- app/src/main/java/io/stormbird/wallet/entity/TokenInfo.java
+++ app/src/main/java/io/stormbird/wallet/entity/TokenInfo.java
@@ -1,21 +1,10 @@
 package io.stormbird.wallet.entity;
 
-import android.content.Context;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.view.View;
 
-import io.stormbird.wallet.R;
 import io.stormbird.wallet.ui.AddTokenActivity;
-import io.stormbird.wallet.ui.widget.holder.TokenHolder;
-import io.stormbird.wallet.viewmodel.BaseViewModel;
-import io.stormbird.wallet.viewmodel.TokensViewModel;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.List;
-
-import static io.stormbird.wallet.ui.widget.holder.TokenHolder.EMPTY_BALANCE;
 
 public class TokenInfo implements Parcelable {
     public final String address;

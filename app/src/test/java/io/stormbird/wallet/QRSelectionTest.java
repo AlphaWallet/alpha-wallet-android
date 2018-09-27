702b00e694043368b0d11d10f423c8caffff3f32
James Brown <soylentblue@hotmail.co.uk>
2018-09-27 23:04:08 +0800

    Increase thouroughness of QR signature test

--- app/src/test/java/io/stormbird/wallet/QRSelectionTest.java
+++ app/src/test/java/io/stormbird/wallet/QRSelectionTest.java
@@ -177,7 +177,7 @@ public class QRSelectionTest
         final int indicesCount = 8*2;
         final int combinations = (int)Math.pow(2, indicesCount);
 
-        for (int i = 1; i < combinations; i += 7) // don't pick all the combinations
+        for (int i = 1; i < combinations; i += 1) // don't pick all the combinations
         {
             QREncoding qr = new QREncoding();
             qrList.add(qr);

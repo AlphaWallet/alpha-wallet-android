bc00f51dda5eac92cc7336b4d0d788bd27572210
justindg <jrbdeguzman@gmail.com>
2018-10-05 16:06:37 +0800

    Add status blocks for asset display

--- app/src/main/java/io/stormbird/wallet/ui/widget/holder/OpenseaHolder.java
+++ app/src/main/java/io/stormbird/wallet/ui/widget/holder/OpenseaHolder.java
@@ -3,6 +3,7 @@ package io.stormbird.wallet.ui.widget.holder;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 import io.reactivex.disposables.Disposable;
+import io.stormbird.wallet.C;
 import io.stormbird.wallet.R;
 import io.stormbird.wallet.entity.OpenseaElement;
 import io.stormbird.wallet.service.OpenseaService;
@@ -60,5 +62,19 @@ public class OpenseaHolder extends BinderViewHolder<OpenseaElement> {
         Glide.with(getContext())
             .load(element.imageURL)
             .into(image);
+    }
+
+    private void setStatus(C.TokenStatus status) {
+        if (status == C.TokenStatus.PENDING) {
+            statusText.setVisibility(View.VISIBLE);
+            statusText.setBackgroundResource(R.drawable.background_status_pending);
+            statusText.setText(R.string.status_pending);
+        } else if (status == C.TokenStatus.INCOMPLETE){
+            statusText.setVisibility(View.VISIBLE);
+            statusText.setBackgroundResource(R.drawable.background_status_incomplete);
+            statusText.setText(R.string.status_incomplete_data);
+        } else {
+            statusText.setVisibility(View.GONE);
+        }
     }
 }

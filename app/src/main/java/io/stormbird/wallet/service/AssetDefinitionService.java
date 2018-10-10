a48070de501164a498ff3e897dc44a29d5d0beb4
James Brown <soylentblue@hotmail.co.uk>
2018-10-10 10:06:38 +0800

    Fixes

--- app/src/main/java/io/stormbird/wallet/service/AssetDefinitionService.java
+++ app/src/main/java/io/stormbird/wallet/service/AssetDefinitionService.java
@@ -393,10 +393,14 @@ public class AssetDefinitionService
                 if (extension.equals("xml"))
                 {
                     String name = f.getName().substring(0, f.getName().lastIndexOf('.')).toLowerCase();
-                    if (Address.isAddress(name))
+                    try
                     {
                         FileInputStream stream = new FileInputStream(f);
                         parseFile(stream);
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
                     }
                 }
             }

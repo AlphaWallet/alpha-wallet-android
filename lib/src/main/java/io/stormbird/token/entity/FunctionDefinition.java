dd0fc271e73db0271161c701f719b77ba1c07d2e
James Brown <soylentblue@hotmail.co.uk>
2018-11-10 14:39:43 +1100

    Add function definition

---
+++ lib/src/main/java/io/stormbird/token/entity/FunctionDefinition.java
@@ -0,0 +1,14 @@
+package io.stormbird.token.entity;
+
+import io.stormbird.token.tools.TokenDefinition;
+
+/**
+ * Created by James on 10/11/2018.
+ * Stormbird in Singapore
+ */
+
+public class FunctionDefinition
+{
+    public String method;
+    public TokenDefinition.Syntax syntax;
+}

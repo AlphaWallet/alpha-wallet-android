a8649ef31b8901612be277604cd30e24b3479e48
James Brown <soylentblue@hotmail.co.uk>
2018-11-29 16:34:36 +1100

    Update web3 provider

--- app/src/main/res/raw/init.js
+++ app/src/main/res/raw/init.js
@@ -1,55 +1,50 @@
-
 const addressHex = "%1$s";
 const rpcURL = "%2$s";
 const chainID = "%3$s";
 
 function executeCallback (id, error, value) {
-  Trust.executeCallback(id, error, value)
+  AlphaWallet.executeCallback(id, error, value)
 }
-function onSignSuccessful(id, value) {
-  Trust.executeCallback(id, null, value)
-}
-function onSignError(id, error) {
-  Trust.executeCallback(id, error, null)
-}
-window.Trust.init(rpcURL, {
+
+window.AlphaWallet.init(rpcURL, {
   getAccounts: function (cb) { cb(null, [addressHex]) },
   processTransaction: function (tx, cb){
     console.log('signing a transaction', tx)
     const { id = 8888 } = tx
-    Trust.addCallback(id, cb)
+    AlphaWallet.addCallback(id, cb)
 
     var gasLimit = tx.gasLimit || tx.gas || null;
     var gasPrice = tx.gasPrice || null;
     var data = tx.data || null;
     var nonce = tx.nonce || -1;
-    trust.signTransaction(id, tx.to || null, tx.value, nonce, gasLimit, gasPrice, data);
+    alpha.signTransaction(id, tx.to || null, tx.value, nonce, gasLimit, gasPrice, data);
   },
   signMessage: function (msgParams, cb) {
-    console.log('signMessage', msgParams)
-    const { data, chainType } = msgParams
-    const { id = 8888 } = msgParams
-    Trust.addCallback(id, cb)
-    trust.signMessage(id, data);
+      console.log('signMessage', msgParams)
+      const { data, chainType } = msgParams
+      const { id = 8888 } = msgParams
+    AlphaWallet.addCallback(id, cb)
+    alpha.signMessage(id, data);
   },
   signPersonalMessage: function (msgParams, cb) {
-    console.log('signPersonalMessage', msgParams)
-    const { data, chainType } = msgParams
-    const { id = 8888 } = msgParams
-    Trust.addCallback(id, cb)
-    trust.signPersonalMessage(id, data);
+      console.log('signPersonalMessage', msgParams)
+      const { data, chainType } = msgParams
+      const { id = 8888 } = msgParams
+    AlphaWallet.addCallback(id, cb)
+    alpha.signPersonalMessage(id, data);
   },
   signTypedMessage: function (msgParams, cb) {
     console.log('signTypedMessage ', msgParams)
     const { data } = msgParams
     const { id = 8888 } = msgParams
-    Trust.addCallback(id, cb)
-    trust.signTypedMessage(id, JSON.stringify(data))
+    AlphaWallet.addCallback(id, cb)
+    alpha.signTypedMessage(id, JSON.stringify(data))
   }
 }, {
     address: addressHex,
     networkVersion: chainID
 })
+
 window.web3.setProvider = function () {
   console.debug('Alpha Wallet - overrode web3.setProvider')
 }
@@ -61,3 +56,4 @@ window.web3.eth.getCoinbase = function(cb) {
     return cb(null, addressHex)
 }
 window.web3.eth.defaultAccount = addressHex
+window.web3.currentProvider.isAlphaWallet = false

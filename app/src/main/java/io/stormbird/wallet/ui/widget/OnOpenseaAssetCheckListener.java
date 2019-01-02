51f48d0db42664e122e0b3d3d318e8c28ecd3826
James Brown <soylentblue@hotmail.co.uk>
2019-01-02 21:03:35 +0800

    Add opensea click listenee

---
+++ app/src/main/java/io/stormbird/wallet/ui/widget/OnOpenseaAssetCheckListener.java
@@ -0,0 +1,12 @@
+package io.stormbird.wallet.ui.widget;
+
+import io.stormbird.wallet.entity.opensea.Asset;
+
+/**
+ * Created by James on 2/01/2019.
+ * Stormbird in Singapore
+ */
+public interface OnOpenseaAssetCheckListener
+{
+    void onAssetCheck (Asset asset);
+}

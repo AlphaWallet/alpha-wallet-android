8b43f07a5d0ae8212bed333540b75247ac5889b5
justindg <jrbdeguzman@gmail.com>
2019-03-17 16:55:28 +0800

    Added switch network function in MyAddressActivity

--- app/src/main/java/io/stormbird/wallet/ui/MyAddressActivity.java
+++ app/src/main/java/io/stormbird/wallet/ui/MyAddressActivity.java
@@ -1,94 +1,134 @@
 package io.stormbird.wallet.ui;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.web3j.utils.Convert;
+
+import java.math.BigDecimal;
 
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjection;
 import io.stormbird.wallet.C;
 import io.stormbird.wallet.R;
-import io.stormbird.wallet.entity.*;
-import io.stormbird.wallet.interact.FetchTokensInteract;
-import io.stormbird.wallet.repository.EthereumNetworkRepositoryType;
-import io.stormbird.wallet.repository.TokenRepositoryType;
+import io.stormbird.wallet.entity.AmountUpdateCallback;
+import io.stormbird.wallet.entity.EIP681Request;
+import io.stormbird.wallet.entity.NetworkInfo;
+import io.stormbird.wallet.entity.Token;
+import io.stormbird.wallet.entity.Wallet;
+import io.stormbird.wallet.repository.EthereumNetworkRepository;
 import io.stormbird.wallet.ui.widget.entity.AmountEntryItem;
 import io.stormbird.wallet.util.KeyboardUtils;
 import io.stormbird.wallet.util.QRUtils;
-import org.web3j.utils.Convert;
+import io.stormbird.wallet.util.Utils;
+import io.stormbird.wallet.viewmodel.MyAddressViewModel;
+import io.stormbird.wallet.viewmodel.MyAddressViewModelFactory;
+import io.stormbird.wallet.widget.SelectNetworkDialog;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
-import static io.stormbird.wallet.C.EXTRA_CONTRACT_ADDRESS;
 import static io.stormbird.wallet.C.Key.WALLET;
+import static io.stormbird.wallet.C.RESET_WALLET;
 
 public class MyAddressActivity extends BaseActivity implements View.OnClickListener, AmountUpdateCallback
 {
     public static final String KEY_ADDRESS = "key_address";
+    public static final String KEY_MODE = "mode";
+    public static final int MODE_ADDRESS = 100;
+    public static final int MODE_POS = 101;
 
     @Inject
-    protected EthereumNetworkRepositoryType ethereumNetworkRepository;
-    @Inject
-    protected TokenRepositoryType tokenRepository;
+    MyAddressViewModelFactory myAddressViewModelFactory;
+    private MyAddressViewModel viewModel;
 
     private Wallet wallet;
     private String displayAddress;
     private Token token;
-
     private ImageView qrImageView;
     private TextView titleView;
+    private TextView address;
     private Button copyButton;
     private LinearLayout inputAmount;
     private LinearLayout selectAddress;
-
+    private TextView currentNetwork;
+    private RelativeLayout selectNetworkLayout;
+    private View networkIcon;
+    private LinearLayout layoutHolder;
     private AmountEntryItem amountInput = null;
+    private NetworkInfo networkInfo;
+    private int currentMode = MODE_ADDRESS;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         AndroidInjection.inject(this);
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_my_address);
+        initViews();
+        initViewModel();
+        getInfo();
+        getPreviousMode();
+    }
 
+    private void getPreviousMode() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            int mode = intent.getIntExtra(KEY_MODE, MODE_ADDRESS);
+            if (mode == MODE_POS) {
+                showPointOfSaleMode();
+            }
+        }
+    }
+
+    private void initViews() {
+        toolbar();
+        setTitle(getString(R.string.empty));
         titleView = findViewById(R.id.title_my_address);
         copyButton = findViewById(R.id.copy_action);
-
-        toolbar();
-
-        setTitle(getString(R.string.empty));
-
-        getInfo();
-
+        currentNetwork = findViewById(R.id.current_network);
+        selectNetworkLayout = findViewById(R.id.select_network_layout);
+        selectNetworkLayout.setOnClickListener(v -> selectNetwork());
         inputAmount = findViewById(R.id.layout_define_request);
         selectAddress = findViewById(R.id.layout_select_address);
-
-        ((TextView) findViewById(R.id.address)).setText(displayAddress);
+        address =  findViewById(R.id.address);
         copyButton.setOnClickListener(this);
+        qrImageView = findViewById(R.id.qr_image);
+        inputAmount = findViewById(R.id.layout_define_request);
+        selectAddress = findViewById(R.id.layout_select_address);
+        layoutHolder = findViewById(R.id.layout_holder);
+        networkIcon = findViewById(R.id.network_icon);
+    }
+
+    private void initViewModel() {
+        viewModel = ViewModelProviders.of(this, myAddressViewModelFactory).get(MyAddressViewModel.class);
+        viewModel.defaultNetwork().observe(this, this::onDefaultNetwork);
+        viewModel.prepare();
+    }
+
+    private void onDefaultNetwork(NetworkInfo networkInfo) {
+        this.networkInfo = networkInfo;
+        currentNetwork.setText(networkInfo.name);
+        Utils.setChainColour(networkIcon, networkInfo.chainId);
     }
 
     @Override
     public void onResume()
     {
         super.onResume();
-        inputAmount = findViewById(R.id.layout_define_request);
-        selectAddress = findViewById(R.id.layout_select_address);
-
-        findViewById(R.id.layout_holder).setOnClickListener(view -> {
+        layoutHolder.setOnClickListener(view -> {
             if (getCurrentFocus() != null)
             {
                 KeyboardUtils.hideKeyboard(getCurrentFocus());
@@ -118,31 +158,68 @@ public class MyAddressActivity extends BaseActivity implements View.OnClickListe
     {
         switch (item.getItemId())
         {
-            case R.id.action_receive_payment:
+            case R.id.action_receive_payment: {
                 if (selectAddress.getVisibility() == View.VISIBLE)
                 {
-                    //Generate QR link for receive payment.
-                    //Initially just generate simple payment.
-                    //need ticker so user can see how much $USD the value is
-                    selectAddress.setVisibility(View.GONE);
-                    inputAmount.setVisibility(View.VISIBLE);
-                    amountInput = new AmountEntryItem(this, tokenRepository,
-                                                      ethereumNetworkRepository.getDefaultNetwork().symbol, true, ethereumNetworkRepository.getDefaultNetwork().chainId);
-                    amountInput.getValue();
+                    showPointOfSaleMode();
+                    currentMode = MODE_POS;
                 }
                 else
                 {
-                    KeyboardUtils.hideKeyboard(getCurrentFocus());
-                    selectAddress.setVisibility(View.VISIBLE);
-                    inputAmount.setVisibility(View.GONE);
-                    amountInput.onClear();
-                    amountInput = null;
-                    onWindowFocusChanged(true);
+                    showAddress();
+                    currentMode = MODE_ADDRESS;
                 }
                 break;
+            }
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showPointOfSaleMode() {
+        //Generate QR link for receive payment.
+        //Initially just generate simple payment.
+        //need ticker so user can see how much $USD the value is
+        selectAddress.setVisibility(View.GONE);
+        inputAmount.setVisibility(View.VISIBLE);
+        amountInput = new AmountEntryItem(
+                this,
+                viewModel.getTokenRepository(),
+                viewModel.getEthereumNetworkRepository().getDefaultNetwork().symbol,
+                true,
+                viewModel.getEthereumNetworkRepository().getDefaultNetwork().chainId);
+        amountInput.getValue();
+    }
+
+    private void showAddress() {
+        if (getCurrentFocus() != null) {
+            KeyboardUtils.hideKeyboard(getCurrentFocus());
+        }
+        selectAddress.setVisibility(View.VISIBLE);
+        inputAmount.setVisibility(View.GONE);
+        if (amountInput != null) {
+            amountInput.onClear();
+            amountInput = null;
+        }
+        onWindowFocusChanged(true);
+    }
+
+    private void selectNetwork() {
+        SelectNetworkDialog dialog = new SelectNetworkDialog(this, viewModel.getNetworkList(), networkInfo.name);
+        dialog.setOnClickListener(v1 -> {
+            viewModel.setNetwork(dialog.getSelectedItem());
+
+            // restart activity
+            if (!networkInfo.name.equals(dialog.getSelectedItem())) {
+                Intent intent = getIntent();
+                intent.putExtra(KEY_MODE, currentMode);
+                finish();
+                startActivity(intent);
+            }
+            sendBroadcast(new Intent(RESET_WALLET));
+            dialog.dismiss();
+        });
+        dialog.show();
     }
 
     @Override
@@ -154,7 +231,6 @@ public class MyAddressActivity extends BaseActivity implements View.OnClickListe
             if (amountInput == null)
             {
                 getInfo();
-                qrImageView = findViewById(R.id.qr_image);
                 qrImageView.setImageBitmap(QRUtils.createQRImage(this, displayAddress, qrImageView.getWidth()));
                 qrImageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
             }
@@ -173,6 +249,7 @@ public class MyAddressActivity extends BaseActivity implements View.OnClickListe
         displayAddress = wallet.address;
         titleView.setText(R.string.my_wallet_address);
         copyButton.setText(R.string.copy_wallet_address);
+        address.setText(displayAddress);
     }
 
     @Override
@@ -191,7 +268,7 @@ public class MyAddressActivity extends BaseActivity implements View.OnClickListe
         //generate payment request link
         //EIP681 format
         BigDecimal weiAmount = Convert.toWei(newAmount, Convert.Unit.ETHER);
-        EIP681Request request = new EIP681Request(displayAddress, ethereumNetworkRepository.getDefaultNetwork().chainId, weiAmount);
+        EIP681Request request = new EIP681Request(displayAddress, viewModel.getEthereumNetworkRepository().getDefaultNetwork().chainId, weiAmount);
         String eip681String = request.generateRequest();
         qrImageView.setImageBitmap(QRUtils.createQRImage(this, eip681String, qrImageView.getWidth()));
     }

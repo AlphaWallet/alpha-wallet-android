1b45b2925772a15692fceecdd2779ea5e4b4f5ce
colourful-land <a@colourful.land>
2019-08-14 23:13:32 +1000

    remove the text of the Settings -> Help & FAQ's blank region (which can still be clicked) so this PR can be merged to master without visible damage

--- app/src/main/java/io/stormbird/wallet/ui/AWSLambdaInterface.java
+++ app/src/main/java/io/stormbird/wallet/ui/AWSLambdaInterface.java
@@ -6,8 +6,11 @@ import io.stormbird.token.tools.TokenScriptTrustAddressRequest;
 public interface AWSLambdaInterface {
 
     /**
-     * Invoke the Lambda function "AndroidBackendLambdaFunction".
+     * Invoke the Lambda function "TokenScriptTrustAddress".
      * The function name is the method name.
+     * Note that this Lambda function actually is the io.stormbird.token.tools.TokenScriptTrustAddress class
+     * I named the Lambda function after the class name to make it appear less confusing
+     * -- Weiwu
      */
     @LambdaFunction
     String TokenScriptTrustAddress(TokenScriptTrustAddressRequest request);

42f77f0eaf51bc461bbe0b670811f7d2affad539
colourful-land <a@colourful.land>
2019-08-16 12:33:14 +1000

    make AWS Lambda accept DigestValue instead of the entire TokenScript because it's not needed that way anyway.

--- lib/src/main/java/io/stormbird/token/tools/TrustAddressGenerator.java
+++ lib/src/main/java/io/stormbird/token/tools/TrustAddressGenerator.java
@@ -4,7 +4,6 @@ import org.bouncycastle.asn1.x9.X9ECParameters;
 import org.bouncycastle.crypto.ec.CustomNamedCurves;
 import org.bouncycastle.crypto.params.ECDomainParameters;
 import org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util;
-import org.bouncycastle.util.encoders.Base64;
 import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.ECPointUtil;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
@@ -13,22 +12,21 @@ import org.bouncycastle.math.ec.ECCurve;
 import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.jcajce.provider.digest.Keccak;
 import org.bouncycastle.util.encoders.Hex;
-import org.xml.sax.SAXException;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.math.BigInteger;
 import java.security.*;
 import java.security.interfaces.ECPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
-import javax.xml.crypto.MarshalException;
-import javax.xml.crypto.dsig.XMLSignature;
-import javax.xml.crypto.dsig.XMLSignatureException;
-import javax.xml.parsers.ParserConfigurationException;
 
+/***** WARNING *****
+ *
+ * TrustAddress can be generated without the TokenScript being
+ * signed. It's digest is produced in the way "as if tokenscript is
+ * signed", therefore please do not add logic like extracting
+ * <SignedInfo> from the TokenScript assuming it's signed.
+ * - Weiwu
+ */
 public class TrustAddressGenerator {
     private static final X9ECParameters CURVE_PARAMS = CustomNamedCurves.getByName("secp256k1");
     private static final ECDomainParameters CURVE = new ECDomainParameters(CURVE_PARAMS.getCurve(), CURVE_PARAMS.getG(),
@@ -36,13 +34,11 @@ public class TrustAddressGenerator {
 
     public static final byte[] masterPubKey = Hex.decode("04f0985bd9dbb6f461adc994a0c12595716a7f4fb2879bfc5155dffec3770096201c13f8314b46db8d8177887f8d95af1f2dd217291ce6ffe9183681186696bbe5");
 
-    public static String getTrustAddress(String contractAddress, InputStream tokenscript) throws Exception {
-        String digest = getXMLDSigSignedInfoDigest(tokenscript);
+    public static String getTrustAddress(String contractAddress, String digest) throws Exception {
         return preimageToAddress((contractAddress + "TRUST" + digest).getBytes());
     }
 
-    public static String getRevokeAddress(String contractAddress, InputStream tokenscript) throws Exception {
-        String digest = getXMLDSigSignedInfoDigest(tokenscript);
+    public static String getRevokeAddress(String contractAddress, String digest) throws Exception {
         return preimageToAddress((contractAddress + "REVOKE" + digest).getBytes());
     }
 
@@ -61,51 +57,6 @@ public class TrustAddressGenerator {
         ECPoint donatePKPoint = extractPublicKey(decodeKey(masterPubKey));
         ECPoint digestPKPoint = donatePKPoint.multiply(keyDerivationFactor);
         return getAddress(digestPKPoint);
-    }
-
-    private static String getXMLDSigSignedInfoDigest(InputStream tsmlFile) throws MarshalException, ParserConfigurationException, SAXException, XMLSignatureException, IOException {
-        XMLDSigVerifier sigVerifier = new XMLDSigVerifier();
-        XMLSignature signature = sigVerifier.getValidXMLSignature(tsmlFile);
-        InputStream digest = signature.getSignedInfo().getCanonicalizedData();
-        return convertHexToBase64String(Numeric.toHexString(getBytesFromInputStream(digest)));
-    }
-
-    private static byte[] getBytesFromInputStream(InputStream is) throws IOException {
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        byte[] buffer = new byte[0xFFFF];
-        for (int len = is.read(buffer); len != -1; len = is.read(buffer)) {
-            os.write(buffer, 0, len);
-        }
-        return os.toByteArray();
-    }
-
-    private static String convertHexToBase64String(String input) throws IOException {
-        byte barr[] = new byte[16];
-        int bcnt = 0;
-        for (int i = 0; i < 32; i += 2) {
-            char c1 = input.charAt(i);
-            char c2 = input.charAt(i + 1);
-            int i1 = convertCharToInt(c1);
-            int i2 = convertCharToInt(c2);
-            barr[bcnt] = 0;
-            barr[bcnt] |= (byte) ((i1 & 0x0F) << 4);
-            barr[bcnt] |= (byte) (i2 & 0x0F);
-            bcnt++;
-        }
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        Base64.encode(barr, outputStream);
-        return outputStream.toString();
-    }
-
-    private static int convertCharToInt(char c) {
-        char[] carr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-        char clower = Character.toLowerCase(c);
-        for (int i = 0; i < carr.length; i++) {
-            if (clower == carr[i]) {
-                return i;
-            }
-        }
-        return 0;
     }
 
     private static ECPoint extractPublicKey(ECPublicKey ecPublicKey) {
@@ -145,44 +96,35 @@ public class TrustAddressGenerator {
 
     /***************** Below are for making this class usable with Amazon Lambda *******************/
 
-    // a public zero-argument constructor is required by Amazon Lambda
-    // object created in this fashion is un-initialised, only suited for handleRequest which has its own initialisation code
-    //public TokenScriptTrustAddress() throws NoSuchProviderException, NoSuchAlgorithmException {
-    //}
-
     public Response DeriveTrustAddress(Request req) throws Exception {
-        InputStream targetStream = new ByteArrayInputStream(req.getTokenScript().getBytes());
-        String trust = getTrustAddress(req.contractAddress, targetStream);
-        targetStream.close();
-        targetStream = new ByteArrayInputStream(req.getTokenScript().getBytes());
-        String revoke = getRevokeAddress(req.contractAddress, targetStream);
-        targetStream.close();
+        String trust = getTrustAddress(req.contract, req.getDigest());
+        String revoke = getRevokeAddress(req.contract, req.getDigest());
         return new Response(trust, revoke);
     }
 
     public static class Request {
-        String contractAddress;
-        String tokenScript;
+        String contract;
+        String digest;
 
         public String getContractAddress() {
-            return contractAddress;
+            return contract;
         }
 
         public void setContractAddress(String contractAddress) {
-            this.contractAddress = contractAddress;
+            this.contract = contractAddress;
         }
 
-        public String getTokenScript() {
-            return tokenScript;
+        public String getDigest() {
+            return digest;
         }
 
-        public void setTokenScript(String tokenScript) {
-            this.tokenScript = tokenScript;
+        public void setDigest(String digest) {
+            this.digest = digest;
         }
 
-        public Request(String contractAddress, String tokenScript) {
-            this.contractAddress = contractAddress;
-            this.tokenScript = tokenScript;
+        public Request(String contractAddress, String digest) {
+            this.contract = contractAddress;
+            this.digest = digest;
         }
 
         public Request() {

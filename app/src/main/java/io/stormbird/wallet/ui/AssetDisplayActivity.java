5317889ebd0c102567e464bdcfa52a34d43ee1a0
James Brown <soylentblue@hotmail.co.uk>
2019-09-17 13:24:20 +0800

    Refactor Action Button bar with drop-in display and function class that works with various views.

--- app/src/main/java/io/stormbird/wallet/ui/AssetDisplayActivity.java
+++ app/src/main/java/io/stormbird/wallet/ui/AssetDisplayActivity.java
@@ -16,6 +16,7 @@ import javax.inject.Inject;
 
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import dagger.android.AndroidInjection;
@@ -27,6 +28,7 @@ import io.stormbird.wallet.C;
 import io.stormbird.wallet.R;
 import io.stormbird.wallet.entity.ERC721Token;
 import io.stormbird.wallet.entity.FinishReceiver;
+import io.stormbird.wallet.entity.StandardFunctionInterface;
 import io.stormbird.wallet.entity.Ticket;
 import io.stormbird.wallet.entity.Token;
 import io.stormbird.wallet.entity.TokenInfo;
@@ -36,6 +38,7 @@ import io.stormbird.wallet.viewmodel.AssetDisplayViewModel;
 import io.stormbird.wallet.viewmodel.AssetDisplayViewModelFactory;
 import io.stormbird.wallet.widget.AWalletAlertDialog;
 import io.stormbird.wallet.widget.CertifiedToolbarView;
+import io.stormbird.wallet.widget.FunctionButtonBar;
 import io.stormbird.wallet.widget.ProgressView;
 import io.stormbird.wallet.widget.SystemView;
 
@@ -55,7 +58,7 @@ import static io.stormbird.wallet.C.Key.WALLET;
 /**
  *
  */
-public class AssetDisplayActivity extends BaseActivity implements OnTokenClickListener, View.OnClickListener, Runnable
+public class AssetDisplayActivity extends BaseActivity implements StandardFunctionInterface
 {
     @Inject
     protected AssetDisplayViewModelFactory assetDisplayViewModelFactory;
@@ -65,12 +68,10 @@ public class AssetDisplayActivity extends BaseActivity implements OnTokenClickLi
     private RecyclerView list;
     private FinishReceiver finishReceiver;
     private CertifiedToolbarView toolbarView;
+    private FunctionButtonBar functionBar;
     private Token token;
     private NonFungibleTokenAdapter adapter;
     private String balance = null;
-    private List<BigInteger> selection = new ArrayList<>();
-    private Handler handler;
-    private boolean activeClick;
     private AWalletAlertDialog dialog;
 
     @Override
@@ -105,23 +106,15 @@ public class AssetDisplayActivity extends BaseActivity implements OnTokenClickLi
         viewModel.ticket().observe(this, this::onTokenUpdate);
         viewModel.sig().observe(this, this::onSigData);
 
-        adapter = new NonFungibleTokenAdapter(this, token, viewModel.getAssetDefinitionService(), viewModel.getOpenseaService());
-        if (token instanceof ERC721Token)
-        {
-            findViewById(R.id.button_use).setVisibility(View.GONE);
-            findViewById(R.id.button_sell).setVisibility(View.GONE);
-        }
-
-        findViewById(R.id.button_transfer).setOnClickListener(this);
-        findViewById(R.id.button_use).setOnClickListener(this);
+        functionBar = findViewById(R.id.layoutButtons);
+        adapter = new NonFungibleTokenAdapter(functionBar, token, viewModel.getAssetDefinitionService(), viewModel.getOpenseaService());
+        functionBar.setupFunctions(this, viewModel.getAssetDefinitionService(), token, adapter);
 
         list.setLayoutManager(new LinearLayoutManager(this));
-        findViewById(R.id.button_sell).setOnClickListener(this);
         list.setAdapter(adapter);
         list.setHapticFeedbackEnabled(true);
 
         finishReceiver = new FinishReceiver(this);
-        checkForTokenScript();
         findViewById(R.id.certificate_spinner).setVisibility(View.VISIBLE);
         viewModel.checkTokenScriptValidity(token);
     }
@@ -136,38 +129,12 @@ public class AssetDisplayActivity extends BaseActivity implements OnTokenClickLi
         adapter.notifyItemChanged(0); //notify issuer update
     }
 
-    /**
-     * Hide the button bar, and check if it's not ERC875: if not then hide the 'use' button.
-     * TODO: need to populate button bar as per ERC20 button bar
-     */
-    private void checkForTokenScript()
-    {
-        findViewById(R.id.layoutButtons).setVisibility(View.GONE);
-
-        final Button[] buttons = new Button[3];
-        buttons[1] = findViewById(R.id.button_use);
-        buttons[0] = findViewById(R.id.button_sell);
-
-        Map<String, TSAction> functions = viewModel.getAssetDefinitionService().getTokenFunctionMap(token.tokenInfo.chainId, token.getAddress());
-        if (functions != null && functions.size() > 0)
-        {
-            int index = 0;
-            for (String function : functions.keySet())
-            {
-                buttons[index].setVisibility(View.VISIBLE);
-                buttons[index].setText(function);
-                index++;
-            }
-        }
-    }
-
     @Override
     protected void onResume()
     {
         super.onResume();
         viewModel.prepare(token);
-        handler = new Handler();
-        activeClick = false;
+        if (functionBar == null) functionBar.setupFunctions(this, viewModel.getAssetDefinitionService(), token, adapter);
     }
 
     @Override
@@ -217,156 +184,45 @@ public class AssetDisplayActivity extends BaseActivity implements OnTokenClickLi
     }
 
     @Override
-    public void onClick(View v)
+    public void selectRedeemTokens(List<BigInteger> selection)
     {
-        if (!activeClick)
-        {
-            activeClick = true;
-            handler.postDelayed(this, 500);
-
-            switch (v.getId())
-            {
-                case R.id.button_transfer:
-                {
-                    viewModel.showTransferToken(this, token, selection);
-                }
-                break;
-
-                default:
-                    Button b = findViewById(v.getId());
-                    handleUseClick(b.getText().toString(), v.getId());
-                    break;
-            }
-        }
-    }
-
-    private boolean hasCorrectTokens(TSAction action)
-    {
-        //get selected tokens
-        List<BigInteger> selected = adapter.getSelectedTokenIds(selection);
-        int groupings = adapter.getSelectedGroups();
-        if (action.function != null)
-        {
-            int requiredCount = action.function.getTokenRequirement();
-            if (requiredCount == 1 && selected.size() > 1 && groupings == 1)
-            {
-                BigInteger first = selected.get(0);
-                selected.clear();
-                selected.add(first);
-            }
-
-            return selected.size() == requiredCount;
-        }
-        return true;
-    }
-
-    private void handleUseClick(String function, int useId)
-    {
-        //first see if this is override function
-        Map<String, TSAction> functions = viewModel.getAssetDefinitionService().getTokenFunctionMap(token.tokenInfo.chainId, token.getAddress());
-        if (functions != null && functions.containsKey(function))
-        {
-            TSAction action = functions.get(function);
-            //ensure we have sufficient tokens for selection
-            if (!hasCorrectTokens(action))
-            {
-                if (dialog == null) dialog = new AWalletAlertDialog(this);
-                dialog.setIcon(AWalletAlertDialog.ERROR);
-                dialog.setTitle(R.string.token_selection);
-                dialog.setMessage(getString(R.string.token_requirement, String.valueOf(action.function.getTokenRequirement())));
-                dialog.setButtonText(R.string.dialog_ok);
-                dialog.setButtonListener(v -> dialog.dismiss());
-                dialog.show();
-            }
-            else
-            {
-                //handle TS function
-                Intent intent = new Intent(this, FunctionActivity.class);
-                intent.putExtra(TICKET, token);
-                intent.putExtra(WALLET, viewModel.defaultWallet().getValue());
-                intent.putExtra(C.EXTRA_STATE, function);
-                intent.putExtra(C.EXTRA_TOKEN_ID, token.intArrayToString(adapter.getSelectedTokenIds(selection), true));
-                intent.setFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-                startActivity(intent);
-            }
-        }
-        else if (token.isERC875())
-        {
-            switch (useId)
-            {
-                case R.id.button_sell:
-                    viewModel.sellTicketRouter(this, token, token.intArrayToString(selection, false));
-                    break;
-                case R.id.button_use:
-                    viewModel.selectRedeemTokens(this, token, selection);
-                    break;
-                default:
-                    break;
-            }
-        }
+        viewModel.selectRedeemTokens(this, token, selection);
     }
 
     @Override
-    public void onTokenClick(View v, Token token, List<BigInteger> tokenIds, boolean selected)
+    public void sellTicketRouter(List<BigInteger> selection)
     {
-        Map<String, TSAction> functions = viewModel.getAssetDefinitionService().getTokenFunctionMap(token.tokenInfo.chainId, token.getAddress());
-        int maxSelect = 1;
-
-        if (!selected && tokenIds.containsAll(selection))
-        {
-            selection = new ArrayList<>();
-        }
-
-        if (!selected) return;
-
-        if (functions != null)
-        {
-            for (TSAction action : functions.values())
-            {
-                if (action.function != null && action.function.getTokenRequirement() > maxSelect)
-                {
-                    maxSelect = action.function.getTokenRequirement();
-                }
-            }
-        }
-
-        if (maxSelect <= 1)
-        {
-            selection = tokenIds;
-            adapter.setRadioButtons(true);
-        }
+        viewModel.sellTicketRouter(this, token, token.intArrayToString(selection, false));
     }
 
     @Override
-    public void onLongTokenClick(View view, Token token, List<BigInteger> tokenIds)
+    public void showTransferToken(List<BigInteger> selection)
     {
-        //show radio buttons of all token groups
-        adapter.setRadioButtons(true);
-
-        selection = tokenIds;
-        Vibrator vb = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        if (vb != null && vb.hasVibrator())
-        {
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                VibrationEffect vibe = VibrationEffect.createOneShot(200, DEFAULT_AMPLITUDE);
-                vb.vibrate(vibe);
-            }
-            else
-            {
-                //noinspection deprecation
-                vb.vibrate(200);
-            }
-        }
-
-        if (findViewById(R.id.layoutButtons).getVisibility() != View.VISIBLE)
-        {
-            findViewById(R.id.layoutButtons).setVisibility(View.VISIBLE);
-        }
+        viewModel.showTransferToken(this, token, selection);
     }
 
     @Override
-    public void run()
+    public void displayTokenSelectionError(TSAction action)
     {
-        activeClick = false;
+        if (dialog == null) dialog = new AWalletAlertDialog(this);
+        dialog.setIcon(AWalletAlertDialog.ERROR);
+        dialog.setTitle(R.string.token_selection);
+        dialog.setMessage(getString(R.string.token_requirement, String.valueOf(action.function.getTokenRequirement())));
+        dialog.setButtonText(R.string.dialog_ok);
+        dialog.setButtonListener(v -> dialog.dismiss());
+        dialog.show();
+    }
+
+    @Override
+    public void handleTokenScriptFunction(String function, List<BigInteger> selection)
+    {
+        //handle TS function
+        Intent intent = new Intent(this, FunctionActivity.class);
+        intent.putExtra(TICKET, token);
+        intent.putExtra(WALLET, viewModel.defaultWallet().getValue());
+        intent.putExtra(C.EXTRA_STATE, function);
+        intent.putExtra(C.EXTRA_TOKEN_ID, token.intArrayToString(adapter.getSelectedTokenIds(selection), true));
+        intent.setFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        startActivity(intent);
     }
 }
--- app/src/main/java/io/stormbird/wallet/ui/TokenFunctionActivity.java
+++ app/src/main/java/io/stormbird/wallet/ui/TokenFunctionActivity.java
@@ -14,11 +14,13 @@ import io.stormbird.token.entity.TSAction;
 import io.stormbird.token.entity.TicketRange;
 import io.stormbird.wallet.C;
 import io.stormbird.wallet.R;
+import io.stormbird.wallet.entity.StandardFunctionInterface;
 import io.stormbird.wallet.entity.Token;
 import io.stormbird.wallet.viewmodel.TokenFunctionViewModel;
 import io.stormbird.wallet.viewmodel.TokenFunctionViewModelFactory;
 import io.stormbird.wallet.web3.Web3TokenView;
 import io.stormbird.wallet.web3.entity.PageReadyCallback;
+import io.stormbird.wallet.widget.FunctionButtonBar;
 import io.stormbird.wallet.widget.ProgressView;
 import io.stormbird.wallet.widget.SystemView;
 
@@ -33,7 +35,7 @@ import static io.stormbird.wallet.C.Key.TICKET;
  * Created by James on 2/04/2019.
  * Stormbird in Singapore
  */
-public class TokenFunctionActivity extends BaseActivity implements View.OnClickListener, Runnable, PageReadyCallback
+public class TokenFunctionActivity extends BaseActivity implements StandardFunctionInterface, PageReadyCallback
 {
     @Inject
     protected TokenFunctionViewModelFactory tokenFunctionViewModelFactory;
@@ -41,9 +43,9 @@ public class TokenFunctionActivity extends BaseActivity implements View.OnClickL
 
     private Web3TokenView tokenView;
     private Token token;
-    private Handler handler;
     private List<BigInteger> idList = null;
     private StringBuilder attrs;
+    private FunctionButtonBar functionBar;
 
     private void initViews() {
         token = getIntent().getParcelableExtra(TICKET);
@@ -55,6 +57,8 @@ public class TokenFunctionActivity extends BaseActivity implements View.OnClickL
         TicketRange data = new TicketRange(idList, token.tokenInfo.address, false);
 
         token.displayTicketHolder(data, frameLayout, viewModel.getAssetDefinitionService(), this, false);
+        functionBar.setupFunctions(this, viewModel.getAssetDefinitionService(), token, null);
+        functionBar.revealButtons();
     }
 
     @Override
@@ -69,15 +73,15 @@ public class TokenFunctionActivity extends BaseActivity implements View.OnClickL
         ProgressView progressView = findViewById(R.id.progress_view);
         systemView.hide();
         progressView.hide();
+        functionBar = findViewById(R.id.layoutButtons);
 
         initViews();
         toolbar();
         setTitle(getString(R.string.token_function));
-        setupFunctions();
+
+        //setupFunctions();
         viewModel.startGasPriceUpdate(token.tokenInfo.chainId);
         viewModel.getCurrentWallet();
-
-        handler = new Handler();
     }
 
     private void setupFunctions()
@@ -87,7 +91,7 @@ public class TokenFunctionActivity extends BaseActivity implements View.OnClickL
         buttons[0] = findViewById(R.id.button_sell);
         buttons[2] = findViewById(R.id.button_transfer);
 
-        for (Button b : buttons) { b.setOnClickListener(this); }
+        //for (Button b : buttons) { b.setOnClickListener(this); }
 
         Map<String, TSAction> functions = viewModel.getAssetDefinitionService().getTokenFunctionMap(token.tokenInfo.chainId, token.getAddress());
         if (functions != null)
@@ -109,7 +113,6 @@ public class TokenFunctionActivity extends BaseActivity implements View.OnClickL
     public void onResume()
     {
         super.onResume();
-        handler.postDelayed(this, 300);
     }
 
     @Override
@@ -123,54 +126,77 @@ public class TokenFunctionActivity extends BaseActivity implements View.OnClickL
     }
 
     @Override
-    public void onClick(View v)
-    {
-        switch (v.getId())
-        {
-            case R.id.button_use:
-            {
-                viewModel.selectRedeemToken(this, token, idList);
-            }
-            break;
-            case R.id.button_sell:
-            {
-                Map<String, TSAction> functions = viewModel.getAssetDefinitionService().getTokenFunctionMap(token.tokenInfo.chainId, token.getAddress());
-                //this will be the user function
-                if (functions == null || functions.size() == 0)
-                {
-                    viewModel.openUniversalLink(this, token, token.intArrayToString(idList, false));
-                }
-                else
-                {
-                    String buttonText = ((Button) v).getText().toString();
-                    viewModel.showFunction(this, token, buttonText, idList);
-                }
-            }
-            break;
-            case R.id.button_transfer:
-            {
-                viewModel.showTransferToken(this, token, token.intArrayToString(idList, false));
-            }
-            break;
-        }
-    }
-
-    @Override
     public void onDestroy()
     {
         super.onDestroy();
         viewModel.stopGasSettingsFetch();
     }
 
-    @Override
-    public void run()
-    {
-        //adapter.notifyDataSetChanged();
-    }
 
     @Override
     public void onPageLoaded()
     {
         tokenView.callToJS("refresh()");
+    }
+
+    /*
+
+    @Override
+    public void onClick(View v)
+    {
+        Map<String, TSAction> functions = viewModel.getAssetDefinitionService().getTokenFunctionMap(token.tokenInfo.chainId, token.getAddress());
+        //this will be the user function
+        String buttonText = ((Button) v).getText().toString();
+        if (functions != null && functions.containsKey(buttonText))
+        {
+            viewModel.showFunction(this, token, buttonText, idList);
+        }
+        else
+        {
+            switch (v.getId())
+            {
+                case R.id.button_use:
+                    viewModel.selectRedeemToken(this, token, idList);
+                    break;
+                case R.id.button_sell:
+                    viewModel.openUniversalLink(this, token, token.intArrayToString(idList, false));
+                    break;
+                case R.id.button_transfer:
+                    viewModel.showTransferToken(this, token, token.intArrayToString(idList, false));
+                    break;
+            }
+        }
+    }
+
+     */
+
+    @Override
+    public void selectRedeemTokens(List<BigInteger> selection)
+    {
+        viewModel.selectRedeemToken(this, token, idList);
+    }
+
+    @Override
+    public void sellTicketRouter(List<BigInteger> selection)
+    {
+        viewModel.openUniversalLink(this, token, token.intArrayToString(idList, false));
+    }
+
+    @Override
+    public void showTransferToken(List<BigInteger> selection)
+    {
+        viewModel.showTransferToken(this, token, token.intArrayToString(idList, false));
+    }
+
+    @Override
+    public void displayTokenSelectionError(TSAction action)
+    {
+
+    }
+
+    @Override
+    public void handleTokenScriptFunction(String function, List<BigInteger> selection)
+    {
+        viewModel.showFunction(this, token, function, idList);
     }
 }

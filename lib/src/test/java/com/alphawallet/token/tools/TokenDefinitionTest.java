dfda68aae7d32d5b2a38a6acd4e1c522efe6134a
James Sangalli <j.l.sangalli@gmail.com>
2019-10-11 19:24:55 +1100

    clean up and add basic tests for the TokenScript client

--- lib/src/test/java/com/alphawallet/token/tools/TokenDefinitionTest.java
+++ lib/src/test/java/com/alphawallet/token/tools/TokenDefinitionTest.java
@@ -1,19 +1,14 @@
 package com.alphawallet.token.tools;
 
+import com.alphawallet.token.entity.ContractInfo;
+import com.alphawallet.token.entity.ParseResult;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.util.Locale;
-import java.util.stream.Stream;
-import com.alphawallet.token.entity.AttributeInterface;
-import com.alphawallet.token.entity.AttributeType;
-import com.alphawallet.token.entity.ContractAddress;
-import com.alphawallet.token.entity.ContractInfo;
-import com.alphawallet.token.entity.ParseResult;
-import com.alphawallet.token.entity.TransactionResult;
+import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
@@ -21,18 +16,14 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 
-public class TokenDefinitionTest implements AttributeInterface, ParseResult
+public class TokenDefinitionTest implements ParseResult
 {
-    Stream<BigInteger> ticketIDs = Stream.of(
-            "1015b4a28f000000000000000000000", "1015b4a28f000000000000000000000",
-            "1015b4a28f000000000000000000000", "1015b4a28f000000000000000000000"
-    ).map(hexstr -> new BigInteger(hexstr, 16));
-    File file = new File("src/test/ts/entrytoken.xml");
+    private File entryTokenTestFile = new File("src/test/ts/entrytoken.xml");
 
     @Test
     public void TokenInformationCanBeExtracted() throws IOException, SAXException {
-        assertTrue(file.exists());
-        TokenDefinition entryToken = new TokenDefinition(new FileInputStream(file), new Locale("en"), this);
+        assertTrue(entryTokenTestFile.exists());
+        TokenDefinition entryToken = new TokenDefinition(new FileInputStream(entryTokenTestFile), new Locale("en"), this);
         assertFalse(entryToken.attributeTypes.isEmpty());
         for (String contractName : entryToken.contracts.keySet())
         {
@@ -49,72 +40,15 @@ public class TokenDefinitionTest implements AttributeInterface, ParseResult
         {
             for (String address : ci.addresses.get(networkId))
             {
-                assertEquals(40+2, address.length());
+                assertEquals(40 + 2, address.length());
             }
         }
     }
 
-    @Test
-    public void AttributeTypesShouldParse() throws IOException, SAXException {
-        TokenDefinition entryToken = new TokenDefinition(new FileInputStream(file), new Locale("en"), this);
-        BigInteger tokenId = new BigInteger("1015b4a28f000000000000000000000", 16);
-
-        //StringBuilder tokenData = new StringBuilder();
-
-        //get first holding token
-        String holdingContract = entryToken.holdingToken;
-        ContractInfo ci = entryToken.contracts.get(holdingContract); //ropsten
-        ContractAddress cAddr = new ContractAddress(3, ci.addresses.get(3).get(0));
-
-        //Have to move this test to app or DMZ test suite as need to supply ethereum interface
-        //supplying a web3j interface here will clash either with DMZ's Java or App's Android web3j.
-        //Could just supply a direct call to eth via HTTPS or hard code the expected result as a call
-        //test fetching street attribute
-        /*TokenScriptResult.Attribute streetAttr = entryToken.fetchAttrResult("street", tokenId, cAddr, this).blockingFirst();
-
-        assertNotNull(streetAttr);
-        assertNotNull(streetAttr.text);
-        assertTrue(streetAttr.text.length() > 0);*/
-    }
-
-    @Test
-    public void XMLSignatureShouldValidate() throws IOException, SAXException {
-        TokenDefinition entryToken = new TokenDefinition(new FileInputStream(file), new Locale("en"), this);
-        assertEquals("EntryToken", entryToken.holdingToken);
-        // TODO: actually validate XML signature
-    }
-
     @Test(expected = SAXException.class)
     public void BadLocaleShouldThrowException() throws IOException, SAXException {
-        TokenDefinition ticketAsset = new TokenDefinition(new FileInputStream(file), new Locale("asdf"), this);
-    }
-
-
-
-    //No caching or optimisation for tests
-
-    @Override
-    public TransactionResult getFunctionResult(ContractAddress contract, AttributeType attr, BigInteger tokenId)
-    {
-        return new TransactionResult(contract.chainId, contract.address, tokenId, attr);
-    }
-
-    @Override
-    public TransactionResult storeAuxData(TransactionResult tResult)
-    {
-        return tResult;
-    }
-
-    @Override
-    public boolean resolveOptimisedAttr(ContractAddress contract, AttributeType attr, TransactionResult transactionResult)
-    {
-        return false;
-    }
-
-    @Override
-    public String getWalletAddr()
-    {
-        return "0x00000000000000000000";
+        TokenDefinition ticketAsset = new TokenDefinition(new FileInputStream(entryTokenTestFile), new Locale("asdf"), this);
+        assertNotNull(ticketAsset);
     }
 
     @Override

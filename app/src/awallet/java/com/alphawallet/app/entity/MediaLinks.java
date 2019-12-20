9e98f375398a7f2c2650cdc827b4392b3790ede7
James Brown <soylentblue@hotmail.co.uk>
2019-12-20 14:23:54 +1000

    Add Reddit link to settings

--- app/src/awallet/java/com/alphawallet/app/entity/MediaLinks.java
+++ app/src/awallet/java/com/alphawallet/app/entity/MediaLinks.java
@@ -8,6 +8,7 @@ public class MediaLinks
     public static final String AWALLET_TWITTER_URL = "https://twitter.com/AlphaWallet";
     public static final String AWALLET_FACEBOOK_URL = "https://www.facebook.com/AlphaWallet/";
     public static final String AWALLET_LINKEDIN_URL = null;
+    public static final String AWALLET_REDDIT_URL = "https://www.reddit.com/r/AlphaWallet/";
 
     public static final String AWALLET_EMAIL1 = "feedback+android";
     public static final String AWALLET_EMAIL2 = "alphawallet.com";

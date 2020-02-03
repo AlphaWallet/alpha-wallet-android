02271fb3e66cff2fd477ec8db07001f1c1fbce52
James Sangalli <j.l.sangalli@gmail.com>
2020-02-03 14:17:34 +1100

    uses TS name rather than contract name

--- app/src/main/java/com/alphawallet/app/entity/tokens/ERC721Token.java
+++ app/src/main/java/com/alphawallet/app/entity/tokens/ERC721Token.java
@@ -129,7 +129,7 @@ public class ERC721Token extends Token implements Parcelable
 
         holder.balanceEth.setVisibility(View.VISIBLE);
 
-        addTokenName(holder);
+        addTokenName(holder, definition);
     }
 
     @Override

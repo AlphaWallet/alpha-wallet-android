f187d16c5a105e9ed6e03077cf272045b1564eca
James Brown <soylentblue@hotmail.co.uk>
2020-02-04 21:08:44 +1100

    cache the certificate data; update at periodic intervals or if the file hash changes.

---
+++ app/src/main/java/com/alphawallet/app/repository/entity/RealmCertificateData.java
@@ -0,0 +1,123 @@
+package com.alphawallet.app.repository.entity;
+
+import com.alphawallet.token.entity.SigReturnType;
+import com.alphawallet.token.entity.XMLDsigDescriptor;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by JB on 4/02/2020.
+ */
+public class RealmCertificateData extends RealmObject
+{
+    @PrimaryKey
+    private String instanceKey; //should be token address and tokenID
+    private String result;
+    private String subject;
+    private String keyName;
+    private String keyType;
+    private String issuer;
+    private String certificateName;
+    private int type;
+
+    public String getInstanceKey()
+    {
+        return instanceKey;
+    }
+
+    public void setInstanceKey(String instanceKey)
+    {
+        this.instanceKey = instanceKey;
+    }
+
+    public void setFromSig(XMLDsigDescriptor sig)
+    {
+        result = sig.result;
+        subject = sig.subject;
+        keyName = sig.keyName;
+        keyType = sig.keyType;
+        issuer = sig.issuer;
+        certificateName = sig.certificateName;
+        if (sig.type == null)
+        {
+            if (sig.result != null && sig.result.equals("pass")) type = SigReturnType.SIGNATURE_PASS.ordinal();
+            else type = SigReturnType.SIGNATURE_INVALID.ordinal();
+        }
+        else
+        {
+            type = sig.type.ordinal();
+        }
+    }
+
+    public String getResult()
+    {
+        return result;
+    }
+
+    public void setResult(String result)
+    {
+        this.result = result;
+    }
+
+    public String getKeyName()
+    {
+        return keyName;
+    }
+
+    public void setKeyName(String keyName)
+    {
+        this.keyName = keyName;
+    }
+
+    public String getSubject()
+    {
+        return subject;
+    }
+
+    public void setSubject(String subject)
+    {
+        this.subject = subject;
+    }
+
+    public SigReturnType getType()
+    {
+        return SigReturnType.values()[type];
+    }
+
+    public void setType(SigReturnType type)
+    {
+        if (type == null && this.result.equals("pass"))
+        this.type = type.ordinal();
+    }
+
+    public String getIssuer()
+    {
+        return issuer;
+    }
+
+    public void setIssuer(String issuer)
+    {
+        this.issuer = issuer;
+    }
+
+    public String getCertificateName()
+    {
+        return certificateName;
+    }
+
+    public void setCertificateName(String certificateName)
+    {
+        this.certificateName = certificateName;
+    }
+
+    public String getKeyType()
+    {
+        return keyType;
+    }
+
+    public void setKeyType(String keyType)
+    {
+        this.keyType = keyType;
+    }
+}
--- app/src/main/java/com/alphawallet/app/service/AWRealmMigration.java
+++ app/src/main/java/com/alphawallet/app/service/AWRealmMigration.java
@@ -1,6 +1,9 @@
 package com.alphawallet.app.service;
 
+import com.alphawallet.app.repository.entity.RealmCertificateData;
+
 import io.realm.DynamicRealm;
+import io.realm.FieldAttribute;
 import io.realm.RealmMigration;
 import io.realm.RealmObjectSchema;
 import io.realm.RealmSchema;
@@ -22,6 +25,21 @@ public class AWRealmMigration implements RealmMigration
         {
             RealmObjectSchema realmToken = schema.get("RealmToken");
             if (!realmToken.hasField("lastTxTime")) realmToken.addField("lastTxTime", long.class); //add the last transaction update time, used to check tokenscript cached result validity
+            oldVersion++;
+        }
+
+        //Version 6
+        if (oldVersion == 6)
+        {
+            schema.create("RealmCertificateData")
+                    .addField("instanceKey", String.class, FieldAttribute.PRIMARY_KEY)
+                    .addField("result", String.class, FieldAttribute.INDEXED)
+                    .addField("subject", String.class, FieldAttribute.INDEXED)
+                    .addField("keyName", String.class, FieldAttribute.INDEXED)
+                    .addField("keyType", String.class, FieldAttribute.INDEXED)
+                    .addField("issuer", String.class, FieldAttribute.INDEXED)
+                    .addField("certificateName", String.class, FieldAttribute.INDEXED)
+                    .addField("type", int.class, FieldAttribute.INDEXED);
             oldVersion++;
         }
     }

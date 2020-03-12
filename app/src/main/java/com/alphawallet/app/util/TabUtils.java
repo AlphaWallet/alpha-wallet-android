0baedeacb42410ca783cb1537ee71ff6f45a5521
justindg <jrbdeguzman@gmail.com>
2020-03-12 21:59:02 +0800

    Add util to decorate tab layout

--- app/src/main/java/com/alphawallet/app/util/TabUtils.java
+++ app/src/main/java/com/alphawallet/app/util/TabUtils.java
@@ -2,44 +2,53 @@ package com.alphawallet.app.util;
 
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.res.ResourcesCompat;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.alphawallet.app.R;
 
 public class TabUtils {
-    public static void changeTabsFont(Context context, TabLayout tabLayout) {
-        try {
-            Typeface typeface = null;
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O_MR1) {
-                typeface = context.getResources().getFont(R.font.font_regular);
-            } else {
-                typeface = ResourcesCompat.getFont(context, R.font.font_regular);
+    public static void setSelectedTabFont(TabLayout tabLayout, TabLayout.Tab tab, Typeface typeface) {
+        LinearLayout layout = (LinearLayout) ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(tab.getPosition());
+        TextView tabTextView = (TextView) layout.getChildAt(1);
+        if (tabTextView != null) {
+            tabTextView.setTypeface(typeface);
+        }
+    }
+
+    public static void decorateTabLayout(Context context, TabLayout tabLayout) {
+        if (tabLayout.getTabCount() <= 3) {
+            tabLayout.setTabMode(TabLayout.MODE_FIXED);
+            tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        } else {
+            tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+            tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        }
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                setSelectedTabFont(tabLayout, tab, ResourcesCompat.getFont(context, R.font.font_semibold));
             }
-            ViewGroup vg = (ViewGroup) tabLayout.getChildAt(0);
-            int tabsCount = vg.getChildCount();
-            for (int j = 0; j < tabsCount; j++) {
-                ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
-                int tabChildsCount = vgTab.getChildCount();
-                for (int i = 0; i < tabChildsCount; i++) {
-                    View tabViewChild = vgTab.getChildAt(i);
-                    if (tabViewChild instanceof TextView) {
-                        ((TextView) tabViewChild).setTypeface(typeface);
-                    }
-                }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                setSelectedTabFont(tabLayout, tab, ResourcesCompat.getFont(context, R.font.font_regular));
             }
-        } catch (Resources.NotFoundException nfe) {
-            Log.e(TabUtils.class.getSimpleName(), nfe.getMessage(), nfe);
-        } catch (NullPointerException npe) {
-            Log.e(TabUtils.class.getSimpleName(), npe.getMessage(), npe);
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        TabLayout.Tab firstTab = tabLayout.getTabAt(0);
+        if (firstTab != null) {
+            TabUtils.setSelectedTabFont(tabLayout, firstTab, ResourcesCompat.getFont(context, R.font.font_semibold));
         }
     }
 }

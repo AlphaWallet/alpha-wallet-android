7d9cbe34fc0e788a7d0e171f1d58ea62d67bb491
James Brown <soylentblue@hotmail.co.uk>
2020-05-12 13:36:18 +1000

    Update DMZ to web3j 4.5.17 and update all tests.

--- app/src/test/java/com/alphawallet/app/UniversalLinkTypeTest.java
+++ app/src/test/java/com/alphawallet/app/UniversalLinkTypeTest.java
@@ -113,7 +113,7 @@ public class UniversalLinkTypeTest
         try {
             System.arraycopy(sigData.getR(), 0, sig, 0, 32);
             System.arraycopy(sigData.getS(), 0, sig, 32, 32);
-            sig[64] = sigData.getV();//[0];
+            System.arraycopy(sigData.getV(), 0, sig, 64, 1);
         } catch (IndexOutOfBoundsException e) {
             throw new SalesOrderMalformed("Signature shorter than expected 256");
         }

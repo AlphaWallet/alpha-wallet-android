d4ee0a0146adc9a6486c1a3587d421e9718a7e07
James Brown <soylentblue@hotmail.co.uk>
2020-06-13 16:17:24 +1000

    Remove deprecated files

--- app/src/main/java/com/alphawallet/app/util/VerifyXMLDSig.java
+++
@@ -1,8 +0,0 @@
-package com.alphawallet.app.util;
-
-import com.amazonaws.mobileconnectors.lambdainvoker.LambdaFunction;
-
-public interface VerifyXMLDSig {
-    @LambdaFunction
-    com.alphawallet.token.tools.VerifyXMLDSig.Response VerifyTSMLFile(com.alphawallet.token.tools.VerifyXMLDSig.Request request);
-}

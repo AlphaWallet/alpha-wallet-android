e60854ceb170c81c41169a96244ae30dc77ab5c1
James Brown <soylentblue@hotmail.co.uk>
2020-07-12 16:44:17 +1000

    Minimal implementation for add tokens - subset of the add_update_revamp

--- app/src/main/java/com/alphawallet/app/ui/widget/adapter/TokenListAdapter.java
+++ app/src/main/java/com/alphawallet/app/ui/widget/adapter/TokenListAdapter.java
@@ -1,13 +1,18 @@
 package com.alphawallet.app.ui.widget.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.util.SortedList;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -15,8 +20,17 @@ import android.widget.TextView;
 import com.alphawallet.app.R;
 import com.alphawallet.app.entity.tokens.Token;
 import com.alphawallet.app.repository.EthereumNetworkRepository;
+import com.alphawallet.app.service.AssetDefinitionService;
+import com.alphawallet.app.ui.widget.entity.IconItem;
 import com.alphawallet.app.ui.widget.entity.SortedItem;
+import com.alphawallet.app.util.Utils;
 import com.alphawallet.token.tools.Numeric;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.CustomViewTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,16 +38,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class TokenListAdapter extends RecyclerView.Adapter<TokenListAdapter.ViewHolder> {
-    LayoutInflater inflater;
-    Context context;
-    List<Token> data;
+    private LayoutInflater inflater;
+    private final Context context;
+    private final List<Token> data;
     ItemClickListener listener;
+    protected final AssetDefinitionService assetService;
 
-    public TokenListAdapter(Context context, Token[] tokens, ItemClickListener listener) {
+    public TokenListAdapter(Context context, AssetDefinitionService aService, Token[] tokens, ItemClickListener listener) {
         this.context = context;
         this.inflater = LayoutInflater.from(context);
         this.data = filterTokens(Arrays.asList(tokens));
         this.listener = listener;
+        this.assetService = aService;
     }
 
     private List<Token> filterTokens(List<Token> tokens) {
@@ -50,7 +66,6 @@ public class TokenListAdapter extends RecyclerView.Adapter<TokenListAdapter.View
         return filteredList;
     }
 
-
     @NonNull
     @Override
     public TokenListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
@@ -62,7 +77,9 @@ public class TokenListAdapter extends RecyclerView.Adapter<TokenListAdapter.View
     public void onBindViewHolder(@NonNull TokenListAdapter.ViewHolder viewHolder, int i) {
         Token token = data.get(i);
         viewHolder.setIsRecyclable(false);
-        viewHolder.tokenName.setText(token.getFullName());
+        viewHolder.tokenName.setText(token.getFullName(assetService, 1));
+        viewHolder.chainId = token.tokenInfo.chainId;
+        viewHolder.displayTokenIcon(token);
         viewHolder.switchEnabled.setChecked(token.tokenInfo.isEnabled);
         viewHolder.switchEnabled.setTag(new Integer(i));
         viewHolder.switchEnabled.setOnCheckedChangeListener((v, b) -> {
@@ -83,6 +100,11 @@ public class TokenListAdapter extends RecyclerView.Adapter<TokenListAdapter.View
         RelativeLayout layout;
         TextView tokenName;
         Switch switchEnabled;
+        final ImageView icon;
+        final TextView textIcon;
+        int chainId;
+
+        private final CustomViewTarget viewTarget;
 
         ViewHolder(View itemView) {
             super(itemView);
@@ -90,6 +112,52 @@ public class TokenListAdapter extends RecyclerView.Adapter<TokenListAdapter.View
             tokenName = itemView.findViewById(R.id.name);
             switchEnabled = itemView.findViewById(R.id.switch_enabled);
             layout.setOnClickListener(this);
+            icon = itemView.findViewById(R.id.icon);
+            textIcon = itemView.findViewById(R.id.text_icon);
+
+            viewTarget = new CustomViewTarget<ImageView, BitmapDrawable>(icon) {
+                @Override
+                protected void onResourceCleared(@Nullable Drawable placeholder) { }
+
+                @Override
+                public void onLoadFailed(@Nullable Drawable errorDrawable)
+                {
+                    setupTextIcon();
+                }
+
+                @Override
+                public void onResourceReady(@NotNull BitmapDrawable bitmap, Transition<? super BitmapDrawable> transition)
+                {
+                    textIcon.setVisibility(View.GONE);
+                    icon.setVisibility(View.VISIBLE);
+                    icon.setImageDrawable(bitmap);
+                }
+            };
+        }
+
+        private void displayTokenIcon(Token token)
+        {
+            int chainIcon = EthereumNetworkRepository.getChainLogo(token.tokenInfo.chainId);
+
+            IconItem iconItem = assetService.fetchIconForToken(token);
+            String tokenName = token.getFullName(assetService, token.getTicketCount());
+            viewTarget.getView().setTag(tokenName);
+
+            Glide.with(context.getApplicationContext())
+                    .load(iconItem.getUrl())
+                    .signature(iconItem.getSignature())
+                    .onlyRetrieveFromCache(iconItem.onlyFetchFromCache()) //reduce URL checking, only check once per session
+                    .apply(new RequestOptions().circleCrop())
+                    .apply(new RequestOptions().placeholder(chainIcon))
+                    .dontAnimate()
+                    .into(viewTarget);
+        }
+
+        private void setupTextIcon() {
+            icon.setVisibility(View.GONE);
+            textIcon.setVisibility(View.VISIBLE);
+            textIcon.setBackgroundTintList(ContextCompat.getColorStateList(context, Utils.getChainColour(chainId)));
+            textIcon.setText(Utils.getIconisedText(tokenName.getText().toString()));
         }
 
         @Override
@@ -100,14 +168,10 @@ public class TokenListAdapter extends RecyclerView.Adapter<TokenListAdapter.View
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             listener.onItemClick(data.get(getAdapterPosition()), isChecked);
-
         }
     }
 
     public interface ItemClickListener {
         void onItemClick(Token token, boolean enabled);
     }
-
-
-
 }

7f4977bc2609562ad72adc732b910b2908839c9e
ChintanRathod <rathod.chintan.h@gmail.com>
2020-09-15 19:51:41 +0530

    Add mixpanel integration

--- app/src/analytics/java/com/alphawallet/app/service/AnalyticsService.java
+++ app/src/analytics/java/com/alphawallet/app/service/AnalyticsService.java
@@ -6,7 +6,13 @@ import android.os.Bundle;
 import com.alphawallet.app.C;
 import com.alphawallet.app.entity.AnalyticsProperties;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
 
 public class AnalyticsService<T> implements AnalyticsServiceType<T> {
 
@@ -29,6 +35,7 @@ public class AnalyticsService<T> implements AnalyticsServiceType<T> {
     public void track(String eventName)
     {
         //firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, eventName);
+        mixpanelAPI.track(eventName);
     }
 
     @Override
@@ -36,8 +43,14 @@ public class AnalyticsService<T> implements AnalyticsServiceType<T> {
     {
         AnalyticsProperties analyticsProperties = (AnalyticsProperties) event;
 
+        trackFirebase(analyticsProperties, eventName);
+        trackMixPanel(analyticsProperties, eventName);
+    }
+
+    private void trackFirebase(AnalyticsProperties analyticsProperties, String eventName)
+    {
         Bundle props = new Bundle();
-        if (!analyticsProperties.getWalletType().isEmpty())
+        if(!analyticsProperties.getWalletType().isEmpty())
         {
             props.putString(C.AN_WALLET_TYPE, analyticsProperties.getWalletType());
         }
@@ -45,15 +58,45 @@ public class AnalyticsService<T> implements AnalyticsServiceType<T> {
         firebaseAnalytics.logEvent(eventName, props);
     }
 
+    private void trackMixPanel(AnalyticsProperties analyticsProperties, String eventName)
+    {
+        try
+        {
+            JSONObject props = new JSONObject();
+
+            if(analyticsProperties.getWalletType() != null)
+            {
+                props.put(C.AN_WALLET_TYPE, analyticsProperties.getWalletType());
+            }
+
+            mixpanelAPI.track(eventName, props);
+        }
+        catch(JSONException e)
+        {
+            //Something went wrong
+        }
+    }
+
     @Override
     public void identify(String uuid)
     {
         firebaseAnalytics.setUserId(uuid);
+        mixpanelAPI.identify(uuid);
+        mixpanelAPI.getPeople().identify(uuid);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful())
+                    {
+                        String token = Objects.requireNonNull(task.getResult()).getToken();
+                        mixpanelAPI.getPeople().setPushRegistrationId(token);
+                    }
+                });
     }
 
     @Override
     public void flush()
     {
         //Nothing like flush in firebase
+        mixpanelAPI.flush();
     }
 }

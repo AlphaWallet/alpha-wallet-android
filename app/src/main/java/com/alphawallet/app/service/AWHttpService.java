a6e349613fafd03a326795e599c0e0c22a48e578
Symblox Dev <symblox@protonmail.com>
2020-10-08 17:10:09 +0800

    Removed unused libs

--- app/src/main/java/com/alphawallet/app/service/AWHttpService.java
+++ app/src/main/java/com/alphawallet/app/service/AWHttpService.java
@@ -9,7 +9,6 @@ package com.alphawallet.app.service;
  *
  */
 
-import com.google.android.gms.common.util.IOUtils;
 import com.google.gson.JsonParseException;
 
 import org.slf4j.Logger;
@@ -27,7 +26,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import io.grpc.internal.IoUtils;
 import okhttp3.CipherSuite;
 import okhttp3.ConnectionSpec;
 import okhttp3.Headers;

b14cc8a1f1852b8f15de7cf30a6b9277281490f1
James Brown <soylentblue@hotmail.co.uk>
2021-01-16 19:20:15 +1100

    fix test

--- app/src/test/java/com/alphawallet/app/CryptoFunctions.java
+++ app/src/test/java/com/alphawallet/app/CryptoFunctions.java
@@ -8,7 +8,11 @@ import java.security.SignatureException;
 import java.util.Arrays;
 import java.util.Base64;
 
+import com.alphawallet.app.web3j.StructuredDataEncoder;
 import com.alphawallet.token.entity.CryptoFunctionsInterface;
+import com.alphawallet.token.entity.ProviderTypedData;
+
+import wallet.core.jni.Hash;
 
 public class CryptoFunctions implements CryptoFunctionsInterface
 {
@@ -36,6 +40,40 @@ public class CryptoFunctions implements CryptoFunctionsInterface
     public String getAddressFromKey(BigInteger recoveredKey)
     {
         return Keys.getAddress(recoveredKey);
+    }
+
+    @Override
+    public byte[] keccak256(byte[] message)
+    {
+        return Hash.keccak256(message);
+    }
+
+    @Override
+    public CharSequence formatTypedMessage(ProviderTypedData[] rawData)
+    {
+        return null;
+    }
+
+    @Override
+    public CharSequence formatEIP721Message(String messageData)
+    {
+        return null;
+    }
+
+    @Override
+    public byte[] getStructuredData(String messageData)
+    {
+        try
+        {
+            StructuredDataEncoder eip721Object = new StructuredDataEncoder(messageData);
+            return eip721Object.getStructuredData();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return new byte[0];
     }
 
     public static Sign.SignatureData sigFromByteArray(byte[] sig)

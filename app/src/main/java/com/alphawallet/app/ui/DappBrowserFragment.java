eb38e49e0a14fd267ec926876248022acd2ac49e
James Brown <soylentblue@hotmail.co.uk>
2021-06-01 11:58:53 +1000

    revert

--- app/src/main/java/com/alphawallet/app/ui/DappBrowserFragment.java
+++ app/src/main/java/com/alphawallet/app/ui/DappBrowserFragment.java
@@ -1370,13 +1370,13 @@ public class DappBrowserFragment extends Fragment implements OnSignTransactionLi
     {
         //headers
         return new HashMap<String, String>() {{
-            /*put("Connection", "close");
+            put("Connection", "close");
             put("Content-Type", "text/plain");
             put("Access-Control-Allow-Origin", "*");
             put("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
             put("Access-Control-Max-Age", "600");
             put("Access-Control-Allow-Credentials", "true");
-            put("Access-Control-Allow-Headers", "accept, authorization, Content-Type");*/
+            put("Access-Control-Allow-Headers", "accept, authorization, Content-Type");
         }};
     }
 

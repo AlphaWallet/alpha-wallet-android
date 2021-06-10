2220462a0c207e04dbb080bcbd6aacdb036dd35f
Jenny Jingjing Li <jennyli12320@gmail.com>
2021-06-10 17:14:13 +1000

    ActionSheetDialog link

--- app/src/main/java/com/alphawallet/app/web3/entity/Web3Transaction.java
+++ app/src/main/java/com/alphawallet/app/web3/entity/Web3Transaction.java
@@ -15,6 +15,7 @@ import com.alphawallet.app.walletconnect.entity.WCEthereumTransaction;
 import com.alphawallet.app.widget.ActionSheetMode;
 import com.alphawallet.token.entity.MagicLinkInfo;
 
+
 import org.web3j.protocol.core.methods.request.Transaction;
 
 import java.math.BigDecimal;
@@ -122,6 +123,19 @@ public class Web3Transaction implements Parcelable {
         payload = (mode == ActionSheetMode.CANCEL_TRANSACTION) ? "0x": tx.input;
         leafPosition = -1;
         description = null;
+    }
+
+    public Web3Transaction(com.alphawallet.app.entity.Transaction tx, Boolean isCancelling)
+    {
+
+        recipient = new Address(tx.to);
+        contract = new Address (tx.to);
+        value = (isCancelling) ? BigInteger.ZERO: new BigInteger(tx.value);
+        gasPrice = new BigInteger(tx.gasPrice);
+        gasLimit= new BigInteger(tx.gasUsed);
+        nonce = tx.nonce;
+        payload = (isCancelling) ? "0x": tx.hash;
+        leafPosition = -1;
     }
 
     Web3Transaction(Parcel in) {

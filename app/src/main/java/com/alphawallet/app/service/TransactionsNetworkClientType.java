4757319dc695f989c649fa9fff0fe9b08004feb1
James Brown <soylentblue@hotmail.co.uk>
2021-06-15 08:35:18 +1000

    Update polygon routes (#1896)

--- app/src/main/java/com/alphawallet/app/service/TransactionsNetworkClientType.java
+++ app/src/main/java/com/alphawallet/app/service/TransactionsNetworkClientType.java
@@ -10,4 +10,5 @@ public interface TransactionsNetworkClientType {
     Single<Transaction[]> storeNewTransactions(String walletAddress, NetworkInfo networkInfo, String tokenAddress, long lastBlock);
     Single<TransactionMeta[]> fetchMoreTransactions(String walletAddress, NetworkInfo network, long lastTxTime);
     Single<Integer> readTransfers(String currentAddress, NetworkInfo networkByChain, TokensService tokensService, boolean nftCheck);
+    void checkRequiresAuxReset(String walletAddr);
 }

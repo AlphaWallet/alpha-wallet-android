f270d2b2173a49223f58515f5b604e1c7d5f89ef
Jenny Jingjing Li <jennyli12320@gmail.com>
2021-07-22 22:47:02 +1000

    GasSettings filters out any default speeds less than the minimum gas price

--- app/src/main/java/com/alphawallet/app/web3/entity/Web3Transaction.java
+++ app/src/main/java/com/alphawallet/app/web3/entity/Web3Transaction.java
@@ -6,6 +6,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.text.TextUtils;
 import android.text.style.StyleSpan;
+import android.util.Log;
 
 import com.alphawallet.app.R;
 import com.alphawallet.app.util.BalanceUtils;
@@ -96,7 +97,7 @@ public class Web3Transaction implements Parcelable {
         gasPrice = minGas;
         gasLimit= new BigInteger(tx.gasUsed);
         nonce = tx.nonce;
-        payload = (isCancelling) ? "0x": tx.hash;
+        payload = (isCancelling) ? "0x": tx.input;
         leafPosition = -1;
     }
 

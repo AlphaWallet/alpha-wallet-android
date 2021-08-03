5a614aa0832f04b41a8bc4b6f44a7c27a6cab0d7
Michael Paschenko <mpaschenko@gmail.com>
2021-08-03 13:05:27 +0300

    Implemented wallet naming feature (#1795)

---
+++ app/src/main/java/com/alphawallet/app/ui/NameThisWalletActivity.java
@@ -0,0 +1,102 @@
+package com.alphawallet.app.ui;
+
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.alphawallet.app.R;
+import com.alphawallet.app.entity.StandardFunctionInterface;
+import com.alphawallet.app.entity.Wallet;
+import com.alphawallet.app.repository.TokenRepository;
+import com.alphawallet.app.util.AWEnsResolver;
+import com.alphawallet.app.viewmodel.NameThisWalletViewModel;
+import com.alphawallet.app.viewmodel.NameThisWalletViewModelFactory;
+import com.alphawallet.app.viewmodel.NewSettingsViewModel;
+import com.alphawallet.app.viewmodel.NewSettingsViewModelFactory;
+import com.alphawallet.app.widget.FunctionButtonBar;
+import com.alphawallet.app.widget.InputView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import dagger.android.support.AndroidSupportInjection;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+import static com.alphawallet.ethereum.EthereumNetworkBase.MAINNET_ID;
+
+public class NameThisWalletActivity extends BaseActivity implements StandardFunctionInterface {
+
+    @Inject
+    NameThisWalletViewModelFactory viewModelFactory;
+
+    private NameThisWalletViewModel viewModel;
+
+    private FunctionButtonBar functionBar;
+    private InputView inputName;
+
+    @Nullable
+    private Disposable disposable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_this_wallet);
+        AndroidInjection.inject(this);
+
+        toolbar();
+
+        setTitle(getString(R.string.name_this_wallet));
+
+        functionBar = findViewById(R.id.layoutButtons);
+        functionBar.setupFunctions(this, new ArrayList<>(Collections.singletonList(R.string.action_save_name)));
+        functionBar.revealButtons();
+
+        inputName = findViewById(R.id.input_name);
+
+        viewModel = new ViewModelProvider(this, viewModelFactory)
+                .get(NameThisWalletViewModel.class);
+        viewModel.defaultWallet().observe(this, this::onDefaultWallet);
+        viewModel.ensName().observe(this, this::onENSSuccess);
+    }
+
+    public void onENSSuccess(String resolvedAddress) {
+        inputName.getEditText().setHint(resolvedAddress);
+    }
+
+    private void onDefaultWallet(Wallet wallet) {
+       inputName.setText(wallet.name);
+    }
+
+    public void handleClick(String action, int actionId) {
+        disposable = viewModel.setWalletName(inputName.getText().toString()).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(n -> {
+                    finish();
+                });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        viewModel.prepare();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (disposable != null && !disposable.isDisposed())
+            disposable.dispose();
+
+        disposable = null;
+    }
+}

54e8298e07fd40dc28da5740cc717e87593f5b89
Michael Paschenko <mpaschenko@gmail.com>
2021-08-05 10:22:32 +0300

    Switched faq to open remote url (#1749) (#1958)

--- app/src/main/java/com/alphawallet/app/entity/MediaLinks.java
+++ app/src/main/java/com/alphawallet/app/entity/MediaLinks.java
@@ -12,6 +12,7 @@ public class MediaLinks
     public static final String AWALLET_REDDIT_URL = "https://www.reddit.com/r/AlphaWallet/";
     public static final String AWALLET_INSTAGRAM_URL = null;
     public static final String AWALLET_BLOG_URL = null;
+    public static final String AWALLET_FAQ_URL = "https://alphawallet.com/faq/";
 
     public static final String AWALLET_EMAIL1 = "feedback+android";
     public static final String AWALLET_EMAIL2 = "alphawallet.com";

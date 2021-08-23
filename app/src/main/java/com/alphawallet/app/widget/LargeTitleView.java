bf9dd1a112abc74c4f1001243a765f2d08ae8287
Michael Paschenko <mpaschenko@gmail.com>
2021-08-23 10:05:40 +0300

    added large title view on wallet tab (#1768)

--- app/src/main/java/com/alphawallet/app/widget/LargeTitleView.java
+++ app/src/main/java/com/alphawallet/app/widget/LargeTitleView.java
@@ -1,2 +1,19 @@
-package com.alphawallet.app.widget;public class LargeTitleView {
+package com.alphawallet.app.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+import com.alphawallet.app.R;
+
+public class LargeTitleView extends LinearLayout {
+
+    public LargeTitleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        LayoutInflater.from(context).inflate(R.layout.layout_large_title_view, this, true);
+    }
 }

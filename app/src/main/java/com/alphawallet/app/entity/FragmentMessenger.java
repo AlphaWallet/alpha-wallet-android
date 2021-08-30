d8bac2b6e7722ff4410e55bb0599af3a5395c286
James Brown <soylentblue@hotmail.co.uk>
2021-08-30 12:36:13 +1000

    fix for Update dialog

--- app/src/main/java/com/alphawallet/app/entity/FragmentMessenger.java
+++ app/src/main/java/com/alphawallet/app/entity/FragmentMessenger.java
@@ -1,7 +1,5 @@
 package com.alphawallet.app.entity;
 
-import com.google.android.play.core.appupdate.AppUpdateInfo;
-
 /**
  * Created by James on 1/02/2019.
  * Stormbird in Singapore
@@ -11,5 +9,5 @@ public interface FragmentMessenger
     void TokensReady();
     void AddToken(String address);
     void tokenScriptError(String message);
-    void updateReady(AppUpdateInfo updateInfo);
+    void updateReady(int versionUpdate);
 }
--- app/src/noAnalytics/java/com/alphawallet/app/util/UpdateUtils.java
+++ app/src/noAnalytics/java/com/alphawallet/app/util/UpdateUtils.java
@@ -2,10 +2,12 @@ package com.alphawallet.app.util;
 
 import android.app.Activity;
 
+import com.alphawallet.app.entity.FragmentMessenger;
+
 public class UpdateUtils {
     public static void checkForUpdates(Activity context, FragmentMessenger messenger) {
     }
 
-    public static void pushUpdateDialog(Activity context, AppUpdateInfo updateInfo) {
+    public static void pushUpdateDialog(Activity context) {
     }
 }

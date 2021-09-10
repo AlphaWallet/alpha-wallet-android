b7ca3399c925b45392027eada1a57462fb5ddb34
Michael Paschenko <mpaschenko@gmail.com>
2021-09-10 10:51:42 +0300

    fixed crash on tap on IME button on search tokens #2001

--- app/src/main/java/com/alphawallet/app/ui/widget/holder/SearchTokensHolder.java
+++ app/src/main/java/com/alphawallet/app/ui/widget/holder/SearchTokensHolder.java
@@ -2,14 +2,21 @@ package com.alphawallet.app.ui.widget.holder;
 
 import android.os.Bundle;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.alphawallet.app.R;
 import com.alphawallet.app.ui.widget.entity.ManageTokensData;
+
+import static android.view.View.FOCUS_DOWN;
+import static android.view.View.FOCUS_FORWARD;
 
 
 public class SearchTokensHolder extends BinderViewHolder<ManageTokensData> {
@@ -21,6 +28,15 @@ public class SearchTokensHolder extends BinderViewHolder<ManageTokensData> {
     @Override
     public void bind(@Nullable ManageTokensData data, @NonNull Bundle addition) {
         editSearch.addTextChangedListener(textWatcher);
+        editSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public SearchTokensHolder(int res_id, ViewGroup parent, TextWatcher textWatcher) {

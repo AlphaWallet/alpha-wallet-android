258b2fb688ea70dcef8cbf4300e251eabe149a58
James Brown <soylentblue@hotmail.co.uk>
2021-09-22 11:23:14 +1000

    Screen size fetch for Android 10 and below vs Android 11

--- app/src/main/java/com/alphawallet/app/ui/QRScanning/DisplayUtils.java
+++ app/src/main/java/com/alphawallet/app/ui/QRScanning/DisplayUtils.java
@@ -1,25 +1,42 @@
 package com.alphawallet.app.ui.QRScanning;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Configuration;
 import android.graphics.Insets;
 import android.graphics.Point;
-import android.view.Display;
+import android.util.DisplayMetrics;
 import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.WindowMetrics;
 
+import java.lang.reflect.Method;
+
 public class DisplayUtils
 {
+    @SuppressWarnings("deprecation")
     public static Point getScreenResolution(Activity activity) {
         Point screenResolution = new Point();
-        WindowMetrics windowMetrics = activity.getWindowManager().getCurrentWindowMetrics();
-        Insets insets = windowMetrics.getWindowInsets()
-                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars());
+        WindowManager windowManager = activity.getWindowManager();
 
-        screenResolution.set(windowMetrics.getBounds().width() - insets.left - insets.right,
-                windowMetrics.getBounds().height() - insets.top - insets.bottom);
+        try
+        {
+            Method m = windowManager.getClass().getDeclaredMethod(
+                    "getCurrentWindowMetrics"); //Use reflection to see if method is available
+
+            WindowMetrics windowMetrics = (WindowMetrics) m.invoke(windowManager);
+            Insets insets = windowMetrics.getWindowInsets()
+                    .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars());
+
+            screenResolution.set(windowMetrics.getBounds().width() - insets.left - insets.right,
+                    windowMetrics.getBounds().height() - insets.top - insets.bottom);
+        }
+        catch (Exception e)
+        {
+            //Use legacy method
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            screenResolution.x = displayMetrics.widthPixels;
+            screenResolution.y = displayMetrics.heightPixels;
+        }
 
         return screenResolution;
     }

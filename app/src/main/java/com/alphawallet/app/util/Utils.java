382e05f3a548a9ea02e7a710742498d92d09e82e
James Brown <soylentblue@hotmail.co.uk>
2021-10-19 11:17:50 +1100

    Fix binance token lookup

--- app/src/main/java/com/alphawallet/app/util/Utils.java
+++ app/src/main/java/com/alphawallet/app/util/Utils.java
@@ -864,10 +864,13 @@ public class Utils {
                 repoChain = "poa";
                 break;
             case BINANCE_MAIN_ID:
-                repoChain = "binance";
+                repoChain = "smartchain";
                 break;
             case AVALANCHE_ID:
                 repoChain = "avalanche";
+                break;
+            case OPTIMISTIC_MAIN_ID:
+                repoChain = "optimism";
                 break;
             case MATIC_ID:
                 repoChain = "polygon";

154d91086b75c5f88a088dbe15f819b8f1ae8762
Seaborn Lee <seabornlee.cn@gmail.com>
2021-12-09 08:56:30 +0800

    Move disable animation settings from gradle to shell script

--- e2e.sh
+++ e2e.sh
@@ -1,1 +1,8 @@
-./gradlew :app:uninstallAll :app:connectedNoAnalyticsDebugAndroidTest
+#!/bin/sh
+
+# disable animations or test may not stable
+adb shell settings put global window_animation_scale 0.0
+adb shell settings put global transition_animation_scale 0.0
+adb shell settings put global animator_duration_scale 0.0
+
+./gradlew :app:uninstallAll :app:connectedNoAnalyticsDebugAndroidTest -x lint -PdisablePreDex

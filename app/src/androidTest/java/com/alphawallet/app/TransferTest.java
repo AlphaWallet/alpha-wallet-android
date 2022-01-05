64f222b9f4787da9ab30837a7b9b99d6fa45b35e
Seaborn Lee <seabornlee.cn@gmail.com>
2022-01-05 15:12:29 +0800

    Try close backup dialog by click outside the overlay

--- app/src/androidTest/java/com/alphawallet/app/TransferTest.java
+++ app/src/androidTest/java/com/alphawallet/app/TransferTest.java
@@ -14,6 +14,7 @@ import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.UiDevice;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
@@ -94,7 +95,12 @@ public class TransferTest {
             click(withText(R.string.ok));
         }
         click(withText("CREATE A NEW WALLET"));
-        click(withId(R.id.btn_close));
+        closeBackupTipsDialog();
+    }
+
+    private void closeBackupTipsDialog() {
+//        click(withId(R.id.btn_close)); // works well locally but NOT work with GitHub actions
+        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).click(200, 200);
     }
 
     private void selectTestNet() {

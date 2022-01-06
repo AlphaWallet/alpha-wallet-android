f07f85ac4f8c41fcaba9933c177fb88c4bb6d4cf
Seaborn Lee <seabornlee.cn@gmail.com>
2022-01-06 20:12:39 +0800

    Fix click randomly become long press

--- e2e.sh
+++ e2e.sh
@@ -5,6 +5,10 @@ adb shell settings put global window_animation_scale 0.0
 adb shell settings put global transition_animation_scale 0.0
 adb shell settings put global animator_duration_scale 0.0
 
+# Sometimes Android Espresso performs longClick instead of click
+# https://stackoverflow.com/questions/32330671/android-espresso-performs-longclick-instead-of-click
+adb shell settings put secure long_press_timeout 1500
+
 rm -rf DCIM
 adb shell rm /storage/emulated/0/DCIM/*.png
 ./gradlew :app:uninstallAll :app:connectedNoAnalyticsDebugAndroidTest -x lint -PdisablePreDex

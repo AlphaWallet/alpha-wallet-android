b96f3415e0d00dd4cc89300ade0599c1b2407dfc
Seaborn Lee <seabornlee.cn@gmail.com>
2022-01-12 10:54:38 +0800

    Fix test

--- app/src/androidTest/java/com/alphawallet/app/TransferTest.java
+++ app/src/androidTest/java/com/alphawallet/app/TransferTest.java
@@ -175,6 +175,7 @@ public class TransferTest {
         SnapshotUtil.take("after-add");
         click(withId(R.id.import_account_action));
         onView(allOf(withId(R.id.edit_text), withParent(withParent(withParent(withId(R.id.input_seed)))))).perform(replaceText(seedPhrase));
+        Helper.wait(2); // Avoid error: Error performing a ViewAction! soft keyboard dismissal animation may have been in the way. Retrying once after: 1000 millis
         click(withId(R.id.import_action));
         Helper.wait(10);
     }

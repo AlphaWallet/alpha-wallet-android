118f6916630c619347ec2cbb8fe24fdf781c316f
Seaborn Lee <seabornlee.cn@gmail.com>
2022-01-13 11:40:35 +0800

    Refactor: extract methods

--- app/src/main/java/com/alphawallet/app/service/PriceAlertsService.java
+++ app/src/main/java/com/alphawallet/app/service/PriceAlertsService.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 
+import com.alphawallet.app.R;
 import com.alphawallet.app.entity.CurrencyItem;
 import com.alphawallet.app.entity.Wallet;
 import com.alphawallet.app.entity.tokens.Token;
@@ -17,9 +18,9 @@ import com.alphawallet.app.ui.widget.entity.PriceAlert;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
@@ -52,7 +53,6 @@ public class PriceAlertsService extends Service {
     @Inject
     GenericWalletInteract genericWalletInteract;
 
-    // @Inject
     private Wallet defaultWallet;
     @Inject
     AssetDefinitionService assetDefinitionService;
@@ -108,23 +108,12 @@ public class PriceAlertsService extends Service {
     }
 
     private void heartBeat() {
-
-        // get list of the alerts
-        Type listType = new TypeToken<List<PriceAlert>>() {
-        }.getType();
-
-        ArrayList<PriceAlert> list = null;
-        String json = preferenceRepository.getPriceAlerts();
-        if (!json.isEmpty()) {
-            list = new Gson().fromJson(json, listType);
-        } else {
-            return ;
+        if (defaultWallet == null) {
+            return;
         }
-        ArrayList<PriceAlert> finalList = list;
-        for (PriceAlert priceAlert : finalList) {
-            if (!priceAlert.isEnabled() || defaultWallet == null) {
-                continue;
-            }
+
+        List<PriceAlert> enabledPriceAlerts = getEnabledPriceAlerts();
+        for (PriceAlert priceAlert : enabledPriceAlerts) {
             tickerService.convertPair(TickerService.getCurrencySymbolTxt(), priceAlert.getCurrency())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
@@ -136,29 +125,55 @@ public class PriceAlertsService extends Service {
                         double currentTokenPrice = Double.parseDouble(tokensService.getTokenTicker(token).price);
 
                         if (priceAlert.match(rate, currentTokenPrice)) {
-                            // raise alert
-                            boolean hasDefinition = assetDefinitionService.hasDefinition(token.tokenInfo.chainId, token.getAddress());
-                            Intent intent = tokenDetailRouter.makeERC20DetailsIntent(this, token.getAddress(), token.tokenInfo.symbol, token.tokenInfo.decimals,
-                                    !token.isEthereum(), defaultWallet, token, hasDefinition);
-
-                            String content = "";
-                            if (priceAlert.getIndicator()) {
-                                content = "Above ";
-                            } else {
-                                content = "Below ";
-                            }
-
-                            CurrencyItem currencyItem = CurrencyRepository.getCurrencyByISO(priceAlert.getCurrency());
-                            content += currencyItem.getSymbol() + TickerService.getCurrencyWithoutSymbol(new Double(priceAlert.getValue()));
-
-                            notificationService.displayPriceAlertNotification(priceAlert.getToken(), content, 0, intent);
+                            String content = constructContent(priceAlert, Objects.requireNonNull(CurrencyRepository.getCurrencyByISO(priceAlert.getCurrency())));
+                            notificationService.displayPriceAlertNotification(priceAlert.getToken(), content, 0, constructIntent(token));
                             // disable notification to avoid run multiple times
                             priceAlert.setEnabled(false);
-
-                            String updatedJson = new Gson().toJson(finalList, listType);
-                            preferenceRepository.setPriceAlerts(updatedJson);
+                            updatePriceAlerts(enabledPriceAlerts);
                         }
                     }, Throwable::printStackTrace).isDisposed();
         }
+    }
+
+    private List<PriceAlert> getEnabledPriceAlerts() {
+        List<PriceAlert> enabledPriceAlerts = new ArrayList<>();
+        for (PriceAlert pa : getPriceAlerts()) {
+            if (pa.isEnabled()) {
+                enabledPriceAlerts.add(pa);
+            }
+        }
+        return enabledPriceAlerts;
+    }
+
+    private List<PriceAlert> getPriceAlerts() {
+        String json = preferenceRepository.getPriceAlerts();
+        if (json.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return new Gson().fromJson(json, new TypeToken<List<PriceAlert>>() {
+        }.getType());
+    }
+
+    private void updatePriceAlerts(List<PriceAlert> priceAlerts) {
+        String json = new Gson().toJson(priceAlerts, new TypeToken<List<PriceAlert>>() {
+        }.getType());
+        preferenceRepository.setPriceAlerts(json);
+    }
+
+    private Intent constructIntent(Token token) {
+        boolean hasDefinition = assetDefinitionService.hasDefinition(token.tokenInfo.chainId, token.getAddress());
+        return tokenDetailRouter.makeERC20DetailsIntent(this, token.getAddress(), token.tokenInfo.symbol, token.tokenInfo.decimals,
+                !token.isEthereum(), defaultWallet, token, hasDefinition);
+    }
+
+    private String constructContent(PriceAlert priceAlert, CurrencyItem currencyItem) {
+        return getIndicatorText(priceAlert.getIndicator()) + " " + currencyItem.getSymbol() + TickerService.getCurrencyWithoutSymbol(Double.parseDouble(priceAlert.getValue()));
+    }
+
+    private String getIndicatorText(boolean isAbove) {
+        if (isAbove) {
+            return getString(R.string.price_alert_indicator_above);
+        }
+        return getString(R.string.price_alert_indicator_below);
     }
 }

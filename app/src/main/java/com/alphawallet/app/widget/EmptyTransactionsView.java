2cea16e2efa5d581506db0d39e0e7167b5f28e7f
James Brown <soylentblue@hotmail.co.uk>
2022-01-22 11:30:18 +1100

    Add Myanmar localisation and fix currency change issues observed.

--- app/src/main/java/com/alphawallet/app/widget/EmptyTransactionsView.java
+++ app/src/main/java/com/alphawallet/app/widget/EmptyTransactionsView.java
@@ -1,16 +1,12 @@
 package com.alphawallet.app.widget;
 
 import android.content.Context;
-import androidx.annotation.NonNull;
 import android.view.LayoutInflater;
-import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.TextView;
 
-import com.alphawallet.app.C;
+import androidx.annotation.NonNull;
+
 import com.alphawallet.app.R;
-import com.alphawallet.app.entity.CustomViewSettings;
-import com.alphawallet.app.ui.HomeActivity;
 
 public class EmptyTransactionsView extends FrameLayout {
 

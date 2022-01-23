f93823947a4bd8a6f7a803b0ca82b382f3a51b0d
James Brown <soylentblue@hotmail.co.uk>
2022-01-23 11:09:46 +1100

    Fix toolbar view & tokenscript functions

--- app/src/main/java/com/alphawallet/app/widget/FunctionButtonBar.java
+++ app/src/main/java/com/alphawallet/app/widget/FunctionButtonBar.java
@@ -532,24 +532,23 @@ public class FunctionButtonBar extends LinearLayout implements AdapterView.OnIte
 
         Map<String, TSAction> availableFunctions = new HashMap<>();
 
+        //TokenScript first:
+        addTokenScriptFunctions(availableFunctions, token, tokenId);
+
         //If Token is Non-Fungible then display the custom functions first - usually these are more frequently used
         if (!token.isNonFungible())
         {
             addStandardTokenFunctions(token);
         }
 
-        boolean hasOverrideFunctions = setupCustomTokenActions();
+        setupCustomTokenActions();
 
         //Only add TokenScript if not already overridden
-        if (!hasOverrideFunctions)
-        {
-            addTokenScriptFunctions(availableFunctions, token, tokenId);
-        }
-        else
+        /*if (hasOverrideFunctions)
         {
             token.setFunctionAvailability(null);
-            functions = null;
-        }
+            //functions = null;
+        }*/
 
         //Add buy function
         if (hasBuyFunction)

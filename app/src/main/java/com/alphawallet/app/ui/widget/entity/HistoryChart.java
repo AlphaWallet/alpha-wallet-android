800f21fc47b92af9edf05a5221cc2e53dbaf5877
Jenny Jingjing Li <jennyli12320@gmail.com>
2022-02-01 12:19:25 +1100

    History chart revisions (#2363)
    * remove shadow #2354 
    * 2355 add red colouring
    * #2353 add grey lines and edge values
    * adjust min/max values to local currency
    * add text margin

--- app/src/main/java/com/alphawallet/app/ui/widget/entity/HistoryChart.java
+++ app/src/main/java/com/alphawallet/app/ui/widget/entity/HistoryChart.java
@@ -2,15 +2,12 @@ package com.alphawallet.app.ui.widget.entity;
 
 import static com.alphawallet.app.service.TickerService.chainPairs;
 import static com.alphawallet.app.service.TickerService.coinGeckoChainIdToAPIName;
-import static com.alphawallet.ethereum.EthereumNetworkBase.MATIC_ID;
 
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
-import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.util.Pair;
 import android.util.TypedValue;
@@ -20,18 +17,13 @@ import androidx.annotation.Nullable;
 
 import com.alphawallet.app.R;
 import com.alphawallet.app.entity.tokens.Token;
-import com.alphawallet.app.entity.tokens.TokenInfo;
 import com.alphawallet.app.service.TickerService;
-import com.alphawallet.app.web3.entity.Address;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -41,7 +33,10 @@ import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
-public class HistoryChart extends View {
+public class HistoryChart extends View
+{
+
+    private static final float TEXT_MARGIN = 16.0f;
 
     static OkHttpClient httpClient = new OkHttpClient.Builder()
             .connectTimeout(5, TimeUnit.SECONDS)
@@ -83,8 +78,10 @@ public class HistoryChart extends View {
         }
     }
 
-    static class Datasource {
-        ArrayList<Pair<Long, Float>> getEntries() {
+    static class Datasource
+    {
+        ArrayList<Pair<Long, Float>> getEntries()
+        {
             return entries;
         }
 
@@ -94,12 +91,14 @@ public class HistoryChart extends View {
 
         ArrayList<Pair<Long, Float>> entries = new ArrayList<>();
 
-        static Single<Datasource> fetchHistory(Range range, String tokenId) {
+        static Single<Datasource> fetchHistory(Range range, String tokenId)
+        {
             return Single.fromCallable(() -> {
                 ArrayList<Pair<Long, Float>> entries = new ArrayList<>();
-                try {
+                try
+                {
                     Request request = new Request.Builder()
-                            .url("https://api.coingecko.com/api/v3/coins/" + tokenId + "/market_chart?days=" + range.value + "&vs_currency=USD")
+                            .url("https://api.coingecko.com/api/v3/coins/" + tokenId + "/market_chart?days=" + range.value + "&vs_currency=" + TickerService.getCurrencySymbolTxt())
                             .get()
                             .build();
                     okhttp3.Response response = httpClient.newCall(request)
@@ -108,15 +107,18 @@ public class HistoryChart extends View {
                         JSONArray prices = new JSONObject(response.body().string()).getJSONArray("prices");
                         float minValue = Float.MAX_VALUE;
                         float maxValue = 0;
-                        for (int i = 0; i < prices.length(); i++) {
+                        for (int i = 0; i < prices.length(); i++)
+                        {
                             JSONArray entry = prices.getJSONArray(i);
                             long timestamp = entry.getLong(0);
                             float value = (float) entry.getDouble(1);
                             entries.add(Pair.create(timestamp, value));
-                            if (minValue > value) {
+                            if (minValue > value)
+                            {
                                 minValue = value;
                             }
-                            if (maxValue < value) {
+                            if (maxValue < value)
+                            {
                                 maxValue = value;
                             }
                         }
@@ -133,90 +135,136 @@ public class HistoryChart extends View {
             });
         }
 
-        long minTime() {
+        long minTime()
+        {
             return entries.get(0).first;
         }
 
-        long maxTime() {
+        long maxTime()
+        {
             return entries.get(entries.size() - 1).first;
         }
 
-        float minValue() {
+        boolean isGreen()
+        {
+            return (entries.get(0).second - entries.get(entries.size() - 1).second) < 0;
+        }
+
+        float minValue()
+        {
             return minValue;
         }
 
-        float maxValue() {
+        float maxValue()
+        {
             return maxValue;
         }
     }
 
     Cache cache = new Cache();
-
     Paint paint = new Paint();
-    Paint textPaint = new Paint();
+    Paint noDataTextPaint = new Paint();
     Path path = new Path();
-    LinearGradient gradient = null;
+    Paint greyPaint = new Paint();
+    Path greyLines = new Path();
+    Paint edgeValPaint = new Paint();
 
-    private void init() {
-        paint.setColor(getResources().getColor(R.color.nasty_green, getContext().getTheme()));
-
+    private void init()
+    {
+        paint.setColor(getResources().getColor(R.color.green, getContext().getTheme()));
 
         Resources r = getResources();
         int strokeWidth = (int) TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
-                5,
+                3,
                 r.getDisplayMetrics()
         );
 
         paint.setStrokeWidth(strokeWidth);
         paint.setDither(true);
 
-        textPaint.setTextAlign(Paint.Align.CENTER);
+
+        greyPaint.setColor(getResources().getColor(R.color.black_12,getContext().getTheme()));
+        greyPaint.setStrokeWidth(1);
+
+        noDataTextPaint.setTextAlign(Paint.Align.CENTER);
         int textSize = (int) TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_SP,
                 14,
                 r.getDisplayMetrics()
         );
-        textPaint.setTextSize(textSize);
-        textPaint.setColor(getResources().getColor(R.color.black, getContext().getTheme()));
+        noDataTextPaint.setTextSize(textSize);
+        noDataTextPaint.setColor(getResources().getColor(R.color.black_12, getContext().getTheme()));
+
+        edgeValPaint.setTextAlign(Paint.Align.RIGHT);
+        edgeValPaint.setTextSize(
+                TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_SP,
+                        12,
+                        r.getDisplayMetrics()
+                )
+        );
+        edgeValPaint.setColor(getResources().getColor(R.color.black, getContext().getTheme()));
+
     }
 
-    public HistoryChart(Context context, @Nullable AttributeSet attrs) {
+    public HistoryChart(Context context, @Nullable AttributeSet attrs)
+    {
         super(context, attrs);
         init();
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas)
+    {
         super.onDraw(canvas);
 
         Datasource datasource = cache.getCurrentDatasource(cache.range);
-        if (datasource == null || datasource.entries.size() == 0) {
+        if (datasource == null || datasource.entries.size() == 0)
+        {
             // draw no chart data available message
             int xPos = (getWidth() / 2);
-            int yPos = (int) ((getHeight() / 2) - ((textPaint.descent() + textPaint.ascent()) / 2));
-            canvas.drawText(getContext().getString(R.string.no_chart_data_available), xPos, yPos, textPaint);
+            int yPos = (int) ((getHeight() / 2) - ((noDataTextPaint.descent() + noDataTextPaint.ascent()) / 2));
+            canvas.drawText(getContext().getString(R.string.no_chart_data_available), xPos, yPos, noDataTextPaint);
             return;
         }
 
-        path.reset();
 
         // draw chart
         float width = getWidth();
         float height = getHeight();
 
-        float xScale = width / (datasource.maxTime() - datasource.minTime());
-        float yScale = (height * 0.9f) / (datasource.maxValue() - datasource.minValue());
+        //colour changes depending on first and last values
+        path.reset();
+        int color = datasource.isGreen() ? R.color.green : R.color.danger;
+        paint.setColor(getResources().getColor(color,getContext().getTheme()));
 
-        for (int i = 0; i < datasource.entries.size(); i++) {
+
+        float xScale = width / (datasource.maxTime() - datasource.minTime());
+        float yScale = ((height * 0.9f) / (datasource.maxValue() - datasource.minValue()));
+
+        for (float i = datasource.minValue();
+             i <= datasource.maxValue();
+             i = i + (datasource.maxValue() - datasource.minValue())/4) {
+            float lineVal = height - (i - datasource.minValue()) * yScale;
+            greyLines.moveTo(0, lineVal);
+            greyLines.lineTo(width, lineVal);
+        }
+        greyPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(greyLines,greyPaint);
+
+        for (int i = 0; i < datasource.entries.size(); i++)
+        {
             Pair<Long, Float> entry = datasource.entries.get(i);
 
             float x = (entry.first - datasource.minTime()) * xScale;
             float y = height - (entry.second - datasource.minValue()) * yScale;
 
-            if (i == 0) {
+            if (i == 0)
+            {
                 path.moveTo(x, y);
-            } else {
+            } else
+            {
                 path.lineTo(x, y);
             }
         }
@@ -225,21 +273,13 @@ public class HistoryChart extends View {
         paint.setShader(null);
         canvas.drawPath(path, paint);
 
-        if (gradient == null) {
-            // create gradient fill on the first run
-            gradient = new LinearGradient(0, 0, 0, height, 0xFFf7fbf4, 0xffffffff, Shader.TileMode.CLAMP);
-        }
-
-        paint.setShader(gradient);
-
-        // add bottom points
-        paint.setStyle(Paint.Style.FILL);
-        path.lineTo(width, height);
-        path.lineTo(0, height);
-        canvas.drawPath(path, paint);
+        // add min/max values to chart
+        canvas.drawText(String.format("%.02f", datasource.minValue()),width - TEXT_MARGIN,height,edgeValPaint);
+        canvas.drawText(String.format("%.02f",datasource.maxValue()),width - TEXT_MARGIN,0.05f*height,edgeValPaint);
     }
 
-    public void fetchHistory(Token token, final Range range) {
+    public void fetchHistory(Token token, final Range range)
+    {
         // use cache
         cache.range = range;
         if (cache.getCurrentDatasource(range) != null) {
@@ -247,12 +287,13 @@ public class HistoryChart extends View {
             return;
         }
 
-        if (!TickerService.validateCoinGeckoAPI(token)) return; //wouldn't have tickers
+        if (!TickerService.validateCoinGeckoAPI(token)) { return; } //wouldn't have tickers
 
         String coingeckoTokenId = token.isEthereum() ? chainPairs.get(token.tokenInfo.chainId)
                 : coinGeckoChainIdToAPIName.get(token.tokenInfo.chainId) + "/contract/" + token.getAddress().toLowerCase();
 
-        if (coingeckoTokenId != null) {
+        if (coingeckoTokenId != null)
+        {
             Datasource.fetchHistory(range, coingeckoTokenId)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
@@ -260,7 +301,9 @@ public class HistoryChart extends View {
         }
     }
 
-    private void onEntries(Range range, Datasource datasource) {
+
+    private void onEntries(Range range, Datasource datasource)
+    {
         // invalidate
         cache.datasourceMap.put(range, datasource);
         invalidate();

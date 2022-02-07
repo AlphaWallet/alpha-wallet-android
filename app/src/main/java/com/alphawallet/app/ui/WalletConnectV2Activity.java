c2f85f96182d81364c209d4628a10312d59adf39
Seaborn Lee <seabornlee.cn@gmail.com>
2022-02-07 23:27:11 +0800

    Show wallets

--- app/src/main/java/com/alphawallet/app/ui/WalletConnectV2Activity.java
+++ app/src/main/java/com/alphawallet/app/ui/WalletConnectV2Activity.java
@@ -28,7 +28,6 @@ import com.walletconnect.walletconnectv2.client.WalletConnectClient;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import androidx.annotation.NonNull;

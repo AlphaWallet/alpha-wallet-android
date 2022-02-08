f6f67eaea2540938a166678c6f3e381e074f4766
Seaborn Lee <seabornlee.cn@gmail.com>
2022-02-08 09:22:24 +0800

    Introduce ViewHolder

--- app/src/main/java/com/alphawallet/app/ui/widget/adapter/WalletAdapter.java
+++ app/src/main/java/com/alphawallet/app/ui/widget/adapter/WalletAdapter.java
@@ -6,12 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.alphawallet.app.R;
 import com.alphawallet.app.entity.Wallet;
-import com.alphawallet.app.ui.WalletConnectV2Activity;
 import com.alphawallet.app.widget.UserAvatar;
 
 import androidx.annotation.NonNull;
@@ -19,7 +17,7 @@ import androidx.annotation.Nullable;
 
 public class WalletAdapter extends ArrayAdapter<Wallet>
 {
-    public WalletAdapter(@NonNull Context context, Wallet[] wallets)
+    public WalletAdapter(Context context, Wallet[] wallets)
     {
         super(context, 0, wallets);
     }
@@ -28,29 +26,45 @@ public class WalletAdapter extends ArrayAdapter<Wallet>
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
     {
-        Wallet wallet = getItem(position);
         if (convertView == null)
         {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_wallet, parent, false);
+            convertView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_wallet, parent, false);
+
+            convertView.setTag(new ViewHolder(convertView));
         }
 
-        TextView walletName = convertView.findViewById(R.id.wallet_name);
-        TextView walletAddress = convertView.findViewById(R.id.wallet_address);
-        TextView balance = convertView.findViewById(R.id.wallet_balance);
-        TextView walletAddressSeparator = convertView.findViewById(R.id.wallet_address_separator);
-        UserAvatar userAvatar = convertView.findViewById(R.id.wallet_icon);
+        ViewHolder holder = (ViewHolder) convertView.getTag();
+
+        Wallet wallet = getItem(position);
 
         if (!TextUtils.isEmpty(wallet.ENSname))
         {
-            walletName.setText(wallet.ENSname);
+            holder.walletName.setText(wallet.ENSname);
         } else {
-            walletName.setVisibility(View.GONE);
-            walletAddressSeparator.setVisibility(View.GONE);
+            holder.walletName.setVisibility(View.GONE);
+            holder.walletAddressSeparator.setVisibility(View.GONE);
         }
-        walletAddress.setText(wallet.address);
-        userAvatar.bind(wallet);
-        balance.setText(String.format("%s %s", wallet.balance, wallet.balanceSymbol));
-
+        holder.walletAddress.setText(wallet.address);
+        holder.userAvatar.bind(wallet);
+        holder.balance.setText(String.format("%s %s", wallet.balance, wallet.balanceSymbol));
         return convertView;
+    }
+
+    static class ViewHolder {
+        TextView walletName;
+        TextView walletAddress;
+        TextView balance;
+        TextView walletAddressSeparator;
+        UserAvatar userAvatar;
+
+        public ViewHolder(@NonNull View view)
+        {
+            walletName = view.findViewById(R.id.wallet_name);
+            walletAddress = view.findViewById(R.id.wallet_address);
+            balance = view.findViewById(R.id.wallet_balance);
+            walletAddressSeparator = view.findViewById(R.id.wallet_address_separator);
+            userAvatar = view.findViewById(R.id.wallet_icon);
+        }
     }
 }

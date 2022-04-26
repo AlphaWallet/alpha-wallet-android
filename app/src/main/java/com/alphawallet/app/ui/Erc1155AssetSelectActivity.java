00a46963d92fe7379174b0bf2ed11ca8c9fe8feb
justindg <jrbdeguzman@gmail.com>
2022-04-25 20:31:11 -0700

    Rework Numeric Input Sheet

--- app/src/main/java/com/alphawallet/app/ui/Erc1155AssetSelectActivity.java
+++ app/src/main/java/com/alphawallet/app/ui/Erc1155AssetSelectActivity.java
@@ -1,11 +1,8 @@
 package com.alphawallet.app.ui;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -21,40 +18,48 @@ import com.alphawallet.app.entity.StandardFunctionInterface;
 import com.alphawallet.app.entity.Wallet;
 import com.alphawallet.app.entity.WalletType;
 import com.alphawallet.app.entity.nftassets.NFTAsset;
-import com.alphawallet.app.entity.tokens.ERC1155Token;
 import com.alphawallet.app.entity.tokens.Token;
 import com.alphawallet.app.ui.widget.OnAssetSelectListener;
 import com.alphawallet.app.ui.widget.adapter.Erc1155AssetSelectAdapter;
 import com.alphawallet.app.ui.widget.divider.ListDivider;
-import com.alphawallet.app.ui.widget.entity.AmountReadyCallback;
-import com.alphawallet.app.ui.widget.entity.NumericInputBottomSheet;
-import com.alphawallet.app.util.Utils;
+import com.alphawallet.app.ui.widget.entity.QuantitySelectorDialogInterface;
 import com.alphawallet.app.viewmodel.Erc1155AssetSelectViewModel;
 import com.alphawallet.app.widget.FunctionButtonBar;
+import com.alphawallet.app.widget.QuantitySelectorDialog;
 import com.alphawallet.ethereum.EthereumNetworkBase;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
-
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class Erc1155AssetSelectActivity extends BaseActivity implements StandardFunctionInterface, OnAssetSelectListener, AmountReadyCallback
+public class Erc1155AssetSelectActivity extends BaseActivity
+        implements StandardFunctionInterface, OnAssetSelectListener, QuantitySelectorDialogInterface
 {
-
-    Erc1155AssetSelectViewModel viewModel;
-    List<NFTAsset> selectedAssets = new ArrayList<>();
+    private Erc1155AssetSelectViewModel viewModel;
+    private QuantitySelectorDialog dialog;
+    private RecyclerView recyclerView;
+    private Erc1155AssetSelectAdapter adapter;
     private Token token;
     private Wallet wallet;
     private List<BigInteger> tokenIds;
-    private RecyclerView recyclerView;
-    private Erc1155AssetSelectAdapter adapter;
-    private NumericInputBottomSheet numericInput;
+
+    private ActivityResultLauncher<Intent> handleTransactionSuccess = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getData() == null) return;
+                String transactionHash = result.getData().getStringExtra(C.EXTRA_TXHASH);
+                //process hash
+                if (!TextUtils.isEmpty(transactionHash))
+                {
+                    Intent intent = new Intent();
+                    intent.putExtra(C.EXTRA_TXHASH, transactionHash);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            });
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
@@ -63,17 +68,24 @@ public class Erc1155AssetSelectActivity extends BaseActivity implements Standard
         setContentView(R.layout.activity_erc1155_asset_select);
         toolbar();
         setTitle(getString(R.string.title_x_selected, "0"));
-
         initViewModel();
         getIntentData();
+        initViews();
+    }
 
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        viewModel.getAssets(token, tokenIds);
+    }
+
+    private void initViews()
+    {
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.addItemDecoration(new ListDivider(this));
-
-        numericInput = findViewById(R.id.numeric_input);
-
-        viewModel.getAssets(token, tokenIds);
+        dialog = new QuantitySelectorDialog(this, this);
     }
 
     private void getIntentData()
@@ -112,63 +124,34 @@ public class Erc1155AssetSelectActivity extends BaseActivity implements Standard
         setupFunctionBar();
     }
 
+    private void updateTitle()
+    {
+        List<NFTAsset> selectedAssets = adapter.getSelectedAssets();
+        setTitle(getString(R.string.title_x_selected, String.valueOf(selectedAssets.size())));
+    }
+
     @Override
     public void onAssetSelected(BigInteger tokenId, NFTAsset asset, int position)
     {
-        selectedAssets = adapter.getSelectedAssets();
-        setTitle(getString(R.string.title_x_selected, String.valueOf(selectedAssets.size())));
+        updateTitle();
 
         if (asset.isAssetMultiple())
         {
-            int selectedValue = asset.getSelectedBalance().intValue() > 0 ? asset.getSelectedBalance().intValue() : 1;
-            numericInput.setVisibility(View.VISIBLE);
-            numericInput.initAmount(asset.getBalance().intValue(), selectedValue, this, position);
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.showSoftInput(numericInput.getInputField(), InputMethodManager.SHOW_IMPLICIT);
-            FunctionButtonBar functionBar = findViewById(R.id.layoutButtons);
-            functionBar.setVisibility(View.GONE);
+            int balance = asset.getBalance().intValue();
+            dialog.init(balance, position);
+            dialog.show();
         }
         else
         {
-            numericInput.completeLastSelection(position);
             adapter.setSelectedAmount(position, 1);
-            removeInput();
         }
     }
 
     @Override
     public void onAssetUnselected()
     {
-        selectedAssets = adapter.getSelectedAssets();
-        setTitle(getString(R.string.title_x_selected, String.valueOf(selectedAssets.size())));
-        if (numericInput.getVisibility() == View.VISIBLE)
-        {
-            removeInput();
-        }
+        updateTitle();
     }
-
-    @Override
-    public void amountReady(BigDecimal value, BigDecimal position)
-    {
-        //got new amount from widget
-        adapter.setSelectedAmount(position.intValue(), value.intValue());
-        FunctionButtonBar functionBar = findViewById(R.id.layoutButtons);
-        functionBar.setVisibility(View.VISIBLE);
-    }
-
-    ActivityResultLauncher<Intent> handleTransactionSuccess = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getData() == null) return;
-                String transactionHash = result.getData().getStringExtra(C.EXTRA_TXHASH);
-                //process hash
-                if (!TextUtils.isEmpty(transactionHash))
-                {
-                    Intent intent = new Intent();
-                    intent.putExtra(C.EXTRA_TXHASH, transactionHash);
-                    setResult(RESULT_OK, intent);
-                    finish();
-                }
-            });
 
     @Override
     public void showTransferToken(List<BigInteger> selection)
@@ -177,10 +160,16 @@ public class Erc1155AssetSelectActivity extends BaseActivity implements Standard
         handleTransactionSuccess.launch(launchTransfer);
     }
 
-    private void removeInput()
+    @Override
+    public void onConfirm(int position, int quantity)
     {
-        numericInput.setVisibility(View.GONE);
-        FunctionButtonBar functionBar = findViewById(R.id.layoutButtons);
-        functionBar.setVisibility(View.VISIBLE);
+        adapter.setSelectedAmount(position, quantity);
+    }
+
+    @Override
+    public void onCancel(int position)
+    {
+        adapter.setSelectedAmount(position, 0);
+        updateTitle();
     }
 }

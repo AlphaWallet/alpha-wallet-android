f95f4400ae7e49f4aa6086025890e4b4b828e9bc
Seaborn Lee <seabornlee.cn@gmail.com>
2022-05-24 19:19:36 +0800

    Auto generate release notes and remove redundant info

--- release-github.sh
+++ release-github.sh
@@ -9,5 +9,14 @@ src_apk=$(pwd)/app/build/outputs/apk/noAnalytics/release/app-noAnalytics-release
 dst_apk=$(pwd)/AlphaWallet-$tag.apk
 
 cp $src_apk $dst_apk
-gh release create -d $tag $dst_apk
+gh release create --generate-notes -d $tag $dst_apk
+
+temp_file=build/temp_release_notes.txt
+gh release view $tag --json body -q .body > $temp_file
+
+sed -i '' -n '2,/^$/p' $temp_file
+sed -i '' 's/by\ @.*$//' $temp_file
+gh release edit $tag -F $temp_file
+
 rm $dst_apk
+rm $temp_file

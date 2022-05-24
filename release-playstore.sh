124eea740bd1b26e25cb647ecd5241660caf7160
Seaborn Lee <seabornlee.cn@gmail.com>
2022-05-24 19:25:36 +0800

    Fix release PlayStore script

--- release-playstore.sh
+++ release-playstore.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+version_name=$(./gradlew -q printVersionName)
+tag=v$version_name
 release_notes=$(gh release view $tag --json body -q .body)
+
 version_code=$(./gradlew -q printVersionCode)
 echo $release_notes > ./fastlane/metadata/android/en-US/changelogs/$version_code.txt 
 fastlane android beta

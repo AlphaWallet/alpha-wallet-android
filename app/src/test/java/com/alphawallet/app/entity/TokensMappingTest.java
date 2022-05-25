e61d6a61c3b3c3f3ad249a8bbae40f44353ce878
Seaborn Lee <seabornlee.cn@gmail.com>
2022-05-25 17:10:46 +0800

    2622 defi governance tabs empty (#2624)
    * Fix 1inch not parsed as Governance group
    * Fix JSON parsing error
    * optimise token storage

--- app/src/test/java/com/alphawallet/app/entity/TokensMappingTest.java
+++ app/src/test/java/com/alphawallet/app/entity/TokensMappingTest.java
@@ -5,21 +5,44 @@ import com.alphawallet.token.entity.ContractAddress;
 import com.google.gson.Gson;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class TokensMappingTest {
+    private String groupString;
+    private TokenGroup groupEnum;
+
+    public TokensMappingTest(String groupString, TokenGroup groupEnum) {
+        this.groupString = groupString;
+        this.groupEnum = groupEnum;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"Assets", TokenGroup.ASSET},
+                {"DeFi", TokenGroup.DEFI},
+                {"Governance", TokenGroup.GOVERNANCE},
+        });
+    }
+
     @Test
     public void serializeAndDeserialize() {
-        String rawJson = "{\"contracts\":[{\"address\":\"abcdefg\",\"chainId\":456789}],\"group\":\"Assets\"}";
+        String rawJson = "{\"contracts\":[{\"address\":\"abcdefg\",\"chainId\":456789}],\"group\":\"" + groupString + "\"}";
         TokensMapping tokensMapping = new Gson().fromJson(rawJson, TokensMapping.class);
 
         ContractAddress contract = tokensMapping.getContracts().get(0);
         assertThat(contract.address, equalTo("abcdefg"));
         assertThat(contract.chainId, equalTo(456789L));
 
-        assertThat(tokensMapping.getGroup(), equalTo(TokenGroup.ASSET));
+        assertThat(tokensMapping.getGroup(), equalTo(groupEnum));
 
         //Can we go back to JSON, then back again and get the same result?
         String json = new Gson().toJson(tokensMapping);
@@ -27,6 +50,7 @@ public class TokensMappingTest {
         contract = tokensMapping.getContracts().get(0);
         assertThat(contract.address, equalTo("abcdefg"));
         assertThat(contract.chainId, equalTo(456789L));
-        assertThat(tokensMapping.getGroup(), equalTo(TokenGroup.ASSET));
+        assertThat(tokensMapping.getGroup(), equalTo(groupEnum));
     }
+
 }

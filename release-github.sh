b190da21bb39a3fdd411eb74c1cc0d0d2f7f6f6e
Seaborn Lee <seabornlee.cn@gmail.com>
2022-05-27 11:16:45 +0800

    Replace * with - in change log

--- release-github.sh
+++ release-github.sh
@@ -16,6 +16,7 @@ gh release view $tag --json body -q .body > $temp_file
 
 sed -i '' -n '2,/^$/p' $temp_file
 sed -i '' 's/\ by\ @.*$//' $temp_file
+sed -i '' 's/*\ /-\ /' $temp_file
 gh release edit $tag -F $temp_file
 
 rm $dst_apk

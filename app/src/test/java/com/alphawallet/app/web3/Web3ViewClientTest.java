25db0de34df3e892ecbdf2abd0809f387818b6d5
Seaborn Lee <seabornlee.cn@gmail.com>
2022-06-16 15:58:34 +0800

    test: should notify user if telegram not installed

--- app/src/test/java/com/alphawallet/app/web3/Web3ViewClientTest.java
+++ app/src/test/java/com/alphawallet/app/web3/Web3ViewClientTest.java
@@ -31,11 +31,15 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowToast;
 
 @RunWith(AndroidJUnit4.class)
 @Config(shadows = {ShadowRealm.class, ShadowJsInjectorClient.class})
 public class Web3ViewClientTest
 {
+    private static final String PACKAGE_NAME_OF_TELEGRAM = "org.telegram.messenger";
+    private static final String URL_TG_JOIN_INVITE = "tg:join?invite=12345";
+
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
 
@@ -58,19 +62,35 @@ public class Web3ViewClientTest
     public void should_override_url_loading_and_start_telegram_if_installed()
     {
         Context context = Mockito.spy(ApplicationProvider.getApplicationContext());
-        String packageName = "org.telegram.messenger";
-        assumeAppInstalled(context, packageName);
-        String url = "tg:join?invite=12345";
-        boolean overrideUrlLoading = new Web3ViewClient(context).shouldOverrideUrlLoading(null, url);
+        assumeAppInstalled(context, PACKAGE_NAME_OF_TELEGRAM);
+        boolean overrideUrlLoading = new Web3ViewClient(context).shouldOverrideUrlLoading(null, URL_TG_JOIN_INVITE);
         assertTrue(overrideUrlLoading);
 
         Mockito.verify(context).startActivity(intentCaptor.capture());
         Intent expectedIntent = intentCaptor.getValue();
         assertThat(expectedIntent.getAction(), is(ACTION_VIEW));
-        assertThat(expectedIntent.getData(), is(Uri.parse(url)));
-        assertThat(expectedIntent.getPackage(), is(packageName));
+        assertThat(expectedIntent.getData(), is(Uri.parse(URL_TG_JOIN_INVITE)));
+        assertThat(expectedIntent.getPackage(), is(PACKAGE_NAME_OF_TELEGRAM));
         assertThat(expectedIntent.getFlags(), is(FLAG_ACTIVITY_NEW_TASK));
-        assertThat(expectedIntent.getStringExtra(EXTRA_TEXT), is(url));
+        assertThat(expectedIntent.getStringExtra(EXTRA_TEXT), is(URL_TG_JOIN_INVITE));
+    }
+
+    @Test
+    public void should_override_url_loading_and_notify_user_if_telegram_not_installed() throws PackageManager.NameNotFoundException
+    {
+        Context context = Mockito.spy(ApplicationProvider.getApplicationContext());
+        assumeNotAppInstalled(context, PACKAGE_NAME_OF_TELEGRAM);
+        boolean overrideUrlLoading = new Web3ViewClient(context).shouldOverrideUrlLoading(null, URL_TG_JOIN_INVITE);
+        assertTrue(overrideUrlLoading);
+
+        assertThat(ShadowToast.getTextOfLatestToast(), is("Required App not Installed"));
+    }
+
+    private void assumeNotAppInstalled(Context context, String packageName) throws PackageManager.NameNotFoundException
+    {
+        PackageManager mockPm = Mockito.mock(PackageManager.class);
+        Mockito.doThrow(PackageManager.NameNotFoundException.class).when(mockPm).getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+        Mockito.doReturn(mockPm).when(context).getPackageManager();
     }
 
     private void assumeAppInstalled(Context context, String packageName)

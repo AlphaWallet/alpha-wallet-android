9408ee5fe1c34001895e176ead556840cc838914
James Brown <soylentblue@hotmail.co.uk>
2022-07-04 23:34:26 +0100

    simplify logic

--- app/src/main/java/com/alphawallet/app/entity/tokens/Token.java
+++ app/src/main/java/com/alphawallet/app/entity/tokens/Token.java
@@ -224,7 +224,7 @@ public class Token
     public String getName()
     {
         if (!TextUtils.isEmpty(tokenInfo.symbol) && (TextUtils.isEmpty(tokenInfo.name) || tokenInfo.name.length() < tokenInfo.symbol.length())) {
-            return sanitiseString(!TextUtils.isEmpty(tokenInfo.symbol) ? tokenInfo.symbol : "");
+            return sanitiseString(tokenInfo.symbol);
         } else {
             return sanitiseString(!TextUtils.isEmpty(tokenInfo.name) ? tokenInfo.name : "");
         }

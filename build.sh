15ecc8725a44b5aa6b2438e26832b3e7fbf844e1
Seaborn Lee <seabornlee.cn@gmail.com>
2022-07-27 18:47:23 +0800

    Run specific flavor

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 cd dmz && ../gradlew -i build && ../gradlew -i test && cd ..
 cd lib && ../gradlew -i build && ../gradlew -i test && cd ..
 cd util && ../gradlew -i build && ../gradlew -i test && cd ..
-./gradlew build jacocoTestReport -x lint -x detekt
+./gradlew clean jacocoTestNoAnalyticsDebugUnitTestReport -x lint -x detekt

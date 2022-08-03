514c281c4920b7771ef3d73cdc5459b847e7c5c2
Seaborn Lee <seabornlee.cn@gmail.com>
2022-08-03 15:07:34 +0800

    Fix typo

--- app/src/main/java/com/alphawallet/app/repository/HttpServiceHelper.java
+++ app/src/main/java/com/alphawallet/app/repository/HttpServiceHelper.java
@@ -1,6 +1,6 @@
 package com.alphawallet.app.repository;
 
-import static com.alphawallet.ethereum.EthereumNetworkBase.KLAYTN_BOABAB_ID;
+import static com.alphawallet.ethereum.EthereumNetworkBase.KLAYTN_BAOBAB_ID;
 import static com.alphawallet.ethereum.EthereumNetworkBase.KLAYTN_ID;
 
 import org.web3j.protocol.http.HttpService;
@@ -9,7 +9,7 @@ public class HttpServiceHelper
 {
     public static void addRequiredCredentials(long chainId, HttpService httpService, String key)
     {
-        if ((chainId == KLAYTN_BOABAB_ID || chainId == KLAYTN_ID) && EthereumNetworkBase.usesProductionKey)
+        if ((chainId == KLAYTN_BAOBAB_ID || chainId == KLAYTN_ID) && EthereumNetworkBase.usesProductionKey)
         {
             httpService.addHeader("x-chain-id", Long.toString(chainId));
             httpService.addHeader("Authorization", "Basic " + key);

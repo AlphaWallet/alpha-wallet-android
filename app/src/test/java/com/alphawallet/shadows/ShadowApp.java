21fc6e9c325628928e4fa8df50a605776f6e5ac1
Seaborn Lee <seabornlee.cn@gmail.com>
2022-08-03 15:32:59 +0800

    fix ci

--- app/src/test/java/com/alphawallet/shadows/ShadowApp.java
+++ app/src/test/java/com/alphawallet/shadows/ShadowApp.java
@@ -1,12 +1,14 @@
 package com.alphawallet.shadows;
 
+import android.app.Application;
+
 import com.alphawallet.app.App;
 
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
 @Implements(App.class)
-public class ShadowApp
+public class ShadowApp extends Application
 {
     @Implementation
     public void onCreate()

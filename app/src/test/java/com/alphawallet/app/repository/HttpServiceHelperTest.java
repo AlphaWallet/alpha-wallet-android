60684feab740b5fcb4ffb6febf6c0bf9b11c4cfb
Seaborn Lee <seabornlee.cn@gmail.com>
2022-08-04 17:08:14 +0800

    Fix test

--- app/src/test/java/com/alphawallet/app/repository/HttpServiceHelperTest.java
+++ app/src/test/java/com/alphawallet/app/repository/HttpServiceHelperTest.java
@@ -1,27 +1,20 @@
 package com.alphawallet.app.repository;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.verify;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import com.alphawallet.shadows.ShadowApp;
-import com.alphawallet.shadows.ShadowSystem;
 import com.alphawallet.utils.ReflectionUtil;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.annotation.Config;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.web3j.protocol.http.HttpService;
 
-import java.util.HashMap;
-
-@RunWith(AndroidJUnit4.class)
-@Config(shadows = {ShadowApp.class, ShadowSystem.class})
+@RunWith(PowerMockRunner.class)
 public class HttpServiceHelperTest
 {
+    @Mock
     private HttpService httpService;
 
     @BeforeClass
@@ -30,28 +23,20 @@ public class HttpServiceHelperTest
         ReflectionUtil.setFinalFieldTo(EthereumNetworkBase.class, "usesProductionKey", true);
     }
 
-    @Before
-    public void setUp()
-    {
-        httpService = new HttpService();
-    }
-
     @Test
     public void should_addRequiredCredentials_for_Klaytn_baobab() throws Exception
     {
         HttpServiceHelper.addRequiredCredentials(1001L, httpService, "klaytn-key");
-        HashMap<String, String> headers = httpService.getHeaders();
-        assertThat(headers.get("x-chain-id"), equalTo("1001"));
-        assertThat(headers.get("Authorization"), equalTo("Basic klaytn-key"));
+        verify(httpService).addHeader("x-chain-id", "1001");
+        verify(httpService).addHeader("Authorization", "Basic klaytn-key");
     }
 
     @Test
-    public void should_addRequiredCredentials_for_KLAYTN() throws Exception
+    public void should_addRequiredCredentials_for_Klaytn() throws Exception
     {
         HttpServiceHelper.addRequiredCredentials(8217, httpService, "klaytn-key");
-        HashMap<String, String> headers = httpService.getHeaders();
-        assertThat(headers.get("x-chain-id"), equalTo("8217"));
-        assertThat(headers.get("Authorization"), equalTo("Basic klaytn-key"));
+        verify(httpService).addHeader("x-chain-id", "8217");
+        verify(httpService).addHeader("Authorization", "Basic klaytn-key");
     }
 
 }

e7fc97cf12c03ed5ac01e46c584035aea254fab3
Seaborn Lee <seabornlee.cn@gmail.com>
2022-08-09 14:48:18 +0800

    Fix e2e test

--- app/src/androidTest/java/com/alphawallet/app/SelectNetworkTest.java
+++ app/src/androidTest/java/com/alphawallet/app/SelectNetworkTest.java
@@ -1,10 +1,7 @@
 package com.alphawallet.app;
 
-import static com.alphawallet.app.assertions.Should.shouldSee;
+import static com.alphawallet.app.assertions.Should.shouldSeeTitle;
 import static com.alphawallet.app.steps.Steps.createNewWallet;
-import static com.alphawallet.app.steps.Steps.toggleSwitch;
-
-import com.alphawallet.app.steps.Steps;
 
 import org.junit.Test;
 
@@ -12,6 +9,7 @@ public class SelectNetworkTest extends BaseE2ETest {
     @Test
     public void should_select_network_when_first_create_wallet() {
         createNewWallet();
-        shouldSee(R.id.mainnet_header);
+//        shouldSee(R.id.mainnet_header);
+        shouldSeeTitle("Select Active Networks");
     }
 }
--- app/src/androidTest/java/com/alphawallet/app/assertions/Should.java
+++ app/src/androidTest/java/com/alphawallet/app/assertions/Should.java
@@ -1,10 +1,19 @@
 package com.alphawallet.app.assertions;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withSubstring;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.alphawallet.app.util.Helper.waitUntil;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+
+import android.widget.TextView;
+
+import com.alphawallet.app.R;
 
 public class Should
 {
@@ -16,5 +25,11 @@ public class Should
     public static void shouldSee(int id)
     {
         onView(isRoot()).perform(waitUntil(withId(id), 10 * 60));
+    }
+
+    public static void shouldSeeTitle(String title)
+    {
+        onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.toolbar))))
+                .check(matches(withText(title)));
     }
 }

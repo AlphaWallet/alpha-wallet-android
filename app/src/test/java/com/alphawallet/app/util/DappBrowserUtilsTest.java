eef48e1b004e253c5e925c5c833a28dbc94b3cbe
Seaborn Lee <seabornlee.cn@gmail.com>
2022-08-09 15:43:41 +0800

    Move test from androidTest to test folder

--- app/src/test/java/com/alphawallet/app/util/DappBrowserUtilsTest.java
+++ app/src/test/java/com/alphawallet/app/util/DappBrowserUtilsTest.java
@@ -2,7 +2,8 @@ package com.alphawallet.app.util;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 

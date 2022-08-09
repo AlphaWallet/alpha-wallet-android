241f9eee7144f252308f5a93a0e2e53005a5835c
Seaborn Lee <seabornlee.cn@gmail.com>
2022-08-09 15:44:10 +0800

    Revert "Fix e2e"
    This reverts commit 5fb2c55b511cf454d7b5ffb49ef8cc3d1fd9d412.

--- app/src/androidTest/java/com/alphawallet/app/SelectNetworkTest.java
+++ app/src/androidTest/java/com/alphawallet/app/SelectNetworkTest.java
@@ -1,33 +1,15 @@
 package com.alphawallet.app;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static com.alphawallet.app.assertions.Should.shouldSeeTitle;
+import static com.alphawallet.app.steps.Steps.createNewWallet;
 
-import android.widget.TextView;
-
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-
-import com.alphawallet.app.ui.SplashActivity;
-
-import org.junit.Rule;
 import org.junit.Test;
 
-public class SelectNetworkTest {
-    @Rule
-    public ActivityScenarioRule<SplashActivity> activityScenarioRule
-            = new ActivityScenarioRule<>(SplashActivity.class);
-    
+public class SelectNetworkTest extends BaseE2ETest {
     @Test
     public void should_select_network_when_first_create_wallet() {
-        onView(withText(R.string.ok)).perform(ViewActions.click());
-        onView(withId(R.id.button_create)).perform(ViewActions.click());
-        onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.toolbar))))
-                .check(matches(withText("Select Active Networks")));
+        createNewWallet();
+//        shouldSee(R.id.mainnet_header);
+        shouldSeeTitle("Select Active Networks");
     }
 }

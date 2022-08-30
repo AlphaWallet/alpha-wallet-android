2fe5d8e8f12f382f54235dbc08f22ed83f47b195
Seaborn Lee <seabornlee.cn@gmail.com>
2022-08-30 10:07:15 +0800

    Close ANR dialog if exists

--- app/src/androidTest/java/com/alphawallet/app/BaseE2ETest.java
+++ app/src/androidTest/java/com/alphawallet/app/BaseE2ETest.java
@@ -2,17 +2,23 @@ package com.alphawallet.app;
 
 
 import static androidx.test.espresso.Espresso.setFailureHandler;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 import static com.alphawallet.app.steps.Steps.closeSecurityWarning;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
 
 import com.alphawallet.app.ui.SplashActivity;
 import com.alphawallet.app.util.CustomFailureHandler;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
@@ -25,13 +31,23 @@ public abstract class BaseE2ETest
     @Rule
     public TestRule watcher = new TestWatcher() {
         protected void starting(Description description) {
-            setFailureHandler(new CustomFailureHandler(description.getMethodName(), InstrumentationRegistry.getInstrumentation().getTargetContext()));
+            setFailureHandler(new CustomFailureHandler(description.getMethodName(), getInstrumentation().getTargetContext()));
         }
     };
 
     @Rule
     public ActivityScenarioRule<SplashActivity> activityScenarioRule
             = new ActivityScenarioRule<>(SplashActivity.class);
+
+    @BeforeClass
+    public static void dismissANRSystemDialog() throws UiObjectNotFoundException
+    {
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        UiObject waitButton = device.findObject(new UiSelector().textContains("wait"));
+        if (waitButton.exists()) {
+            waitButton.click();
+        }
+    }
 
     @Before
     public void setUp()

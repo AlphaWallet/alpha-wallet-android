6bdbccf5fa42ab714de6f792f65e278e3d824a43
Seaborn Lee <seabornlee.cn@gmail.com>
2022-09-02 22:11:47 +0800

    WIP

--- myapplication/src/main/java/cn/seabornlee/myapplication/FirstFragment.java
+++ myapplication/src/main/java/cn/seabornlee/myapplication/FirstFragment.java
@@ -1,22 +1,12 @@
 package cn.seabornlee.myapplication;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
-import com.alphawallet.app.App;
-import com.alphawallet.app.entity.ImportWalletCallback;
-import com.alphawallet.app.entity.SignAuthenticationCallback;
-import com.alphawallet.app.entity.Wallet;
-import com.alphawallet.app.entity.cryptokeys.KeyEncodingType;
-import com.alphawallet.app.service.KeyService;
-import com.alphawallet.app.service.KeystoreAccountService;
 
 import cn.seabornlee.myapplication.databinding.FragmentFirstBinding;
 
@@ -46,34 +36,34 @@ public class FirstFragment extends Fragment
             @Override
             public void onClick(View view)
             {
-//                NavHostFragment.findNavController(FirstFragment.this)
-//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-                KeyService keyService = new KeyService(getContext(), null);
-                keyService.importHDKey("essence allow crisp figure tired task melt honey reduce planet twenty rookie", getActivity(), new ImportWalletCallback()
-                {
-                    @Override
-                    public void walletValidated(String s, KeyEncodingType keyEncodingType, KeyService.AuthenticationLevel authenticationLevel)
-                    {
-                        Log.d("seaborn", s);
-                    }
-                });
+////                NavHostFragment.findNavController(FirstFragment.this)
+////                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//                KeyService keyService = new KeyService(getContext(), null);
+//                keyService.importHDKey("essence allow crisp figure tired task melt honey reduce planet twenty rookie", getActivity(), new ImportWalletCallback()
+//                {
+//                    @Override
+//                    public void walletValidated(String s, KeyEncodingType keyEncodingType, KeyService.AuthenticationLevel authenticationLevel)
+//                    {
+//                        Log.d("seaborn", s);
+//                    }
+//                });
 
-                Wallet wallet = new Wallet("0xD0c424B3016E9451109ED97221304DeC639b3F84");
-                keyService.getAuthenticationForSignature(wallet, getActivity(), new SignAuthenticationCallback()
-                {
-                    @Override
-                    public void gotAuthorisation(boolean b)
-                    {
-                        KeystoreAccountService keystoreAccountService = new KeystoreAccountService(null, null, keyService);
-                        keystoreAccountService.signMessage(wallet, null, 1);
-                    }
-
-                    @Override
-                    public void cancelAuthentication()
-                    {
-
-                    }
-                });
+//                Wallet wallet = new Wallet("0xD0c424B3016E9451109ED97221304DeC639b3F84");
+//                keyService.getAuthenticationForSignature(wallet, getActivity(), new SignAuthenticationCallback()
+//                {
+//                    @Override
+//                    public void gotAuthorisation(boolean b)
+//                    {
+//                        KeystoreAccountService keystoreAccountService = new KeystoreAccountService(null, null, keyService);
+//                        keystoreAccountService.signMessage(wallet, null, 1);
+//                    }
+//
+//                    @Override
+//                    public void cancelAuthentication()
+//                    {
+//
+//                    }
+//                });
             }
         });
     }

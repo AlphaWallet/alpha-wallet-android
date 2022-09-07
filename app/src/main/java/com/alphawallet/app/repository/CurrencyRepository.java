30bc43ad2e7d8624b028e9e645a65a73011cc3ee
Seaborn Lee <seabornlee.cn@gmail.com>
2022-09-07 10:49:38 +0800

    Normalise Currency labels

--- app/src/main/java/com/alphawallet/app/repository/CurrencyRepository.java
+++ app/src/main/java/com/alphawallet/app/repository/CurrencyRepository.java
@@ -25,7 +25,7 @@ public class CurrencyRepository implements CurrencyRepositoryType {
             new CurrencyItem("VND", "Vietnamese đồng", "₫", R.drawable.ic_flags_vietnam),
             new CurrencyItem("PKR", "Pakistani Rupee", "Rs", R.drawable.ic_flags_pakistan),
             new CurrencyItem("MMK", "Myanmar Kyat", "Ks", R.drawable.ic_flags_myanmar),
-            new CurrencyItem("IDR", "Indonesian rupiah", "Rp", R.drawable.ic_flags_indonesia)
+            new CurrencyItem("IDR", "Indonesian Rupiah", "Rp", R.drawable.ic_flags_indonesia)
     };
 
     private final PreferenceRepositoryType preferences;

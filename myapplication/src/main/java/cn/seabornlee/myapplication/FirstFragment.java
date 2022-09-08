7f4a4db8cf42de0fb6dab9bdd9447a5e8ca60d0b
Seaborn Lee <seabornlee.cn@gmail.com>
2022-09-08 13:52:58 +0800

    It works

--- myapplication/src/main/java/cn/seabornlee/myapplication/FirstFragment.java
+++ myapplication/src/main/java/cn/seabornlee/myapplication/FirstFragment.java
@@ -1,6 +1,9 @@
 package cn.seabornlee.myapplication;
 
+import static com.alphawallet.app.service.KeystoreAccountService.KEYSTORE_FOLDER;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +11,24 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.alphawallet.app.entity.ImportWalletCallback;
+import com.alphawallet.app.entity.SignAuthenticationCallback;
+import com.alphawallet.app.entity.Wallet;
+import com.alphawallet.app.entity.cryptokeys.KeyEncodingType;
+import com.alphawallet.app.entity.cryptokeys.SignatureFromKey;
+import com.alphawallet.app.service.KeyService;
+import com.alphawallet.app.service.KeystoreAccountService;
+
+import java.io.File;
+import java.math.BigInteger;
+
 import cn.seabornlee.myapplication.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment
 {
 
     private FragmentFirstBinding binding;
+
 
     @Override
     public View onCreateView(
@@ -36,36 +51,52 @@ public class FirstFragment extends Fragment
             @Override
             public void onClick(View view)
             {
-////                NavHostFragment.findNavController(FirstFragment.this)
-////                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-//                KeyService keyService = new KeyService(getContext(), null);
-//                keyService.importHDKey("essence allow crisp figure tired task melt honey reduce planet twenty rookie", getActivity(), new ImportWalletCallback()
-//                {
-//                    @Override
-//                    public void walletValidated(String s, KeyEncodingType keyEncodingType, KeyService.AuthenticationLevel authenticationLevel)
-//                    {
-//                        Log.d("seaborn", s);
-//                    }
-//                });
+                KeyService keyService = new KeyService(getContext(), null);
+                keyService.importHDKey("essence allow crisp figure tired task melt honey reduce planet twenty rookie", getActivity(), new ImportWalletCallback()
+                {
+                    @Override
+                    public void walletValidated(String address, KeyEncodingType keyEncodingType, KeyService.AuthenticationLevel authenticationLevel)
+                    {
+                        binding.textviewFirst.setText(address);
 
-//                Wallet wallet = new Wallet("0xD0c424B3016E9451109ED97221304DeC639b3F84");
-//                keyService.getAuthenticationForSignature(wallet, getActivity(), new SignAuthenticationCallback()
-//                {
-//                    @Override
-//                    public void gotAuthorisation(boolean b)
-//                    {
-//                        KeystoreAccountService keystoreAccountService = new KeystoreAccountService(null, null, keyService);
-//                        keystoreAccountService.signMessage(wallet, null, 1);
-//                    }
-//
-//                    @Override
-//                    public void cancelAuthentication()
-//                    {
-//
-//                    }
-//                });
+                        Wallet wallet = new Wallet(address);
+                        keyService.getAuthenticationForSignature(wallet, getActivity(), new SignAuthenticationCallback()
+                        {
+
+                            @Override
+                            public void gotAuthorisation(boolean b)
+                            {
+                                Log.d("seaborn", String.valueOf(b));
+                                File file = new File(getContext().getFilesDir(), KEYSTORE_FOLDER);
+                                KeystoreAccountService keystoreAccountService = new KeystoreAccountService(file, getContext().getFilesDir(), keyService);
+                                SignatureFromKey signatureFromKey = keystoreAccountService.signTransaction(wallet, address, BigInteger.ZERO, BigInteger.ZERO, BigInteger.ZERO
+                                        , 1, "".getBytes(), 1).blockingGet();
+                                binding.textviewSig.setText(new String(signatureFromKey.signature));
+                            }
+
+                            @Override
+                            public void cancelAuthentication()
+                            {
+
+                            }
+                        });
+
+                    }
+                });
             }
         });
+    }
+
+    private void sleep()
+    {
+        try
+        {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     @Override

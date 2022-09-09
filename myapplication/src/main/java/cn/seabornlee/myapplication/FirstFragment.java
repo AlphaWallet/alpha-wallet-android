b6535c112e49f1f7b56853eda6990d01159cbd41
Seaborn Lee <seabornlee.cn@gmail.com>
2022-09-09 16:21:04 +0800

    Show signature

--- myapplication/src/main/java/cn/seabornlee/myapplication/FirstFragment.java
+++ myapplication/src/main/java/cn/seabornlee/myapplication/FirstFragment.java
@@ -3,7 +3,6 @@ package cn.seabornlee.myapplication;
 import static com.alphawallet.app.service.KeystoreAccountService.KEYSTORE_FOLDER;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +13,17 @@ import androidx.fragment.app.Fragment;
 import com.alphawallet.app.entity.ImportWalletCallback;
 import com.alphawallet.app.entity.SignAuthenticationCallback;
 import com.alphawallet.app.entity.Wallet;
+import com.alphawallet.app.entity.WalletType;
 import com.alphawallet.app.entity.cryptokeys.KeyEncodingType;
 import com.alphawallet.app.entity.cryptokeys.SignatureFromKey;
 import com.alphawallet.app.service.KeyService;
 import com.alphawallet.app.service.KeystoreAccountService;
 
+import org.web3j.crypto.Sign;
+
 import java.io.File;
 import java.math.BigInteger;
+import java.util.Arrays;
 
 import cn.seabornlee.myapplication.databinding.FragmentFirstBinding;
 
@@ -46,57 +49,42 @@ public class FirstFragment extends Fragment
     {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
+        binding.buttonFirst.setOnClickListener(view1 -> {
+            KeyService keyService = new KeyService(getContext(), null);
+            keyService.importHDKey("essence allow crisp figure tired task melt honey reduce planet twenty rookie", getActivity(), new ImportWalletCallback()
             {
-                KeyService keyService = new KeyService(getContext(), null);
-                keyService.importHDKey("essence allow crisp figure tired task melt honey reduce planet twenty rookie", getActivity(), new ImportWalletCallback()
+                @Override
+                public void walletValidated(String address, KeyEncodingType keyEncodingType, KeyService.AuthenticationLevel authenticationLevel)
                 {
-                    @Override
-                    public void walletValidated(String address, KeyEncodingType keyEncodingType, KeyService.AuthenticationLevel authenticationLevel)
-                    {
-                        binding.textviewFirst.setText(address);
+                    binding.textviewFirst.setText(address);
 
-                        Wallet wallet = new Wallet(address);
-                        keyService.getAuthenticationForSignature(wallet, getActivity(), new SignAuthenticationCallback()
+                    Wallet wallet = new Wallet(address);
+                    wallet.setWalletType(WalletType.HDKEY);
+                    keyService.getAuthenticationForSignature(wallet, getActivity(), new SignAuthenticationCallback()
+                    {
+
+                        @Override
+                        public void gotAuthorisation(boolean b)
+                        {
+                            File file = new File(getContext().getFilesDir(), KEYSTORE_FOLDER);
+                            KeystoreAccountService keystoreAccountService = new KeystoreAccountService(file, getContext().getFilesDir(), keyService);
+                            String msg = "hello";
+                            SignatureFromKey signatureFromKey = keystoreAccountService.signTransactionEIP1559(wallet, address, BigInteger.ZERO, BigInteger.ONE, BigInteger.ONE, BigInteger.ONE, 0, msg.getBytes(), 1L).blockingGet();
+
+                            String hash = Numeric.toHexString(signatureFromKey.signature);
+                            binding.textviewSig.setText(hash);
+                        }
+
+                        @Override
+                        public void cancelAuthentication()
                         {
 
-                            @Override
-                            public void gotAuthorisation(boolean b)
-                            {
-                                Log.d("seaborn", String.valueOf(b));
-                                File file = new File(getContext().getFilesDir(), KEYSTORE_FOLDER);
-                                KeystoreAccountService keystoreAccountService = new KeystoreAccountService(file, getContext().getFilesDir(), keyService);
-                                SignatureFromKey signatureFromKey = keystoreAccountService.signTransaction(wallet, address, BigInteger.ZERO, BigInteger.ZERO, BigInteger.ZERO
-                                        , 1, "".getBytes(), 1).blockingGet();
-                                binding.textviewSig.setText(new String(signatureFromKey.signature));
-                            }
+                        }
+                    });
 
-                            @Override
-                            public void cancelAuthentication()
-                            {
-
-                            }
-                        });
-
-                    }
-                });
-            }
+                }
+            });
         });
-    }
-
-    private void sleep()
-    {
-        try
-        {
-            Thread.sleep(1000);
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
     }
 
     @Override

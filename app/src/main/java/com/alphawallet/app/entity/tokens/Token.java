2685e0b82100d163999851e5b2b39ca993d6e04b
James Brown <soylentblue@hotmail.co.uk>
2022-09-20 11:12:59 +1000

    Ensure MetaMask addresses are handled correctly (#2829)

--- app/src/main/java/com/alphawallet/app/entity/tokens/Token.java
+++ app/src/main/java/com/alphawallet/app/entity/tokens/Token.java
@@ -49,6 +49,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 

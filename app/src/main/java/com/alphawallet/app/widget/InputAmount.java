00118c1fce9fbce928431998cfe1b04f5028e9b1
James Brown <soylentblue@hotmail.co.uk>
2022-09-20 14:03:00 +1000

    fix formatting

--- app/src/main/java/com/alphawallet/app/widget/InputAmount.java
+++ app/src/main/java/com/alphawallet/app/widget/InputAmount.java
@@ -1,5 +1,8 @@
 package com.alphawallet.app.widget;
 
+import static com.alphawallet.app.C.GAS_LIMIT_MIN;
+import static com.alphawallet.app.repository.TokensRealmSource.databaseKey;
+
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Handler;
@@ -43,9 +46,6 @@ import io.realm.Case;
 import io.realm.Realm;
 import io.realm.RealmQuery;
 import timber.log.Timber;
-
-import static com.alphawallet.app.C.GAS_LIMIT_MIN;
-import static com.alphawallet.app.repository.TokensRealmSource.databaseKey;
 
 /**
  * Created by JB on 10/11/2020.
@@ -387,8 +387,8 @@ public class InputAmount extends LinearLayout
             if (token.isEthereum() && token.hasPositiveBalance())
             {
                 RealmGasSpread gasSpread = tokensService.getTickerRealmInstance().where(RealmGasSpread.class)
-                            .equalTo("chainId", token.tokenInfo.chainId)
-                            .findFirst();
+                        .equalTo("chainId", token.tokenInfo.chainId)
+                        .findFirst();
 
                 if (gasSpread != null && gasSpread.getGasPrice().compareTo(BigInteger.ZERO) > 0)
                 {
@@ -399,19 +399,19 @@ public class InputAmount extends LinearLayout
                 {
                     gasFetch.setVisibility(View.VISIBLE);
                     Web3j web3j = TokenRepository.getWeb3jService(token.tokenInfo.chainId);
-                    Completable.fromRunnable( () ->
-                    {
-                        try
-                        {
-                            onLatestGasPrice(web3j.ethGasPrice().sendAsync().get().getGasPrice());
-                        }
-                        catch (Exception e)
-                        {
-                            e.printStackTrace();
-                            onGasFetchError(e);
-                        }
-                    }
-                    )
+                    Completable.fromRunnable(() ->
+                                    {
+                                        try
+                                        {
+                                            onLatestGasPrice(web3j.ethGasPrice().sendAsync().get().getGasPrice());
+                                        }
+                                        catch (Exception e)
+                                        {
+                                            e.printStackTrace();
+                                            onGasFetchError(e);
+                                        }
+                                    }
+                            )
                             .subscribeOn(Schedulers.io())
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe(() -> {}, this::onGasFetchError)

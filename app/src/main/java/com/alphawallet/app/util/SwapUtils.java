1a52583c98153788e7803eb97cae5a31cef933d4
justindg <jrbdeguzman@gmail.com>
2022-09-21 14:04:16 -0700

    Make member private

--- app/src/main/java/com/alphawallet/app/util/SwapUtils.java
+++ app/src/main/java/com/alphawallet/app/util/SwapUtils.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class SwapUtils
 {
-    public static final String CURRENT_PRICE_FORMAT = "1 %s ≈ %s %s";
+    private static final String CURRENT_PRICE_FORMAT = "1 %s ≈ %s %s";
     private static final String GAS_PRICE_FORMAT = "%s %s";
     private static final String FEE_FORMAT = "%s %s";
     private static final String MINIMUM_RECEIVED_FORMAT = "%s %s";

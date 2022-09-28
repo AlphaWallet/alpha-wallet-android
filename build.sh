78d6f097fd410592858ba760654f2cde9162c03f
Seaborn Lee <seabornlee.cn@gmail.com>
2022-09-28 13:02:07 +0800

    Skip checkstyle when building

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd dmz && ../gradlew -i build && ../gradlew -i test && cd ..
-cd lib && ../gradlew -i build && ../gradlew -i test && cd ..
-cd util && ../gradlew -i build && ../gradlew -i test && cd ..
-./gradlew clean jacocoTestNoAnalyticsDebugUnitTestReport -x lint -x detekt
+cd dmz && ../gradlew -i build -x checkStyleMain -x checkStyleTest && ../gradlew -i test && cd ..
+cd lib && ../gradlew -i build -x checkStyleMain -x checkStyleTest && ../gradlew -i test && cd ..
+cd util && ../gradlew -i build -x checkStyleMain -x checkStyleTest && ../gradlew -i test && cd ..
+./gradlew clean jacocoTestNoAnalyticsDebugUnitTestReport -x lint -x detekt -x checkStyleMain -x checkStyleTest

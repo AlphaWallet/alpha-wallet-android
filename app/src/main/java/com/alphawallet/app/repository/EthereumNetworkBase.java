269fa9c2190bc1efb22c17142d96844bceec3ccc
justindg <jrbdeguzman@gmail.com>
2022-10-02 04:17:28 -0700

    Make goerli default

--- app/src/main/java/com/alphawallet/app/repository/EthereumNetworkBase.java
+++ app/src/main/java/com/alphawallet/app/repository/EthereumNetworkBase.java
@@ -823,7 +823,7 @@ public abstract class EthereumNetworkBase implements EthereumNetworkRepositoryTy
     @Override
     public Long getDefaultNetwork(boolean isMainNet)
     {
-        return isMainNet ? CustomViewSettings.primaryChain : RINKEBY_ID;
+        return isMainNet ? CustomViewSettings.primaryChain : GOERLI_ID;
     }
 
     @Override

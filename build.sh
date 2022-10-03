773b17c878ba70624b045bc686eb404f8e8e8b3e
James Brown <soylentblue@hotmail.co.uk>
2022-10-03 19:36:57 +1100

    Temp revert code style check

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-cd dmz && ../gradlew -i build -x checkStyleMain -x checkStyleTest && ../gradlew -i test && cd ..
-cd lib && ../gradlew -i build -x checkStyleMain -x checkStyleTest && ../gradlew -i test && cd ..
-cd util && ../gradlew -i build -x checkStyleMain -x checkStyleTest && ../gradlew -i test && cd ..
-./gradlew clean jacocoTestNoAnalyticsDebugUnitTestReport -x lint -x detekt -x checkStyleMain -x checkStyleTest
+# Temporarily revert to get builds working
+# cd dmz && ../gradlew -i build -x checkStyleMain -x checkStyleTest && ../gradlew -i test && cd ..
+# cd lib && ../gradlew -i build -x checkStyleMain -x checkStyleTest && ../gradlew -i test && cd ..
+# cd util && ../gradlew -i build -x checkStyleMain -x checkStyleTest && ../gradlew -i test && cd ..
+# ./gradlew clean jacocoTestNoAnalyticsDebugUnitTestReport -x lint -x detekt -x checkStyleMain -x checkStyleTest
+
+cd dmz && ../gradlew -i build && ../gradlew -i test && cd ..
+cd lib && ../gradlew -i build && ../gradlew -i test && cd ..
+cd util && ../gradlew -i build && ../gradlew -i test && cd ..
+./gradlew clean jacocoTestNoAnalyticsDebugUnitTestReport -x lint -x detekt

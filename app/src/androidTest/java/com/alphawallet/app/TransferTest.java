4cbd4efbb95e069b4ea1f406ea4790e1127dcfc0
Seaborn Lee <seabornlee.cn@gmail.com>
2022-10-20 09:29:57 +0800

    Fix checkstyle

--- app/src/androidTest/java/com/alphawallet/app/TransferTest.java
+++ app/src/androidTest/java/com/alphawallet/app/TransferTest.java
@@ -26,11 +26,13 @@ public class TransferTest extends BaseE2ETest
     // On CI server, run tests on different API levels concurrently may cause failure: Replacement transaction underpriced.
     // Use different wallet to transfer token from can avoid this error
     private static final Map<String, String[]> WALLETS_ON_GANACHE = new HashMap<String, String[]>()
-    {{
-        put("24", new String[]{"0x644022aef70ad515ee186345fd74b005d759f41be8157c2835de3597d943146d", "0xE494323823fdF1A1Ab6ca79d2538C7182690D52a"});
-        put("30", new String[]{"0x5c8843768e0e1916255def80ae7f6197e1f6a2dbcba720038748fc7634e5cffd", "0x162f5e0b63646AAA33a85eA13346F15C5289f901"});
-        put("32", new String[]{"0x992b442eaa34de3c6ba0b61c75b2e4e0241d865443e313c4fa6ab8ba488a6957", "0xd7Ba01f596a7cc926b96b3B0a037c47A22904c06"});
-    }};
+    {
+        {
+            put("24", new String[]{"0x644022aef70ad515ee186345fd74b005d759f41be8157c2835de3597d943146d", "0xE494323823fdF1A1Ab6ca79d2538C7182690D52a"});
+            put("30", new String[]{"0x5c8843768e0e1916255def80ae7f6197e1f6a2dbcba720038748fc7634e5cffd", "0x162f5e0b63646AAA33a85eA13346F15C5289f901"});
+            put("32", new String[]{"0x992b442eaa34de3c6ba0b61c75b2e4e0241d865443e313c4fa6ab8ba488a6957", "0xd7Ba01f596a7cc926b96b3B0a037c47A22904c06"});
+        }
+    };
 
     @Test
     public void should_transfer_from_an_account_to_another()

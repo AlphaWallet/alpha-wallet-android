65cd17654528851e50ee7a1ca4497fb96050ae31
Seaborn Lee <seabornlee.cn@gmail.com>
2022-11-08 14:37:26 +0800

    2864 refactor dappbrowser url bar to widget (#2881)
    * Add Dapp browser e2e test
    * Refactor
    * Wait WebView loading complete

--- app/src/androidTest/java/com/alphawallet/app/BaseE2ETest.java
+++ app/src/androidTest/java/com/alphawallet/app/BaseE2ETest.java
@@ -3,12 +3,10 @@ package com.alphawallet.app;
 
 import static androidx.test.espresso.Espresso.setFailureHandler;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-
 import static com.alphawallet.app.steps.Steps.closeSecurityWarning;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
@@ -18,12 +16,13 @@ import com.alphawallet.app.ui.SplashActivity;
 import com.alphawallet.app.util.CustomFailureHandler;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+
+import timber.log.Timber;
 
 @RunWith(AndroidJUnit4.class)
 public abstract class BaseE2ETest
@@ -42,19 +41,26 @@ public abstract class BaseE2ETest
             = new ActivityScenarioRule<>(SplashActivity.class);
 
     @Before
-    public void setUp() throws UiObjectNotFoundException
+    public void setUp()
     {
         dismissANRSystemDialog();
         closeSecurityWarning();
     }
 
-    private void dismissANRSystemDialog() throws UiObjectNotFoundException
+    private void dismissANRSystemDialog()
     {
         UiDevice device = UiDevice.getInstance(getInstrumentation());
         UiObject waitButton = device.findObject(new UiSelector().textContains("wait"));
         if (waitButton.exists())
         {
-            waitButton.click();
+            try
+            {
+                waitButton.click();
+            }
+            catch (UiObjectNotFoundException e)
+            {
+                Timber.e(e);
+            }
         }
     }
 }

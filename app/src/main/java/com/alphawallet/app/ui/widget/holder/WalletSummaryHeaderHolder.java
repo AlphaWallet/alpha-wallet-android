bf7a675ffef823157c2b0fa384d30045fa3847fb
Seaborn Lee <seabornlee.cn@gmail.com>
2023-01-11 21:36:19 +0800

    Remove isMainnetActive related code

--- app/src/main/java/com/alphawallet/app/ui/widget/holder/WalletSummaryHeaderHolder.java
+++ app/src/main/java/com/alphawallet/app/ui/widget/holder/WalletSummaryHeaderHolder.java
@@ -2,16 +2,10 @@ package com.alphawallet.app.ui.widget.holder;
 
 import static com.alphawallet.app.ui.widget.holder.WalletHolder.FIAT_CHANGE;
 import static com.alphawallet.app.ui.widget.holder.WalletHolder.FIAT_VALUE;
-import static com.alphawallet.app.ui.widget.holder.WalletHolder.IS_MAINNET_ACTIVE;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,47 +14,30 @@ import androidx.core.content.ContextCompat;
 
 import com.alphawallet.app.R;
 import com.alphawallet.app.entity.Wallet;
-import com.alphawallet.app.repository.entity.RealmWalletData;
 import com.alphawallet.app.service.TickerService;
-import com.alphawallet.app.ui.WalletActionsActivity;
 import com.alphawallet.app.ui.widget.entity.WalletClickCallback;
-import com.alphawallet.app.util.Blockies;
-import com.alphawallet.app.util.Utils;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 import io.realm.Realm;
-import io.realm.RealmResults;
 
 public class WalletSummaryHeaderHolder extends BinderViewHolder<Wallet> implements View.OnClickListener {
 
 	public static final int VIEW_TYPE = 1045;
 
-	private final Realm realm;
-	private RealmResults<RealmWalletData> realmUpdate;
-
-	private TextView summaryBalance;
+    private TextView summaryBalance;
 	private TextView summaryChange;
-	private View walletClickLayout;
 
-	private final WalletClickCallback clickCallback;
-
-	public WalletSummaryHeaderHolder(int resId, ViewGroup parent, WalletClickCallback callback, Realm realm) {
+    public WalletSummaryHeaderHolder(int resId, ViewGroup parent) {
 		super(resId, parent);
-		clickCallback = callback;
-		this.realm = realm;
 
-		this.summaryBalance = findViewById(R.id.summary_balance);
+        this.summaryBalance = findViewById(R.id.summary_balance);
 		this.summaryChange = findViewById(R.id.summary_change);
-		this.walletClickLayout = findViewById(R.id.wallet_click_layer);
 	}
 
 	@Override
 	public void bind(@Nullable Wallet data, @NonNull Bundle addition) {
-
-		if (realmUpdate != null) realmUpdate.removeAllChangeListeners();
-
 		//update using addition
 		double fiatValue = addition.getDouble(FIAT_VALUE, 0.00);
 		double oldFiatValue = addition.getDouble(FIAT_CHANGE, 0.00);

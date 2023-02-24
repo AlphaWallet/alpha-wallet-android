f235a077a2bce363feff975bc798f15668dcc9a9
justindg <jrbdeguzman@gmail.com>
2023-02-23 20:16:07 -0800

    Fix lint issues

--- lib/src/main/java/com/alphawallet/token/entity/CryptoFunctionsInterface.java
+++ lib/src/main/java/com/alphawallet/token/entity/CryptoFunctionsInterface.java
@@ -6,14 +6,31 @@ import java.security.SignatureException;
 public interface CryptoFunctionsInterface
 {
     byte[] Base64Decode(String message);
+
     byte[] Base64Encode(byte[] data);
+
     BigInteger signedMessageToKey(byte[] data, byte[] signature) throws SignatureException;
+
     String getAddressFromKey(BigInteger recoveredKey);
+
     byte[] keccak256(byte[] message);
-    CharSequence formatTypedMessage(ProviderTypedData[] rawData); // see class Utils: Uses Android text formatting
-    CharSequence formatEIP721Message(String messageData);         // see class Utils: Uses web3j: you need to provide this function to decode EIP712.
-                                                                  // --- Currently web3j uses a different library for Android and Generic Java packages.
-                                                                  // --- One day web3j could be united, then we can remove these functions
-    byte[] getStructuredData(String messageData);                 // see class Utils: Uses web3j
+
+    /**
+     * See class Utils: Uses Android text formatting
+     */
+    CharSequence formatTypedMessage(ProviderTypedData[] rawData);
+
+    /**
+     * See class Utils: Uses web3j: you need to provide this function to decode EIP712.
+     * -- Currently web3j uses a different library for Android and Generic Java packages.
+     * -- One day web3j could be united, then we can remove these functions
+     */
+    CharSequence formatEIP721Message(String messageData);
+
+    /**
+     * See class Utils: Uses web3j
+     */
+    byte[] getStructuredData(String messageData);
+
     long getChainId(String messageData);
 }

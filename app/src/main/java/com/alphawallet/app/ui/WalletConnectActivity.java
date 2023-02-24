1dd59b184c9b5973ad703788ac665493e17163f5
justindg <jrbdeguzman@gmail.com>
2023-02-23 17:23:06 -0800

    Fix typo

--- app/src/main/java/com/alphawallet/app/ui/WalletConnectActivity.java
+++ app/src/main/java/com/alphawallet/app/ui/WalletConnectActivity.java
@@ -96,7 +96,7 @@ public class WalletConnectActivity extends BaseActivity implements ActionSheetCa
     public static final String WC_LOCAL_PREFIX = "wclocal:";
     public static final String WC_INTENT = "wcintent:";
     private static final String TAG = "WCClient";
-    private static final String DEFAULT_IDON = "https://example.walletconnect.org/favicon.ico";
+    private static final String DEFAULT_ICON = "https://example.walletconnect.org/favicon.ico";
     private static final long CONNECT_TIMEOUT = 10 * DateUtils.SECOND_IN_MILLIS; // 10 Seconds timeout
     private final Handler handler = new Handler(Looper.getMainLooper());
     private final long switchChainDialogCallbackId = 1;
@@ -774,7 +774,7 @@ public class WalletConnectActivity extends BaseActivity implements ActionSheetCa
         }
 
         String[] accounts = {viewModel.getWallet().address};
-        String displayIcon = (peer.getIcons().size() > 0) ? peer.getIcons().get(0) : DEFAULT_IDON;
+        String displayIcon = (peer.getIcons().size() > 0) ? peer.getIcons().get(0) : DEFAULT_ICON;
 
         chainIdOverride = chainIdOverride > 0 ? chainIdOverride : (chainId > 0 ? chainId : MAINNET_ID);
 

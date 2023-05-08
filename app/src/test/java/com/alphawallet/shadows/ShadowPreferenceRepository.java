f4bc19d9f84dd720e423aff17eec81ef45bfe028
justindg <jrbdeguzman@gmail.com>
2023-05-08 15:49:45 -0700

    Add ShadowPreferenceRepostiory

--- app/src/test/java/com/alphawallet/shadows/ShadowPreferenceRepository.java
+++ app/src/test/java/com/alphawallet/shadows/ShadowPreferenceRepository.java
@@ -2,19 +2,16 @@ package com.alphawallet.shadows;
 
 import android.content.Context;
 
+import com.alphawallet.app.repository.SharedPreferenceRepository;
 import com.alphawallet.app.service.AnalyticsService;
 
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
-@Implements(AnalyticsService.class)
-public class ShadowAnalyticsService
+@Implements(SharedPreferenceRepository.class)
+public class ShadowPreferenceRepository
 {
     @Implementation
-    public void __constructor__(Context context, ShadowPreferenceRepository preferenceRepository) {
-    }
-
-    @Implementation
-    public void identify(String uuid) {
+    public void __constructor__(Context context) {
     }
 }

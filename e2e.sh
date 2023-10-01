27b1138f994aecf9d8c899bb3f52bbe043ae72af
James Brown <soylentblue@hotmail.co.uk>
2023-10-01 13:57:13 +1100

    Update ganache config for new runner

--- e2e.sh
+++ e2e.sh
@@ -19,7 +19,7 @@ chmod 666 output/emulator.log                # allow writing to log file
 adb logcat >> output/emulator.log &
 
 source ~/.bashrc
-ganache --chain.chainId 2 -h 0.0.0.0 -m "horse light surface bamboo combine item lumber tunnel choose acid mail feature"
+ganache --chain.chainId 2 -h 0.0.0.0 -m "horse light surface bamboo combine item lumber tunnel choose acid mail feature" --server.port 8555
 adb shell rm /storage/emulated/0/DCIM/*.png
 ./gradlew :app:uninstallAll :app:connectedNoAnalyticsDebugAndroidTest -x lint -PdisablePreDex
 adb shell rm /storage/emulated/0/DCIM/*.png

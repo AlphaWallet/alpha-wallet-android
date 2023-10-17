5c8712695f4195e6b28dd643e5fd114b96ffaef0
James Brown <soylentblue@hotmail.co.uk>
2023-10-17 14:48:48 +1100

    Fix for starting with awallet: intent (#3329)

--- app/src/test/java/com/alphawallet/app/IntentTest.java
+++ app/src/test/java/com/alphawallet/app/IntentTest.java
@@ -32,6 +32,9 @@ public class IntentTest
         String walletconnectTest2 = "wc:d3995aaf65b26f95e6047aa93dec800255b6d763823472238b8137dc3ff519c3%402%3Frelay-protocol%3Dirn%26symKey" +
                 "%3D6ce01accc21d0b59752107d3d43c303ee7ccbdb5f418923c9b12742630f4c792";
         String walletconnectTest3 = "awallet://wc?uri=wc%3A5f577f99-2f54-40f7-9463-7ff640772090%401%3Fbridge%3Dhttps%253A%252F%252Fwalletconnect.depay.com%26key%3D1938aa2c9d4104c91cbc60e94631cf769c96ebad1ea2fc30e18ba09e39bc3c0b";
+        String walletconnectTest4 = "awallet://openURL?q=wc%3A115a1223589295f91056be883897ce4edef1525f0becdec54b24cb6e296e9a40%402%3Frelay-protocol%3Dirn%26symKey%3Dbf97933cebac73de3106e3deff551ec09b0d2a13ac1ed40d7dc8e33de1a4f86b";
+
+        String attestationIntent = "awallet://openURL?q=https%3A%2F%2Fsmart-layer.vercel.app%2Fpass%3Ftype%3Deas%26ticket%3DeNrFU0luHDEM_EufBwF3kUfbnflE4INEUQ8wEiDPj3q-0IBDQSvIIotQ_TrgB9nxQETVvTwO-PtB1mK9n9ymCrt_lAQ_8XxvygQnsRU6ah2Xs3uGjl41uyKEFEwlsNZUbdbYAdqXUFApoZl001YRAdJWkvcXiFaKTmQ2XrUWEU0jqjArg2jWcq3pmcyLNRC4iXBijbWSMfF4ULtwRGxzALd402l6PpsN15-Up4_xPOcbZbeS-UraFsGqBmni1Xs4sQzHYbkZiDj3SITu6sGbkE-IXZxXDhrpaC8QuGkXyO6-BYopIz3g9fD760_dh_ebxeVddvgfoy_ryjIFlVQk9x6i-3N-n3Umjj0aM8M-8zW_L70gCMzF3TJ4BcAW8EKmwt5tzcCapbgV5ZoOcnXGcEHAjLA09vTLHWUMjzFCbF9qRZ8JW60T5qQuTZKbKzYzLNn6m2trZkukQzDelQd8_gOdBAEY%26secret%3D0x1a89af8bed1c8ce25e2737a7e3990a7f302f29094380234d6adf9ec62b7b3636%26id%3Dweihong0427%2540163.com";
 
         DeepLinkRequest request = DeepLinkService.parseIntent(walletconnectTest1, null);
         assertTrue(request.type == DeepLinkType.WALLETCONNECT);
@@ -45,9 +48,16 @@ public class IntentTest
         assertTrue(request.type == DeepLinkType.WALLETCONNECT);
         assertTrue(request.data.startsWith("wc:"));
 
+        request = DeepLinkService.parseIntent(walletconnectTest4, null);
+        assertTrue(request.type == DeepLinkType.WALLETCONNECT);
+        assertTrue(request.data.startsWith("wc:"));
+
         //SmartPass import
         String sp = "https://aw.app/openurl?url=?ticket=eNrFk02OGzAIRu-SdVRhwGCWk0lziaoLfswBqlbq8evMHCFSB3ll2e8DpPfjAt9QLtdxvcDft4GgD7rdHJG_Y1pSyn36Q-gOO8Zc-L7n0svzMUw37_JKnRLFsIoz02JVZEiotsQ07gnskZDmgo4521iaPiE4ta2bo7HHajVi0zW82MeeOVDVypDZdeXQYVukIcyB8nTjlyvqkyPy9hCFh1YrNQK8s9_mJCDoc3bfWM-H-0eoB5UtVeWyzr0nKW7J6p60BKmrNp8QJF4CprVIxqId1Fkw4nP8F-sJOWsX4xODYFf4uPj9689-Hb9ebC7hS2u8TFBRMWUhaTHZ_38Cwq9cIA9gdJvCDSURR7AC34Kr0Plo2tKqySE8yUj3YDuqEeBYIsXmy2H1dgB7Cl6hCMJ0xCvEozYuDa3hMjVzxBrlVDNjL8IRfhzHV_WAn_8AxSYC3Q==&secret=0x2eb74750df993a163a95db3031359f0bfca6eb56e597f6ede056bd69d1803196&id=geman%40gemanji.com";
         request = DeepLinkService.parseIntent(sp, null);
+        assertTrue(request.type == DeepLinkType.SMARTPASS);
+
+        request = DeepLinkService.parseIntent(attestationIntent, null);
         assertTrue(request.type == DeepLinkType.SMARTPASS);
 
         String sp2 = "https://aw.app/openurl?url=ticket=eNrFk02OGzAIRu-SdVRhwGCWk0lziaoLfswBqlbq8evMHCFSB3ll2e8DpPfjAt9QLtdxvcDft4GgD7rdHJG_Y1pSyn36Q-gOO8Zc-L7n0svzMUw37_JKnRLFsIoz02JVZEiotsQ07gnskZDmgo4521iaPiE4ta2bo7HHajVi0zW82MeeOVDVypDZdeXQYVukIcyB8nTjlyvqkyPy9hCFh1YrNQK8s9_mJCDoc3bfWM-H-0eoB5UtVeWyzr0nKW7J6p60BKmrNp8QJF4CprVIxqId1Fkw4nP8F-sJOWsX4xODYFf4uPj9689-Hb9ebC7hS2u8TFBRMWUhaTHZ_38Cwq9cIA9gdJvCDSURR7AC34Kr0Plo2tKqySE8yUj3YDuqEeBYIsXmy2H1dgB7Cl6hCMJ0xCvEozYuDa3hMjVzxBrlVDNjL8IRfhzHV_WAn_8AxSYC3Q==&secret=0x2eb74750df993a163a95db3031359f0bfca6eb56e597f6ede056bd69d1803196&id=geman%40gemanji.com";
@@ -56,6 +66,12 @@ public class IntentTest
 
         //Test URL Redirect
         String url = "https://aw.app/openurl?url=https://ratyeeting.com?ratlaunch=5000";
+        request = DeepLinkService.parseIntent(url, null);
+        assertTrue(request.type == DeepLinkType.URL_REDIRECT);
+        assertTrue(request.data.equals("https://ratyeeting.com?ratlaunch=5000"));
+
+        //test awallet style link
+        url = "awallet://openURL?q=https://ratyeeting.com?ratlaunch=5000";
         request = DeepLinkService.parseIntent(url, null);
         assertTrue(request.type == DeepLinkType.URL_REDIRECT);
         assertTrue(request.data.equals("https://ratyeeting.com?ratlaunch=5000"));

6742cda791ab1823b29a774807ffa4ae092584fc
Michael Wallace <micwallace@gmx.com>
2024-01-18 17:13:54 +1100

    chore: remove redundant and outdated stuff

--- app/src/main/java/com/alphawallet/app/ui/NFTAssetDetailActivity.java
+++ app/src/main/java/com/alphawallet/app/ui/NFTAssetDetailActivity.java
@@ -16,7 +16,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;

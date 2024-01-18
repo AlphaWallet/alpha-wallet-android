78fdfb9cd88b12635dbae898fcee4c4171e06139
Michael Wallace <micwallace@gmx.com>
2024-01-18 17:11:53 +1100

    chore: remove redundant and outdated stuff

--- app/src/main/java/com/alphawallet/app/viewmodel/NFTAssetsViewModel.java
+++ app/src/main/java/com/alphawallet/app/viewmodel/NFTAssetsViewModel.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import com.alphawallet.app.C;
 import com.alphawallet.app.entity.Wallet;
 import com.alphawallet.app.entity.nftassets.NFTAsset;
-import com.alphawallet.app.entity.tokens.Attestation;
 import com.alphawallet.app.entity.tokens.Token;
 import com.alphawallet.app.interact.FetchTransactionsInteract;
 import com.alphawallet.app.service.AssetDefinitionService;
@@ -16,7 +15,6 @@ import com.alphawallet.app.ui.Erc1155AssetListActivity;
 import com.alphawallet.app.ui.NFTAssetDetailActivity;
 
 import java.math.BigInteger;
-import java.util.List;
 
 import javax.inject.Inject;
 
@@ -74,17 +72,6 @@ public class NFTAssetsViewModel extends BaseViewModel
         intent.putExtra(C.EXTRA_CHAIN_ID, token.tokenInfo.chainId);
         intent.putExtra(C.EXTRA_ADDRESS, token.getAddress());
         intent.putExtra(C.EXTRA_TOKEN_ID, tokenId.toString());
-        if (asset != null) intent.putExtra(C.EXTRA_NFTASSET, asset);
-        return intent;
-    }
-
-    public Intent showAssetDetailsEmbedded(Context context, Wallet wallet, long chainId, String tokenAddress, String tokenId, NFTAsset asset)
-    {
-        Intent intent = new Intent(context, NFTAssetDetailActivity.class);
-        intent.putExtra(C.Key.WALLET, wallet);
-        intent.putExtra(C.EXTRA_CHAIN_ID, chainId);
-        intent.putExtra(C.EXTRA_ADDRESS, tokenAddress);
-        intent.putExtra(C.EXTRA_TOKEN_ID, tokenId);
         if (asset != null) intent.putExtra(C.EXTRA_NFTASSET, asset);
         return intent;
     }

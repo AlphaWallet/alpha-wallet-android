5a94f899b0755667782f26c0ca08d5434c93c30c
James Brown <soylentblue@hotmail.co.uk>
2024-03-10 11:14:19 +1100

    Update build test script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#No DMZ until we fix the Gradle 8 build
+#No DMZ until we fix the Gradle 8 build. Deprecate util build
 #cd dmz && ../gradlew -i build && ../gradlew -i test && cd ..
 cd lib && ../gradlew -i build && ../gradlew -i test && cd ..
-cd util && ../gradlew -i build && ../gradlew -i test && cd ..
+#cd util && ../gradlew -i build && ../gradlew -i test && cd ..
 ./gradlew clean jacocoTestNoAnalyticsDebugUnitTestReport -x lint -x detekt
